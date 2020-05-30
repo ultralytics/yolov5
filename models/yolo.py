f129cdda9f4ed7bd23acfd2d4f3b57bb3edaa23b
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-05-30 11:03:50 -0700

    updates

--- models/yolo.py
+++ models/yolo.py
@@ -170,7 +170,7 @@ def parse_model(md, ch):  # model_dict, input_channels(3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='yolov5s_csp.yaml', help='model.yaml')
+    parser.add_argument('--cfg', type=str, default='yolov5s.yaml', help='model.yaml')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     opt = parser.parse_args()
     opt.cfg = glob.glob('./**/' + opt.cfg, recursive=True)[0]  # find file
