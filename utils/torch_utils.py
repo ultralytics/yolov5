c12023f14543223194df8a6aa8ec826264190089
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 20:50:43 -0700

    updates

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -141,7 +141,7 @@ def scale_img(img, ratio=1.0, same_shape=True):  # img(16,3,256,416), r=ratio
     s = (int(h * ratio), int(w * ratio))  # new size
     img = F.interpolate(img, size=s, mode='bilinear', align_corners=False)  # resize
     if not same_shape:  # pad/crop img
-        gs = 64  # (pixels) grid size
+        gs = 32  # (pixels) grid size
         h, w = [math.ceil(x * ratio / gs) * gs for x in (h, w)]
     return F.pad(img, [0, w - s[1], 0, h - s[0]], value=0.447)  # value = imagenet mean
 
