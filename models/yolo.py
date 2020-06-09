715cb08b108b9f92519c5f2eeee6db27e3d9d930
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-08 22:15:16 -0700

    module updates

--- models/yolo.py
+++ models/yolo.py
@@ -149,7 +149,7 @@ def parse_model(md, ch):  # model_dict, input_channels(3)
                 pass
 
         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [nn.Conv2d, Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, ConvPlus, BottleneckCSP, BottleneckLight]:
+        if m in [nn.Conv2d, Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, ConvPlus, BottleneckCSP]:
             c1, c2 = ch[f], args[0]
 
             # Normal
