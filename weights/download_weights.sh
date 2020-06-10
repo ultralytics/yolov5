9b62f9962e198660cdbe32eeab42d20035069854
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-10 00:44:34 -0700

    bug fix #30

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Download common models
 
-python3 -c "from models import *;
+python3 -c "from utils.google_utils import *;
 attempt_download('weights/yolov5s.pt');
 attempt_download('weights/yolov5m.pt');
 attempt_download('weights/yolov5l.pt')"
