afe1df385b5ef37c3f75a49ceda94fdc448bf490
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 15:08:14 -0700

    dist.destroy_process_group() bug fix

--- train.py
+++ train.py
@@ -356,7 +356,7 @@ def train(hyp):
     if not opt.evolve:
         plot_results()  # save as results.png
     print('%g epochs completed in %.3f hours.\n' % (epoch - start_epoch + 1, (time.time() - t0) / 3600))
-    dist.destroy_process_group() if torch.cuda.device_count() > 1 else None
+    dist.destroy_process_group() if device.type != 'cpu' and torch.cuda.device_count() > 1 else None
     torch.cuda.empty_cache()
     return results
 
