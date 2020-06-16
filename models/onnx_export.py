e1e33992865ddc9eb5a0d7f0ed1ccfb7d7a33e8c
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 09:59:42 -0700

    ONNX export bug fix #93

--- models/onnx_export.py
+++ models/onnx_export.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     # Load pytorch model
     google_utils.attempt_download(opt.weights)
-    model = torch.load(opt.weights, map_location=torch.device('cpu'))['model']
+    model = torch.load(opt.weights, map_location=torch.device('cpu'))['model'].float()
     model.eval()
     model.fuse()
 
