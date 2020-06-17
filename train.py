3b2b330872feeff208ac79c469fb9936d9cf2723
Alex Stoken <alex.stoken@gmail.com>
2020-06-17 15:55:45 -0500

    Move results.txt from weights/ to log_dir

--- train.py
+++ train.py
@@ -50,7 +50,7 @@ def train(hyp):
     os.makedirs(wdir, exist_ok=True)
     last = wdir + 'last.pt'
     best = wdir + 'best.pt'
-    results_file = wdir + 'results.txt'
+    results_file = log_dir + 'results.txt'
 
     epochs = opt.epochs  # 300
     batch_size = opt.batch_size  # 64
