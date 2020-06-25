9a9c4f1259c91ea6495095ce17d71f4ffe8a4304
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-24 18:00:43 -0700

    update yolo.py default --cfg

--- models/yolo.py
+++ models/yolo.py
@@ -207,7 +207,7 @@ def parse_model(md, ch):  # model_dict, input_channels(3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='yolov5m-p6.yaml', help='model.yaml')
+    parser.add_argument('--cfg', type=str, default='yolov5s.yaml', help='model.yaml')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     opt = parser.parse_args()
     opt.cfg = check_file(opt.cfg)  # check file
