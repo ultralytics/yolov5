a4e4d6a9a0d8d53de089c0b0ec8ac6b20d67c57b
Russ Ferriday <russ.ferriday@gmail.com>
2020-06-26 23:09:17 +0100

    rename for clarity

--- run_xavier.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker run -it --rm \
-  --runtime nvidia \
-  --network host \
-  --mount src=$(pwd)/weights,target=/usr/src/app/weights/,type=bind \
-  --mount src=$(pwd)/inference,target=/usr/src/app/inference/,type=bind \
-  yolov5-app:latest python3 detect.py
