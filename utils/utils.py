4e62eb90be664dcd6c19b4cc70a8bc4e5bd394fb
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-30 16:41:26 -0700

    check_img_size() int() update

--- utils/utils.py
+++ utils/utils.py
@@ -47,7 +47,7 @@ def check_git_status():
 
 def check_img_size(img_size, s=32):
     # Verify img_size is a multiple of stride s
-    new_size = make_divisible(img_size, s)  # ceil gs-multiple
+    new_size = make_divisible(img_size, int(s))  # ceil gs-multiple
     if new_size != img_size:
         print('WARNING: --img-size %g must be multiple of max stride %g, updating to %g' % (img_size, s, new_size))
     return new_size
