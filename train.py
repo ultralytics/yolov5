13f69777a69c4a6056c7cb8499c7e3910868122d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-02 09:26:03 -0700

    typo fix

--- train.py
+++ train.py
@@ -119,7 +119,7 @@ def train(hyp):
             model.load_state_dict(ckpt['model'], strict=False)
         except KeyError as e:
             s = "%s is not compatible with %s. This may be due to model differences or %s may be out of date. " \
-                "Please delete or update %s and try again, or use --weights '' to train from scatch." \
+                "Please delete or update %s and try again, or use --weights '' to train from scratch." \
                 % (opt.weights, opt.cfg, opt.weights, opt.weights)
             raise KeyError(s) from e
 
