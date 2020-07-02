a8d17ce7124e81907020d5d8e7737a22a46657f0
ChristopherSTAN <497592613@qq.com>
2020-07-02 14:25:28 -0400

    Update train.py

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ hyp = {'lr0': 0.01,  # initial learning rate (SGD=1E-2, Adam=1E-3)
        'obj_pw': 1.0,  # obj BCELoss positive_weight
        'iou_t': 0.20,  # iou training threshold
        'anchor_t': 4.0,  # anchor-multiple threshold
-       'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
+       'fl_gamma': 1.5,  # focal loss gamma (efficientDet default is gamma=1.5)
        'hsv_h': 0.014,  # image HSV-Hue augmentation (fraction)
        'hsv_s': 0.68,  # image HSV-Saturation augmentation (fraction)
        'hsv_v': 0.36,  # image HSV-Value augmentation (fraction)
