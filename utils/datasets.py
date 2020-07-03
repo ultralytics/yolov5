aaba4689a7d2bfdd661736b97395c2fd7436b030
NanoCode012 <kevinvong@rocketmail.com>
2020-07-03 10:47:07 +0700

    Add shuffle parameter for train's dataloader

--- utils/datasets.py
+++ utils/datasets.py
@@ -62,6 +62,7 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
                                              batch_size=batch_size,
                                              num_workers=nw,
                                              pin_memory=True,
+                                             shuffle=True,
                                              collate_fn=LoadImagesAndLabels.collate_fn,
                                              sampler=datasampler)
     else:
