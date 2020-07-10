66497700010d0d759007f7c81f5948f53747996b
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-09 20:39:11 -0700

    --rect test bug fix

--- utils/datasets.py
+++ utils/datasets.py
@@ -343,6 +343,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
             irect = ar.argsort()
             self.img_files = [self.img_files[i] for i in irect]
             self.label_files = [self.label_files[i] for i in irect]
+            self.labels = [self.labels[i] for i in irect]
             self.shapes = s[irect]  # wh
             ar = ar[irect]
 
