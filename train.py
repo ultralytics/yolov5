5de4e25d68a2b6c3f9a51f28a1a26accb86dc318
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-11 11:39:02 -0700

    update tensorboard metric

--- train.py
+++ train.py
@@ -308,7 +308,7 @@ def train(hyp):
         # Tensorboard
         if tb_writer:
             tags = ['train/giou_loss', 'train/obj_loss', 'train/cls_loss',
-                    'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/F1',
+                    'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',
                     'val/giou_loss', 'val/obj_loss', 'val/cls_loss']
             for x, tag in zip(list(mloss[:-1]) + list(results), tags):
                 tb_writer.add_scalar(tag, x, epoch)
