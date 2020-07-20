248308dac91c11e4e416b88cf7be4e96f01d23cb
Alex Stoken <alex.stoken@gmail.com>
2020-07-20 16:02:33 -0500

    Clarify opt print statement

--- train.py
+++ train.py
@@ -426,7 +426,7 @@ if __name__ == '__main__':
         opt.hyp = check_file(opt.hyp)  # check file
         with open(opt.hyp) as f:
             hyp.update(yaml.load(f, Loader=yaml.FullLoader))  # update hyps
-    print(opt)
+    print(f'Training options:{opt}')
     opt.img_size.extend([opt.img_size[-1]] * (2 - len(opt.img_size)))  # extend to 2 sizes (train, test)
     device = torch_utils.select_device(opt.device, apex=mixed_precision, batch_size=opt.batch_size)
     if device.type == 'cpu':
