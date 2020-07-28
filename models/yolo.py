91af0401d8c3684903a07b1a14c2231d75971c61
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-28 14:48:33 -0700

    PyTorch 1.6.0 compatibility updates

--- models/yolo.py
+++ models/yolo.py
@@ -148,6 +148,7 @@ class Model(nn.Module):
         print('Fusing layers... ', end='')
         for m in self.model.modules():
             if type(m) is Conv:
+                m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatability
                 m.conv = torch_utils.fuse_conv_and_bn(m.conv, m.bn)  # update conv
                 m.bn = None  # remove batchnorm
                 m.forward = m.fuseforward  # update forward
