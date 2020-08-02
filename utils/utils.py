023e37807ce4b6ac9fcf5848daa911c8f67c0a23
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-02 13:18:03 -0700

    hyperparameter evolution update (#566)

--- utils/utils.py
+++ utils/utils.py
@@ -841,7 +841,7 @@ def print_mutation(hyp, results, yaml_file='hyp_evolved.yaml', bucket=''):
     for i, k in enumerate(hyp.keys()):
         hyp[k] = float(x[0, i + 7])
     with open(yaml_file, 'w') as f:
-        results = x[0, :7]
+        results = tuple(x[0, :7])
         c = '%10.4g' * len(results) % results  # results (P, R, mAP@0.5, mAP@0.5:0.95, val_losses x 3)
         f.write('# Hyperparameter Evolution Results\n# Generations: %g\n# Metrics: ' % len(x) + c + '\n\n')
         yaml.dump(hyp, f, sort_keys=False)
