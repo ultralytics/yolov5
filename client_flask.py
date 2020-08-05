dbe12d0a880fa2c96b0efa5e94cab8c7888b5597
Yaser Sakkaf <36536522+yasersakkaf@users.noreply.github.com>
2020-08-05 20:54:42 +0530

    Created inference API in file flask_detect.py
    The file flask_detect.py is an edited version of detect.py, it works as an API. I have used Flask for this purpose. To pass required arguments make changes in the the api_data.json

---
+++ client_flask.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+# path to the json file
+json_file = 'api_data.json'
+url = 'http://localhost:5000/detect'
+with open(json_file) as f:
+    data = json.load(f)
+
+server_return = requests.post(url, json=data)
+print(server_return)
+
