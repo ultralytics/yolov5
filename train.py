13e8e6ca7d6021d89feb33ac6b3b96121decfdf7
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-09 19:01:22 -0700

    DDP update

--- train.py
+++ train.py
@@ -51,7 +51,8 @@ def train(hyp, opt, device, tb_writer=None):
     init_seeds(2 + rank)
     with open(opt.data) as f:
         data_dict = yaml.load(f, Loader=yaml.FullLoader)  # model dict
-    check_dataset(data_dict)  # check
+    with torch_distributed_zero_first(rank):
+        check_dataset(data_dict)  # check
     train_path = data_dict['train']
     test_path = data_dict['val']
     nc, names = (1, ['item']) if opt.single_cls else (int(data_dict['nc']), data_dict['names'])  # number classes, names
