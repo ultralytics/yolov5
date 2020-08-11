fe338550023e8d947965016e52724788dcbb2e05
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-11 12:00:53 -0700

    Update google_utils.py

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -5,42 +5,40 @@
 import os
 import platform
 import time
-import torch
 from pathlib import Path
+import torch.hub as hub
 
 
 def attempt_download(weights):
     # Attempt to download pretrained weights if not found locally
     weights = weights.strip().replace("'", '')
-    msg = weights + ' missing, try downloading from https://drive.google.com/drive/folders/1Drs_Aiu7xx6S-ix95f9kNsA6ueKRpN2J'
+    file = Path(weights).name
 
-    r = 1  # return
-    if len(weights) > 0 and not os.path.isfile(weights):
-        d = {'yolov3-spp.pt': '1mM67oNw4fZoIOL1c8M3hHmj66d8e-ni_',  # yolov3-spp.yaml
-             'yolov5s.pt': '1R5T6rIyy3lLwgFXNms8whc-387H0tMQO',  # yolov5s.yaml
-             'yolov5m.pt': '1vobuEExpWQVpXExsJ2w-Mbf3HJjWkQJr',  # yolov5m.yaml
-             'yolov5l.pt': '1hrlqD1Wdei7UT4OgT785BEk1JwnSvNEV',  # yolov5l.yaml
-             'yolov5x.pt': '1mM8aZJlWTxOg7BZJvNUMrTnA2AbeCVzS',  # yolov5x.yaml
-             }
+    msg = weights + ' missing, try downloading from https://github.com/ultralytics/yolov5/releases/'
+    models = ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']  # available models
 
-        file = Path(weights).name
-        if file in d:
-            r = gdrive_download(id=d[file], name=weights)
+    if file in models and not os.path.isfile(weights):
+        # Google Drive
+        # d = {'yolov5s.pt': '1R5T6rIyy3lLwgFXNms8whc-387H0tMQO',
+        #      'yolov5m.pt': '1vobuEExpWQVpXExsJ2w-Mbf3HJjWkQJr',
+        #      'yolov5l.pt': '1hrlqD1Wdei7UT4OgT785BEk1JwnSvNEV',
+        #      'yolov5x.pt': '1mM8aZJlWTxOg7BZJvNUMrTnA2AbeCVzS'}
+        # r = gdrive_download(id=d[file], name=weights) if file in d else 1  # return value
+        # if r == 0 and os.path.exists(weights) and os.path.getsize(weights) > 1E6:  # check
+        #    return
 
-        if not (r == 0 and os.path.exists(weights) and os.path.getsize(weights) > 1E6):  # weights exist and > 1MB
+        # GitHub
+        hub.download_url_to_file('https://github.com/ultralytics/yolov5/releases/download/v2.0/' + file, weights)
+        if os.path.exists(weights) and os.path.getsize(weights) > 1E6:  # check
+            return
+
+        # GCP (redundant backup)
+        hub.download_url_to_file('https://storage.googleapis.com/ultralytics/yolov5/ckpt/' + file, weights)
+        if os.path.exists(weights) and os.path.getsize(weights) > 1E6:  # check
+            return
+        else:
             os.remove(weights) if os.path.exists(weights) else None  # remove partial downloads
-            s = 'curl -L -o %s "storage.googleapis.com/ultralytics/yolov5/ckpt/%s"' % (weights, file)
-            r = os.system(s)  # execute, capture return values
-
-            # Error check
-            if not (r == 0 and os.path.exists(weights) and os.path.getsize(weights) > 1E6):  # weights exist and > 1MB
-                os.remove(weights) if os.path.exists(weights) else None  # remove partial downloads
-                
-                torch.hub.download_url_to_file("https://github.com/ultralytics/yolov5/releases/download/v2.0/%s" % file, weights)
-                if not (os.path.exists(weights) and os.path.getsize(weights) > 1E6):  # weights exist and > 1MB
-                    os.remove(weights) if os.path.exists(weights) else None  # remove partial downloads
-
-                    raise Exception(msg)
+            raise Exception(msg)
 
 
 def gdrive_download(id='1n_oKgR81BJtqk75b00eAjdv03qVCQn2f', name='coco128.zip'):
