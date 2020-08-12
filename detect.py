9f482cbcb885c7b37df47285afd3e3bbaaaa780d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-11 20:11:44 -0700

    utils.general comment updates/bug fixes

--- detect.py
+++ detect.py
@@ -138,7 +138,7 @@ def detect(save_img=False):
 
     if save_txt or save_img:
         print('Results saved to %s' % Path(out))
-        if platform == 'darwin' and not opt.update:  # MacOS
+        if platform.system() == 'Darwin' and not opt.update:  # MacOS
             os.system('open ' + save_path)
 
     print('Done. (%.3fs)' % (time.time() - t0))
