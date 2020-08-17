76812d08a6c12efb09cb7f08e870f957505b4cd5
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-17 14:42:37 -0700

    bug fix in get_latest_run()

--- utils/general.py
+++ utils/general.py
@@ -61,7 +61,7 @@ def init_seeds(seed=0):
 def get_latest_run(search_dir='./runs'):
     # Return path to most recent 'last.pt' in /runs (i.e. to --resume from)
     last_list = glob.glob(f'{search_dir}/**/last*.pt', recursive=True)
-    return max(last_list, key=os.path.getctime)
+    return max(last_list, key=os.path.getctime) if last_list else ''
 
 
 def check_git_status():
