aa6862ae13f09fc18f27c975cc66241df834266c
Baptiste Amato <baptiste.amato@gmail.com>
2020-08-19 14:54:42 +0200

    Make a library.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='yolov5',
+      version='1.0',
+      author="Baptiste Amato",
+      description="Fork of yolov5",
+      url="https://github.com/PsycleResearch/yolov5",
+      packages=['yolov5'],
+      python_requires='>=3.6'
+      )
