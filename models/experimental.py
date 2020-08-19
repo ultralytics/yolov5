d633c3ea3d85f163330330ee6a2294c5376c9329
Baptiste Amato <baptiste.amato@gmail.com>
2020-08-19 14:16:34 +0200

    Fix bug of import.

--- models/experimental.py
+++ models/experimental.py
@@ -1,5 +1,8 @@
 # This file contains experimental modules
 
+import sys
+sys.path.insert(0, './yolov5')  # Fix according to https://github.com/ultralytics/yolov5/issues/353
+
 import numpy as np
 import torch
 import torch.nn as nn
