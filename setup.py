a36e69b71cb757068d05737b008e52506f1568c7
Baptiste Amato <baptiste.amato@gmail.com>
2020-08-19 15:02:00 +0200

    Fix.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='yolov5',
       author="Baptiste Amato",
       description="Fork of yolov5",
       url="https://github.com/PsycleResearch/yolov5",
-      packages=['yolov5'],
+      packages=['yolov5', 'yolov5.models', 'yolov5.utils'],
       python_requires='>=3.6'
       )
