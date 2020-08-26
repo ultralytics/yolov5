1e7674d4dd7ba38ccb9adbdb7c19784693301832
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-25 23:07:22 -0700

    Update export.py --img-size comment

--- models/export.py
+++ models/export.py
@@ -17,7 +17,7 @@ from utils.general import set_logging
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='./yolov5s.pt', help='weights path')  # from yolov5/models/
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='image size')
+    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='image size')  # height, width
     parser.add_argument('--batch-size', type=int, default=1, help='batch size')
     opt = parser.parse_args()
     opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
