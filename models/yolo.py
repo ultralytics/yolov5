93ca28693f530e0d81357d00904195724e3b3ed6
tomguluson92 <314913739@qq.com>
2020-09-11 15:38:11 +0800

    remove `nn.Conv2d` in default settings
    Hi, I found if we modify the `Conv` to `nn.Conv2d`, it's hard to set the in and out channel explicitly, which would hinder the debugging. 
    On top of that, I would suggest to remove `nn.Conv2d` out of the boundary.

--- models/yolo.py
+++ models/yolo.py
@@ -188,7 +188,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 pass
 
         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [nn.Conv2d, Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, C3]:
+        if m in [Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, C3]:
             c1, c2 = ch[f], args[0]
 
             # Normal
