6062bb7deec2a38da5d437b47267bc1d7eb4582a
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-11 17:44:11 -0700

    print self.yaml['nc'] override update

--- models/yolo.py
+++ models/yolo.py
@@ -71,7 +71,7 @@ class Model(nn.Module):
 
         # Define model
         if nc and nc != self.yaml['nc']:
-            print('Overriding %s nc=%g with nc=%g' % (cfg, self.yaml['nc'], nc))
+            print('Overriding model.yaml nc=%g with nc=%g' % (self.yaml['nc'], nc))
             self.yaml['nc'] = nc  # override yaml value
         self.model, self.save = parse_model(deepcopy(self.yaml), ch=[ch])  # model, savelist, ch_out
         # print([x.shape for x in self.forward(torch.zeros(1, ch, 64, 64))])
