cf1bb1c4808b53b068e02be34c8072699a782f91
Jiacong Fang <zldrobit@126.com>
2020-09-12 20:09:30 +0800

    Use img.half() if half and with pytorch

--- detect.py
+++ detect.py
@@ -162,7 +162,7 @@ def detect(save_img=False):
 
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img = img.half() if half and backend == 'pytorch' else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
