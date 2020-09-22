52cf9357f5ad680785aa03e31be70053e3a21d1d
jim879546213 <jim879546213@gmail.com>
2020-09-22 11:34:51 +0800

    Update train.py
    關閉aug

--- train.py
+++ train.py
@@ -165,7 +165,7 @@ def train(hyp, opt, device, tb_writer=None):
 
     # Trainloader
     dataloader, dataset = create_dataloader(train_path, imgsz, batch_size, gs, opt,
-                                            hyp=hyp, augment=True, cache=opt.cache_images, rect=opt.rect,
+                                            hyp=hyp, augment=False, cache=opt.cache_images, rect=opt.rect,
                                             rank=rank, world_size=opt.world_size, workers=opt.workers)
     mlc = np.concatenate(dataset.labels, 0)[:, 0].max()  # max label class
     nb = len(dataloader)  # number of batches
