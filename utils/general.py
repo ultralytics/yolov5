4d6e17c36b253d82357561882984bd63a06f68c3
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-09 14:49:47 +0200

    200 dpi

--- utils/general.py
+++ utils/general.py
@@ -310,7 +310,7 @@ def ap_per_class(tp, conf, pred_cls, target_cls, plot=False, fname='precision-re
         ax.set_ylim(0, 1)
         plt.legend()
         fig.tight_layout()
-        fig.savefig(fname, dpi=300)
+        fig.savefig(fname, dpi=200)
 
     return p, r, ap, f1, unique_classes.astype('int32')
 
