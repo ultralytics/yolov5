88f986b847474c383f3d482c0be1db3cca233227
vnoblet_nanovare <vincent@nanovare.com>
2020-10-14 19:42:06 +0200

    Discard an image empty from annotations // Merge and download separately

--- make_mojo_yolo_data.py
+++ make_mojo_yolo_data.py
@@ -15,13 +15,25 @@ ANNOTATION_CLASSES_TO_ID = {"sperm": 0}
 
 def make_yolo_dataset(rgb=False):
     api = sly.Api()
-    api.download_merge_project(constants.SUPERVISELY_LOCALISATION_PROJECT_ID, dataset_filter_id=[383649, 383651, 383653])
-    frames_path = api.get_project_image_path_list(constants.SUPERVISELY_LOCALISATION_PROJECT_ID)
+    download_list = None
+    merge_list = [383649]
+    api.download_project(
+        constants.SUPERVISELY_LOCALISATION_PROJECT_ID,
+        dataset_filter_id=download_list,
+        update=False,
+    )
+    image_dir = api.merge_project(
+        constants.SUPERVISELY_LOCALISATION_PROJECT_ID,
+        dataset_filter_id=merge_list,
+        dir_name="all_test2",
+    )
+    frames_path = list(image_dir.glob("*.png"))
     
     resolved_root = Path(ROOT_YOLO_OUTPUT).resolve()
     if resolved_root.is_dir():
         shutil.rmtree(resolved_root)
-    
+    discard_count = 0
+    total_count = len(frames_path)
     for frame_path in frames_path:
 
         annotation_path = frame_path.parents[1] / "ann" / (frame_path.name + ".json")
@@ -38,6 +50,19 @@ def make_yolo_dataset(rgb=False):
         else:
             with annotation_path.open() as annotation_file:
                 annotation_data = json.load(annotation_file)
+            discard = False
+            # Remove images with "To ignore" tag
+            for tag in annotation_data["tags"]:
+                if tag["name"] == "To ignore":
+                    discard = True
+                    break
+            # Remove images with no objects (to remove images not tagged yet)
+            if len(annotation_data["objects"]) == 0:
+                discard = True
+
+            if discard:
+                discard_count += 1
+                continue
 
         if rgb:
             frame = cv2.imread(str(frame_path), 1)
@@ -45,6 +70,7 @@ def make_yolo_dataset(rgb=False):
             frame = cv2.imread(str(frame_path), 0)
 
         convert_to_yolo_supervisely(frame, annotation_data, frame_path.stem)
+    print(f"Discard {discard_count} out of {total_count}")
 
 
 def convert_to_yolo_supervisely(frame, annotation_data, image_name, w=80):
@@ -62,6 +88,8 @@ def convert_to_yolo_supervisely(frame, annotation_data, image_name, w=80):
     im_height, im_width = frame.shape[:2]
     with annotation_path.open("w") as f:
         for obj in annotation_data["objects"]:
+            if obj["classTitle"] not in ANNOTATION_CLASSES_TO_ID:
+                continue
             class_id = ANNOTATION_CLASSES_TO_ID[obj["classTitle"]]
             x = obj["points"]["exterior"][0][0]
             y = obj["points"]["exterior"][0][1]
