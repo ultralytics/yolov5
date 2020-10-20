e54303ca15c414e623d7521a48f6ec57f703e345
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-20 13:35:08 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -107,7 +107,6 @@ def detect(save_img=False):
                         line = (cls, conf, *xywh) if opt.save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line) + '\n') % line)
-                            
                     if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
