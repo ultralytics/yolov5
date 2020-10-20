a331187c5e682f40087b573003c93bea6c2425bc
leoyzhang <zhangyi970819@gmail.com>
2020-10-20 11:13:34 +0800

    fixed a bug of ghost conv

--- models/experimental.py
+++ models/experimental.py
@@ -67,8 +67,8 @@ class GhostConv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, g=1, act=True):  # ch_in, ch_out, kernel, stride, groups
         super(GhostConv, self).__init__()
         c_ = c2 // 2  # hidden channels
-        self.cv1 = Conv(c1, c_, k, s, g, act)
-        self.cv2 = Conv(c_, c_, 5, 1, c_, act)
+        self.cv1 = Conv(c1, c_, k, s, None, g, act)
+        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act)
 
     def forward(self, x):
         y = self.cv1(x)
