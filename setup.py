358ac9b28548b6b782d1b60fb21af7b6a0fb6297
vincent@nanovare.com <vincent@nanovare.com>
2020-10-22 19:53:24 +0200

    Make yolov5 be a package again // Create the setup.py without remote torch stable

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from pkg_resources import parse_requirements
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yolov5",
+    version="0.0.1",
+    author="nanovare",
+    author_email="vincent@nanovare.com",
+    description="Integrate yolov5 for mojo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robin-maillot/yolov5",
+    packages=setuptools.find_packages(include=['yolov5', 'yolov5.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_reqs = parse_requirements('requirements.txt')
+)
