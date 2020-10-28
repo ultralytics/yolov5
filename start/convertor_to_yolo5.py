bf4864ac6457af55198d456199cb3875223bd455
Andrey Tulyakov <mhyhre@gmail.com>
2020-10-28 12:19:35 +0300

    check image ext

--- start/convertor_to_yolo5.py
+++ start/convertor_to_yolo5.py
@@ -1,7 +1,6 @@
 import supervisely_lib as sly
 import os
 
-
 workspace_id = 23821
 project_id = 103287#103276#103162#103143#102425
 
@@ -16,11 +15,11 @@ meta = sly.ProjectMeta.from_json(meta_json)
 classe_names = [obj_class.name for obj_class in meta.obj_classes]
 number_of_classes = 'nc: {}\n'.format(len(classe_names))
 names = 'names: {}'.format(classe_names)
-with open(config_file_path, 'a') as f:
-    f.write(train_images)
-    f.write(val_images)
-    f.write(number_of_classes)
-    f.write(names)
+#with open(config_file_path, 'a') as f:
+#    f.write(train_images)
+#    f.write(val_images)
+#    f.write(number_of_classes)
+#    f.write(names)
 
 
 volo5_data_train = '/alex_data/sl/images/train'
@@ -37,7 +36,7 @@ for dataset in datasets:
         image_ids = [image_info.id for image_info in batch]
         image_names = [image_info.name for image_info in batch]
         images_dir_path = [os.path.join(volo5_data_train, im) for im in image_names]
-        api.image.download_paths(dataset.id, image_ids, images_dir_path)
+        #api.image.download_paths(dataset.id, image_ids, images_dir_path)
         ann_infos = api.annotation.download_batch(dataset.id, image_ids)
         for ann_info in ann_infos:
             ann = sly.Annotation.from_json(ann_info.annotation, meta)
@@ -50,7 +49,7 @@ for dataset in datasets:
                 width = round(rect_geometry.width / ann.img_size[1], 6)
                 height = round(rect_geometry.height / ann.img_size[0], 6)
                 res_obj_line = '{} {} {} {} {}\n'.format(class_number, x_center, y_center, width, height)
-                ann_path = os.path.join(volo5_ann_train, (ann_info.image_name[:-4] + '.txt')) #TODO check img ext
+                ann_path = os.path.join(volo5_ann_train, (ann_info.image_name.split('.')[0] + '.txt'))
                 with open(ann_path, 'a') as f:
                     f.write(res_obj_line)
 
