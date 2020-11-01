4178722762cf0e240d10bb58de732938f5a09c05
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-01 23:58:06 +0100

    freeze bug fix

--- train.py
+++ train.py
@@ -81,7 +81,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
         model = Model(opt.cfg, ch=3, nc=nc).to(device)  # create
 
     # Freeze
-    freeze = ['', ]  # parameter names to freeze (full or partial)
+    freeze = []  # parameter names to freeze (full or partial)
     for k, v in model.named_parameters():
         v.requires_grad = True  # train all layers
         if any(x in k for x in freeze):
