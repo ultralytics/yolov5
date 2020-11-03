d40036b541318aad86ce994fee2eedf5e81f147b
Robin <robin@nanovare.com>
2020-11-03 18:05:42 +0100

    Fix imports in package (VERY dirty)

--- setup.py
+++ setup.py
@@ -3,28 +3,59 @@ import pathlib
 import setuptools
 from pkg_resources import parse_requirements
 
-import glob
-import os
 import shutil
 
 MODULE_NAME = "yolov5"
 
 
 def copy_to_module():
-    if os.path.isdir(MODULE_NAME):
-        shutil.rmtree(MODULE_NAME)
+    current_path = pathlib.Path(".").resolve()
+    module_root_path = current_path / MODULE_NAME
+    module_root_path = module_root_path.relative_to(current_path)
 
-    for python_file_path in glob.glob("**/*.py", recursive=True):
-        if python_file_path == "setup.py":
+    if module_root_path.is_dir():
+        shutil.rmtree(module_root_path)
+
+    modules_moved = []
+    python_files = list(current_path.glob("**/*.py"))
+    for python_file_path in python_files:
+        python_file_path = python_file_path.relative_to(current_path)
+        if str(python_file_path) == "setup.py" or python_file_path.stem == "__init__":
             continue
+        new_path = module_root_path / python_file_path
+        modules_moved.append(".".join((list(map(str,python_file_path.parents))[::-1][1:] + [python_file_path.stem])))
+        if not new_path.parent.is_dir():
+            new_path.parent.mkdir(parents=True, exist_ok=True)
+            # make an init
+            init_file = new_path.parent / "__init__.py"
+            init_file.touch()
 
-        out_path = f"{MODULE_NAME}/{python_file_path}"
-        out_dir = os.path.dirname(out_path)
-        if not os.path.isdir(out_dir):
-            os.makedirs(out_dir, exist_ok=True)
-            with open(f"{out_dir}/__init__.py", 'a'):
-                pass
-        shutil.copyfile(python_file_path, out_path)
+        shutil.copyfile(python_file_path, new_path)
+
+
+    # Rename modules
+    strings_to_replace = {}
+    for module_name in modules_moved:
+        strings_to_replace[f"from {module_name} "] = f"from {MODULE_NAME}.{module_name} "
+        strings_to_replace[f"import {module_name}"] = f"import {MODULE_NAME}.{module_name} "
+    strings_to_search_for = tuple(strings_to_replace)
+    new_python_files = list(module_root_path.glob("**/*.py"))
+    lines_changed = []
+
+    for python_file_path in new_python_files:
+        data = []
+        with python_file_path.open("r") as f:
+            for line in f:
+                if line.startswith(strings_to_search_for):
+                    lines_changed.append(line)
+                    for key in strings_to_search_for:
+                        line = line.replace(key, strings_to_replace[key])
+                data.append(line)
+        with python_file_path.open("w") as f:
+            for line in data:
+                f.write(line)
+
+    print(f"Changed {len(lines_changed)} lines to accommodate for imports.")
 
 
 with open("README.md", "r") as fh:
@@ -37,8 +68,8 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
         in parse_requirements(requirements_txt)
     ]
 
-
 copy_to_module()
+exit()
 modules = setuptools.find_packages(include=['yolov5', 'yolov5.*'])
 
 setuptools.setup(
