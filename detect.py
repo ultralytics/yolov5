972cba4fc8f06be2cc3fbb7b2df7d0c78f248f53
Matt <49291478+mchirib1@users.noreply.github.com>
2020-11-07 16:07:49 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -106,7 +106,7 @@ def detect(save_img=False):
 
                     if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                        plot_one_box(xyxy, im0, label=None, color=colors[int(cls)], line_thickness=1)
 
             # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))
