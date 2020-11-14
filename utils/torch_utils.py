9f27902368b6c98e5483e9c1b55396c8a90c5501
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-14 14:48:55 +0100

    Improved FLOPS computation (#1398)

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -156,7 +156,7 @@ def model_info(model, verbose=False, img_size=640):
         flops = profile(deepcopy(model), inputs=(torch.zeros(1, 3, stride, stride),), verbose=False)[0] / 1E9 * 2
         img_size = img_size if isinstance(img_size, list) else [img_size, img_size]  # expand if int/float
         fs = ', %.1f GFLOPS' % (flops * img_size[0] / stride * img_size[1] / stride)  # 640x640 FLOPS
-    except ImportError:
+    except (ImportError, Exception):
         fs = ''
 
     logger.info(f"Model Summary: {len(list(model.modules()))} layers, {n_p} parameters, {n_g} gradients{fs}")
