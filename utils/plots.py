92c9b72832a878438f37f4cad223f63de0b55786
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-16 16:34:07 +0100

    Matplotlib AGG backend (#1413)

--- utils/plots.py
+++ utils/plots.py
@@ -19,6 +19,9 @@ from scipy.signal import butter, filtfilt
 from utils.general import xywh2xyxy, xyxy2xywh
 from utils.metrics import fitness
 
+# Settings
+matplotlib.use('Agg')  # for writing to files only
+
 
 def color_list():
     # Return first 10 plt colors as (r,g,b) https://stackoverflow.com/questions/51350872/python-from-color-name-to-rgb
