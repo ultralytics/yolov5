7816c0c2d3d12b9919aa8b9a25125714b00d5881
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-16 23:15:43 +0100

    PyTorch Hub and autoShape update (#1415)

--- hubconf.py
+++ hubconf.py
@@ -14,7 +14,7 @@ from models.yolo import Model
 from utils.general import set_logging
 from utils.google_utils import attempt_download
 
-dependencies = ['torch', 'PyYAML>=5.3', 'Pillow']
+dependencies = ['torch', 'yaml', 'Pillow']
 set_logging()
 
 
