81aa644582be1e4ade2562b2c8c30cb16f2d9149
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-18 10:02:39 +0100

    PosixPath to str webcam bug fix

--- detect.py
+++ detect.py
@@ -115,7 +115,7 @@ def detect(save_img=False):
 
             # Stream results
             if view_img:
-                cv2.imshow(p, im0)
+                cv2.imshow(str(p), im0)
                 if cv2.waitKey(1) == ord('q'):  # q to quit
                     raise StopIteration
 
