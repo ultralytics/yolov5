f98c33571e48874da4b50630c17ec75b089eac99
motive <motive@mathflat.com>
2020-11-19 10:17:05 +0900

    upload preprocessing code and train shell

---
+++ preprocessing.py
@@ -0,0 +1,27 @@
+import yaml
+from glob import glob
+from sklearn.model_selection import train_test_split
+
+img_list = glob("./data/export/images/*.jpg")
+print(len(img_list))
+
+train_img_list, val_img_list = train_test_split(img_list, test_size=0.2, random_state=2000)
+print(len(train_img_list), len(val_img_list))
+
+with open("./data/train.txt", 'w') as f:
+    f.write('\n'.join(train_img_list) + '\n')
+
+with open("./data/val.txt", 'w') as f:
+    f.write('\n'.join(val_img_list) + '\n')
+
+with open("./data/data.yaml", 'r') as f:
+    data = yaml.safe_load(f)
+    print(data)
+
+data['train'] = "./data/train.txt"
+data['val'] = "./data/val.txt"
+
+with open("./data/data.yaml", 'w') as f:
+    yaml.dump(data, f)
+    print(data)
+
---
+++ train.sh
@@ -0,0 +1,4 @@
+python train.py --img 416 --batch 16\
+                --epoch 50 --data ./data/data.yaml\
+                --cfg ./models/yolov5x.yaml --weight yolov5s.pt\
+                --name gun_yolov5s_results
