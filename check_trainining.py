c5f8ac39318baaa90c520452c87bcb38dce711cf
motive <motive@mathflat.com>
2020-11-27 11:54:10 +0900

    check training results.

---
+++ check_trainining.py
@@ -0,0 +1,3 @@
+from utils.plots import plot_results
+plot_results(save_dir='runs/train/graph_yolov5x_results')  # plot results.txt as results.png
+# plot_results(save_dir='runs/train/problem_yolov5x_results6')  # plot results.txt as results.png
