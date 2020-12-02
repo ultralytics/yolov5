22e8efa288a15778c3a4e2841b15468ccf89b00b
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-02 13:52:06 +0100

    Update matplotlib svg backend

--- utils/plots.py
+++ utils/plots.py
@@ -21,7 +21,7 @@ from utils.metrics import fitness
 
 # Settings
 matplotlib.rc('font', **{'size': 11})
-matplotlib.use('Agg')  # for writing to files only
+matplotlib.use('svg')  # for writing to files only
 
 
 def color_list():
