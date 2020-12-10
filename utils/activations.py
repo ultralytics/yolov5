135a31a3e5b17ebf5350b94c4f1f7dc3b5d5209d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-10 13:04:40 -0800

    FReLU bias=False bug fix

--- utils/activations.py
+++ utils/activations.py
@@ -65,7 +65,7 @@ class MemoryEfficientMish(nn.Module):
 class FReLU(nn.Module):
     def __init__(self, c1, k=3):  # ch_in, kernel
         super().__init__()
-        self.conv = nn.Conv2d(c1, c1, k, 1, 1, groups=c1)
+        self.conv = nn.Conv2d(c1, c1, k, 1, 1, groups=c1, bias=False)
         self.bn = nn.BatchNorm2d(c1)
 
     def forward(self, x):
