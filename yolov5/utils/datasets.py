c168d53e00032ce8c67d5a7b7e497aa55adea53d
Baptiste Amato <baptiste.amato@gmail.com>
2020-12-14 17:02:27 +0100

    Fix.

--- yolov5/utils/datasets.py
+++ yolov5/utils/datasets.py
@@ -323,7 +323,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.label_files = [
             f"{x}.txt"
             if len(x.split(".")) == 1  # If there's no image extension
-            else x.replace('images', 'annotations').replace(x.split(".")[-1], 'txt')  # If there's an image extension
+            else x.replace(x.split(".")[-1], 'txt')  # If there's an image extension
             for x in self.img_files
         ]
 
