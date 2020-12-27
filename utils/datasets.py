00d4f848d0a4b91d5b04f24f36f50784a5b35366
Rohan Dubey <48961845+rohandubey@users.noreply.github.com>
2020-12-27 15:22:12 +0530

    Update datasets.py (cap -> self.cap)

--- utils/datasets.py
+++ utils/datasets.py
@@ -271,13 +271,13 @@ class LoadStreams:  # multiple IP or RTSP cameras
         for i, s in enumerate(sources):
             # Start the thread to read frames from the video stream
             print('%g/%g: %s... ' % (i + 1, n, s), end='')
-            cap = cv2.VideoCapture(eval(s) if s.isnumeric() else s)
-            assert cap.isOpened(), 'Failed to open %s' % s
-            w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            fps = cap.get(cv2.CAP_PROP_FPS) % 100
-            _, self.imgs[i] = cap.read()  # guarantee first frame
-            thread = Thread(target=self.update, args=([i, cap]), daemon=True)
+            self.cap = cv2.VideoCapture(eval(s) if s.isnumeric() else s)
+            assert self.cap.isOpened(), 'Failed to open %s' % s
+            w = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            h = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = self.cap.get(cv2.CAP_PROP_FPS) % 100
+            _, self.imgs[i] = self.cap.read()  # guarantee first frame
+            thread = Thread(target=self.update, args=([i, self.cap]), daemon=True)
             print(' success (%gx%g at %.2f FPS).' % (w, h, fps))
             thread.start()
         print('')  # newline
@@ -291,12 +291,12 @@ class LoadStreams:  # multiple IP or RTSP cameras
     def update(self, index, cap):
         # Read next stream frame in a daemon thread
         n = 0
-        while cap.isOpened():
+        while self.cap.isOpened():
             n += 1
-            # _, self.imgs[index] = cap.read()
-            cap.grab()
+            # _, self.imgs[index] = self.cap.read()
+            self.cap.grab()
             if n == 4:  # read every 4th frame
-                _, self.imgs[index] = cap.retrieve()
+                _, self.imgs[index] = self.cap.retrieve()
                 n = 0
             time.sleep(0.01)  # wait time
 
@@ -308,6 +308,7 @@ class LoadStreams:  # multiple IP or RTSP cameras
         self.count += 1
         img0 = self.imgs.copy()
         if cv2.waitKey(1) == ord('q'):  # q to quit
+            self.cap.release()
             cv2.destroyAllWindows()
             raise StopIteration
 
