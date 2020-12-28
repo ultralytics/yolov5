14b820297c941a728221483b744f584ecc87ea66
Baptiste Amato <baptiste.amato@gmail.com>
2020-12-28 12:52:31 +0100

    Add data to installed packages.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='yolov5',
       author="Psycle Research",
       description="Fork of yolov5",
       url="https://github.com/PsycleResearch/yolov5",
-      packages=['yolov5', 'yolov5.models', 'yolov5.utils'],
+      packages=['yolov5', 'yolov5.models', 'yolov5.utils', 'yolov5.data'],
       python_requires='>=3.6'
       )
