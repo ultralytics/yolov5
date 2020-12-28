4f8f4459556d7779b7db346c6e066be55830aebb
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-27 21:24:14 -0800

    update

--- train.py
+++ train.py
@@ -290,6 +290,8 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                 loss, loss_items = compute_loss(pred, targets.to(device), model)  # loss scaled by batch_size
                 if rank != -1:
                     loss *= opt.world_size  # gradient averaged between devices in DDP mode
+                if opt.quad:
+                    loss *= 4.
 
             # Backward
             scaler.scale(loss).backward()
