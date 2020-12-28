9cb3094f53e8cb5349a27638beae6d3542527b49
Arunavameister <arunavameister@yahoo.com>
2020-12-28 11:16:32 +0100

    Add weights to fitness function.

--- yolov5/utils/general.py
+++ yolov5/utils/general.py
@@ -786,9 +786,11 @@ def apply_classifier(x, model, img, im0):
     return x
 
 
-def fitness(x):
+def fitness(x, weights=None):
     # Returns fitness (for use with results.txt or evolve.txt)
     w = [0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
+    if weights is not None:
+        w = weights
     return (x[:, :4] * w).sum(1)
 
 
