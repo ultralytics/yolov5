ef511898efee05a1e7709688dbca22a40426736f
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-30 15:13:39 -0800

    update

--- models/common.py
+++ models/common.py
@@ -105,20 +105,11 @@ class Focus(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super(Focus, self).__init__()
         self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
+        # self.contract = Contract(gain=2)
 
     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
         return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
-
-
-# class Focus(nn.Module):
-#     # Focus wh information into c-space
-#     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-#         super(Focus, self).__init__()
-#         self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
-#         self.contract = Contract(2)
-#
-#     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
-#         return self.conv(self.contract(x))
+        # return self.conv(self.contract(x))
 
 
 class Contract(nn.Module):
