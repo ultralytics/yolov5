808ef9770f69e0a5c6ffa0008c5e982d418b4fd1
asears <asears@users.noreply.github.com>
2021-01-09 20:08:13 -0500

    Update documentation
    Signed-off-by: asears <asears@users.noreply.github.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1013,6 +1013,7 @@ def random_perspective(img, targets=(), degrees=10, translate=.1, scale=.1, shea
     R = np.eye(3)
     a = random.uniform(-degrees, degrees)
     # a += random.choice([-180, -90, 0, 90])  # add 90deg rotations to small rotations
+    # https://github.com/ultralytics/yolov5/issues/1798
     s = random.uniform(1 - scale, 1 + scale)
     # s = 2 ** random.uniform(-scale, scale)
     R[:2] = cv2.getRotationMatrix2D(angle=a, center=(0, 0), scale=s)
