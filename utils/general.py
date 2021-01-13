09804f7820448843304e7a1ef70cb88798ab7202
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-12 21:34:47 -0800

    comment

--- utils/general.py
+++ utils/general.py
@@ -57,7 +57,7 @@ def check_online():
 
 
 def check_git_status():
-    # Suggest 'git pull' if repo is out of date
+    # Suggest 'git pull' if code is out of date
     print(colorstr('github: '), end='')
     try:
         if Path('.git').exists() and check_online():
