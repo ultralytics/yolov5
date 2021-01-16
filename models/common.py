e02d1a88348d15a66e5f6d8072b2d6d6e245854b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-16 13:25:03 -0800

    update

--- models/common.py
+++ models/common.py
@@ -109,7 +109,8 @@ class Focus(nn.Module):
         # self.contract = Contract(gain=2)
 
     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
-        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
+        x = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+        return self.conv(x)
         # return self.conv(self.contract(x))
 
 
