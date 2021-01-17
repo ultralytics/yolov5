e56aedcccf6e8729cd32b4c4567677e34ad09a9d
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-01-17 18:54:00 +0200

    init gui

--- sly_train.py
+++ sly_train.py
@@ -1,28 +1,24 @@
+import os
+import sys
 import supervisely_lib as sly
 
-import sys
-sys.argv.append('--weights')
-sys.argv.append("maxim")
 
-import train
+#sys.argv.append('--weights')
+#sys.argv.append("maxim")
+#import train
+#train.main()
 
-train.main()
+my_app = sly.AppService()
 
-x = 10
-x += 1
 
-# my_app = sly.AppService()
-#
-# TEAM_ID = os.environ["context.teamId"]
-# WORKSPACE_ID = os.environ["context.workspaceId"]
-# INPUT_DIR = os.environ.get("modal.state.slyFolder")
-# INPUT_FILE = os.environ.get("modal.state.slyFile")
+TEAM_ID = os.environ["context.teamId"]
+WORKSPACE_ID = os.environ["context.workspaceId"]
 
 
 #@my_app.callback("yolov5_sly_converter")
 #@sly.timeit
-def yolov5_sly_converter(api: sly.Api, task_id, context, state, app_logger):
-    pass
+# def yolov5_sly_converter(api: sly.Api, task_id, context, state, app_logger):
+#     pass
 
 
 def main():
@@ -34,8 +30,12 @@ def main():
     #     "CONFIG_DIR": os.environ.get("CONFIG_DIR", None)
     # })
 
+    data = {}
+    state = {}
+
     #my_app.run(initial_events=[{"command": "yolov5_sly_converter"}])
-    pass
+    template_path = os.path.join(os.path.dirname(sys.argv[0]), 'supervisely/train/src/gui.html')
+    my_app.run(template_path, data, state)
 
 
 if __name__ == "__main__":
