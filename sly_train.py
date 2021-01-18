4444676a2551a16c1ebf801ebd9389bfb0fcceb7
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-01-18 09:00:37 +0200

    custom classes table

--- sly_train.py
+++ sly_train.py
@@ -11,14 +11,26 @@ import supervisely_lib as sly
 my_app = sly.AppService()
 
 
-TEAM_ID = os.environ["context.teamId"]
-WORKSPACE_ID = os.environ["context.workspaceId"]
+TEAM_ID = int(os.environ['context.teamId'])
+WORKSPACE_ID = int(os.environ['context.workspaceId'])
+PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 
+PROJECT = None
+META = None
 
 #@my_app.callback("yolov5_sly_converter")
 #@sly.timeit
 # def yolov5_sly_converter(api: sly.Api, task_id, context, state, app_logger):
 #     pass
+
+
+def init(api: sly.Api, data, state):
+    global PROJECT, META
+    PROJECT = api.project.get_info_by_id(PROJECT_ID)
+    META = sly.ProjectMeta.from_json(api.project.get_meta(PROJECT_ID))
+
+    data["classes"] = META.obj_classes.to_json()
+    state["classes"] = len(META.obj_classes) * [True]
 
 
 def main():
@@ -31,7 +43,10 @@ def main():
     # })
 
     data = {}
+
     state = {}
+    state["classes"] = []
+    init(my_app.public_api, data, state)
 
     #my_app.run(initial_events=[{"command": "yolov5_sly_converter"}])
     template_path = os.path.join(os.path.dirname(sys.argv[0]), 'supervisely/train/src/gui.html')
