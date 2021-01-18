35400dc7b730b7816051b9be3feca36ccf5a694b
Abhiram V <61599526+Anon-Artist@users.noreply.github.com>
2021-01-19 00:16:20 +0530

    Update plots.py with yaml.SafeLoader (#1968)

--- utils/plots.py
+++ utils/plots.py
@@ -301,7 +301,7 @@ def plot_labels(labels, save_dir=Path(''), loggers=None):
 def plot_evolution(yaml_file='data/hyp.finetune.yaml'):  # from utils.plots import *; plot_evolution()
     # Plot hyperparameter evolution results in evolve.txt
     with open(yaml_file) as f:
-        hyp = yaml.load(f, Loader=yaml.FullLoader)
+        hyp = yaml.load(f, Loader=yaml.SafeLoader)
     x = np.loadtxt('evolve.txt', ndmin=2)
     f = fitness(x)
     # weights = (f - f.min()) ** 2  # for weighted results
