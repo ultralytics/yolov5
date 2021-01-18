aef1ed9ebe55f087fb1f49175295301b9ddb5476
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-01-18 10:05:51 +0200

    training classes

--- sly_train.py
+++ sly_train.py
@@ -24,13 +24,28 @@ META = None
 #     pass
 
 
-def init(api: sly.Api, data, state):
+def init_classes_stats(api: sly.Api, data, state):
     global PROJECT, META
     PROJECT = api.project.get_info_by_id(PROJECT_ID)
     META = sly.ProjectMeta.from_json(api.project.get_meta(PROJECT_ID))
 
-    data["classes"] = META.obj_classes.to_json()
-    state["classes"] = len(META.obj_classes) * [True]
+    stats = api.project.get_stats(PROJECT_ID)
+    class_images = {}
+    for item in stats["images"]["objectClasses"]:
+        class_images[item["objectClass"]["name"]] = item["total"]
+    class_objects = {}
+    for item in stats["objects"]["items"]:
+        class_objects[item["objectClass"]["name"]] = item["total"]
+
+    classes_json = META.obj_classes.to_json()
+    for obj_class in classes_json:
+        obj_class["imagesCount"] = class_images[obj_class["title"]]
+        obj_class["objectsCount"] = class_objects[obj_class["title"]]
+
+    data["classes"] = classes_json
+    state["selectedClasses"] = []
+
+    state["classes"] = len(classes_json) * [True]
 
 
 def main():
@@ -43,10 +58,9 @@ def main():
     # })
 
     data = {}
-
     state = {}
-    state["classes"] = []
-    init(my_app.public_api, data, state)
+
+    init_classes_stats(my_app.public_api, data, state)
 
     #my_app.run(initial_events=[{"command": "yolov5_sly_converter"}])
     template_path = os.path.join(os.path.dirname(sys.argv[0]), 'supervisely/train/src/gui.html')
