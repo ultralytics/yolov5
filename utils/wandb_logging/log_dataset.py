0e219896eedef7673b2b7417f6edb8d0304ba7b1
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-23 13:53:57 -0800

    log_dataset.py cleanup

--- utils/wandb_logging/log_dataset.py
+++ utils/wandb_logging/log_dataset.py
@@ -11,22 +11,21 @@ WANDB_ARTIFACT_PREFIX = 'wandb-artifact://'
 
 def create_dataset_artifact(opt):
     with open(opt.data) as f:
-        data_dict = yaml.load(f, Loader=yaml.SafeLoader)  # data dict
-    wandb_logger = WandbLogger(opt, '', None, data_dict, job_type='create_dataset')
-    nc, names = (1, ['item']) if opt.single_cls else (int(data_dict['nc']), data_dict['names'])
-    trainset = LoadImagesAndLabels(data_dict['train'])
-    testset = LoadImagesAndLabels(data_dict['val'])
+        data = yaml.load(f, Loader=yaml.SafeLoader)  # data dict
+    logger = WandbLogger(opt, '', None, data, job_type='create_dataset')
+    nc, names = (1, ['item']) if opt.single_cls else (int(data['nc']), data['names'])
     names_to_ids = {k: v for k, v in enumerate(names)}
-    wandb_logger.log_dataset_artifact(trainset, names_to_ids, name='train')
-    wandb_logger.log_dataset_artifact(testset, names_to_ids, name='val')
-    # Update/Create new config file with links to artifact
-    data_dict['train'] = WANDB_ARTIFACT_PREFIX + str(Path(opt.project) / 'train')
-    data_dict['val'] = WANDB_ARTIFACT_PREFIX + str(Path(opt.project) / 'val')
-    ouput_data_config = opt.data if opt.overwrite_config else opt.data.replace('.', '_wandb.')
-    data_dict.pop('download', None)  # Don't download the original dataset. Use artifacts
-    with open(ouput_data_config, 'w') as fp:
-        yaml.dump(data_dict, fp)
-    print("New Config file => ", ouput_data_config)
+    logger.log_dataset_artifact(LoadImagesAndLabels(data['train']), names_to_ids, name='train')  # trainset
+    logger.log_dataset_artifact(LoadImagesAndLabels(data['val']), names_to_ids, name='val')  # valset
+
+    # Update data.yaml with artifact links
+    data['train'] = WANDB_ARTIFACT_PREFIX + str(Path(opt.project) / 'train')
+    data['val'] = WANDB_ARTIFACT_PREFIX + str(Path(opt.project) / 'val')
+    path = opt.data if opt.overwrite_config else opt.data.replace('.', '_wandb.')  # updated data.yaml path
+    data.pop('download', None)  # download via artifact instead of predefined field 'download:'
+    with open(path, 'w') as f:
+        yaml.dump(data, f)
+    print("New Config file => ", path)
 
 
 if __name__ == '__main__':
@@ -34,7 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
     parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
     parser.add_argument('--project', type=str, default='yolov5', help='name of W&B Project')
-    parser.add_argument('--overwrite_config', action='store_true', help='replace the origin data config file')
+    parser.add_argument('--overwrite_config', action='store_true', help='overwrite data.yaml')
     opt = parser.parse_args()
 
     create_dataset_artifact(opt)
