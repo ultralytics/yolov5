a18654a6f0c96996394d0791f5906aef9a776572
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-26 21:44:09 -0800

    update

--- utils/loss.py
+++ utils/loss.py
@@ -106,7 +106,8 @@ class ComputeLoss:
 
         det = model.module.model[-1] if is_parallel(model) else model.model[-1]  # Detect() module
         # self.balance = {3: [3.67, 1.0, 0.43], 4: [3.78, 1.0, 0.39, 0.22], 5: [3.88, 1.0, 0.37, 0.17, 0.10]}[det.nl]
-        self.balance = {3: [3.67, 1.0, 0.43], 4: [4.0, 1.0, 0.25, 0.06], 5: [3.88, 1.0, 0.37, 0.17, 0.10]}[det.nl]
+        # self.balance = {3: [3.67, 1.0, 0.43], 4: [4.0, 1.0, 0.25, 0.06], 5: [3.88, 1.0, 0.37, 0.17, 0.10]}[det.nl]
+        self.balance = {3: [3.67, 1.0, 0.43], 4: [4.0, 1.0, 0.4, 0.1], 5: [3.88, 1.0, 0.37, 0.17, 0.10]}[det.nl]
         # self.balance = [1.0] * det.nl
         self.ssi = (det.stride == 16).nonzero(as_tuple=False).item()  # stride 16 index
         self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = BCEcls, BCEobj, model.gr, h, autobalance
