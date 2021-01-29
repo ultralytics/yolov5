2acbe9699a51a0efd8ae0dbfdd775c20a628f362
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-28 18:57:24 -0800

    W&B epoch logging update (#2073)

--- train.py
+++ train.py
@@ -364,7 +364,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                 if tb_writer:
                     tb_writer.add_scalar(tag, x, epoch)  # tensorboard
                 if wandb:
-                    wandb.log({tag: x})  # W&B
+                    wandb.log({tag: x}, step=epoch, commit=tag == tags[-1])  # W&B
 
             # Update best mAP
             fi = fitness(np.array(results).reshape(1, -1))  # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
