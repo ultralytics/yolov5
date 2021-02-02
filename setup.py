fde2f021e4d300ca69c188216ca1b452893413f7
zaruhi.alaverdyan@traxretail.com <zaruhi.alaverdyan@traxretail.com>
2021-02-02 12:03:46 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    README = readme_file.read()
+#with open('README.rst') as readme_file:
+#    README = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    HISTORY = history_file.read()
+#with open('HISTORY.rst') as history_file:
+#    HISTORY = history_file.read()
 
 REQUIREMENTS = [
     'gitpython',
@@ -21,7 +21,7 @@ setup(
     name='yolov5',
     version='0',
     description="Downloads or clones a python project from github and allows to import it from anywhere. Very useful when the repo is not a package",
-    long_description=README + '\n\n' + HISTORY,
+    long_description='yolo5 package',
     author="zara",
     author_email='',
     url='https://github.com/zaraalaverdyan-planorama/yolov5',
