9e5e9be5157968e0bce5bb2b5086d22cf8c184d2
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-05 11:01:45 -0800

    Update log_dataset.py

--- utils/wandb_logging/log_dataset.py
+++ utils/wandb_logging/log_dataset.py
@@ -15,8 +15,8 @@ def create_dataset_artifact(opt):
     logger = WandbLogger(opt, '', None, data, job_type='create_dataset')
     nc, names = (1, ['item']) if opt.single_cls else (int(data['nc']), data['names'])
     names = {k: v for k, v in enumerate(names)}  # to index dictionary
-    logger.log_dataset_artifact(LoadImagesAndLabels(data['train'], data_type="train"), names, name='train')  # trainset
-    logger.log_dataset_artifact(LoadImagesAndLabels(data['val'], data_type="val"), names, name='val')  # valset
+    logger.log_dataset_artifact(LoadImagesAndLabels(data['train']), names, name='train')  # trainset
+    logger.log_dataset_artifact(LoadImagesAndLabels(data['val']), names, name='val')  # valset
 
     # Update data.yaml with artifact links
     data['train'] = WANDB_ARTIFACT_PREFIX + str(Path(opt.project) / 'train')
