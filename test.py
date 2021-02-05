1aff06a3b3abdfc863d7a210546aab431acb9d7d
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-02-05 13:38:47 +0200

    test import sly_utils

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ from utils.metrics import ap_per_class, ConfusionMatrix
 from utils.plots import plot_images, output_to_target, plot_study_txt
 from utils.torch_utils import select_device, time_synchronized
 
-from sly_train_utils import upload_pred_vis
+from supervisely.train.src.sly_train_utils import upload_pred_vis
 
 
 def test(data,
