e9b3de463ad7734d41c059a4a513472ed618b7a1
train255 <thanhdd.it@gmail.com>
2021-02-06 02:12:43 +0700

    Unique *.cache filenames fix (#2134)
    * fix #2121
    
    * Update test.py
    
    * Update train.py
    
    * Update autoanchor.py
    
    * Update datasets.py
    
    * Update log_dataset.py
    
    * Update datasets.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -372,7 +372,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Check cache
         self.label_files = img2label_paths(self.img_files)  # labels
-        cache_path = Path(self.label_files[0]).parent.with_suffix('.cache')  # cached labels
+        cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')  # cached labels
         if cache_path.is_file():
             cache = torch.load(cache_path)  # load
             if cache['hash'] != get_hash(self.label_files + self.img_files) or 'results' not in cache:  # changed
