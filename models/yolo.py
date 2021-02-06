4fa932698b6a3b5e2acc914c86a869a352f512fe
Yiwei Ding <dingyiwei999@foxmail.com>
2021-02-06 21:04:09 +0800

    yolotr

--- models/yolo.py
+++ models/yolo.py
@@ -210,7 +210,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 pass
 
         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, C3]:
+        if m in [Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, C3, C3T]:
             c1, c2 = ch[f], args[0]
 
             # Normal
