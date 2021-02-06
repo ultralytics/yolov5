c4aa85c90666bf565f776391f5392c1f35e19be4
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-06 11:26:22 -0800

    Update train.py

--- train.py
+++ train.py
@@ -467,7 +467,7 @@ if __name__ == '__main__':
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--quad', action='store_true', help='quad dataloader')
-     parser.add_argument('--linear-lr', action='store_true', help='linear LR')
+    parser.add_argument('--linear-lr', action='store_true', help='linear LR')
     opt = parser.parse_args()
 
     # Set DDP variables
