a5359f6c3288c0549193681632c8e9b324e81017
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-06 22:13:39 -0800

    Update data-autodownload background tasks (#2154)
    * Update get_coco.sh
    
    * Update get_voc.sh

--- data/scripts/get_coco.sh
+++ data/scripts/get_coco.sh
@@ -10,8 +10,9 @@
 # Download/unzip labels
 d='../' # unzip directory
 url=https://github.com/ultralytics/yolov5/releases/download/v1.0/
-f='coco2017labels.zip'                                                                 # 68 MB
-echo 'Downloading' $url$f ' ...' && curl -L $url$f -o $f && unzip -q $f -d $d && rm $f # download, unzip, remove
+f='coco2017labels.zip' # 68 MB
+echo 'Downloading' $url$f ' ...'
+curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
 
 # Download/unzip images
 d='../coco/images' # unzip directory
@@ -20,7 +21,7 @@ f1='train2017.zip' # 19G, 118k images
 f2='val2017.zip'   # 1G, 5k images
 f3='test2017.zip'  # 7G, 41k images (optional)
 for f in $f1 $f2; do
-  echo 'Downloading' $url$f '...' && curl -L $url$f -o $f # download, (unzip, remove in background)
-  unzip -q $f -d $d && rm $f &
+  echo 'Downloading' $url$f '...'
+  curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
 done
 wait # finish background tasks
