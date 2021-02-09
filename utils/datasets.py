27bc6b0cb3ef289d1082206115cd7ae4e13923f7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-08 23:28:33 -0800

    updates

--- utils/datasets.py
+++ utils/datasets.py
@@ -470,7 +470,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
                         assert l.shape[1] == 5, 'labels require 5 columns each'
                         assert (l >= 0).all(), 'negative labels'
                         assert (l[:, 1:] <= 1).all(), 'non-normalized or out of bounds coordinate labels'
-                        assert np.unique(l, axis=0).shape[0] == l.shape[0], 'duplicate labels'
+                        # assert np.unique(l, axis=0).shape[0] == l.shape[0], 'duplicate labels'
                     else:
                         ne += 1  # label empty
                         l = np.zeros((0, 5), dtype=np.float32)
