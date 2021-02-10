48c945fe9960af8c687f55680c571c1b5837000b
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-02-10 15:40:35 +0200

    fix charts

--- supervisely/train/src/sly_metrics.py
+++ supervisely/train/src/sly_metrics.py
@@ -2,14 +2,22 @@ import supervisely_lib as sly
 import sly_train_globals as globals
 
 
-def init_chart(title, names, xs, ys):
+def init_chart(title, names, xs, ys, smoothing=None):
     series = []
     for name, x, y in zip(names, xs, ys):
         series.append({
             "name": name,
             "data": [[px, py] for px, py in zip(x, y)]
         })
-    return series
+    result = {
+        "options": {
+            "title": title,
+        },
+        "series": series
+    }
+    if smoothing is not None:
+        result["options"]["smoothingWeight"] = smoothing
+    return result
 
 
 def init(data):
@@ -18,19 +26,22 @@ def init(data):
     data["mBox"] = init_chart("Box Loss",
                               names=["train", "val"],
                               xs=demo_x,
-                              ys=demo_y)
+                              ys=demo_y,
+                              smoothing=0.6)
 
-    data["mObjectness"] = init_chart("Objectness Loss",
+    data["mObjectness"] = init_chart("Obj Loss",
                                      names=["train", "val"],
                                      xs=demo_x,
-                                     ys=demo_y)
+                                     ys=demo_y,
+                                     smoothing=0.6)
 
-    data["mClassification"] = init_chart("Classification Loss",
+    data["mClassification"] = init_chart("Cls Loss",
                                          names=["train", "val"],
                                          xs=demo_x,
-                                         ys=demo_y)
+                                         ys=demo_y,
+                                         smoothing=0.6)
 
-    data["mPR"] = init_chart("Precision / Recall",
+    data["mPR"] = init_chart("Pr + Rec",
                              names=["precision", "recall"],
                              xs=demo_x,
                              ys=demo_y)
@@ -39,28 +50,25 @@ def init(data):
                               names=["mAP@0.5", "mAP@0.5:0.95"],
                               xs=demo_x,
                               ys=demo_y)
-    data["lcOptions"] = {
-        "smoothingWeight": 0.6
-    }
 
 
 def send_metrics(epoch, epochs, metrics):
     sly.logger.debug(f"Metrics: epoch {epoch} / {epochs}", extra={"metrics": metrics})
 
     fields = [
-        {"field": "data.mBox[0].data", "payload": [[epoch, metrics["train/box_loss"]]], "append": True},
-        {"field": "data.mBox[1].data", "payload": [[epoch, metrics["val/box_loss"]]], "append": True},
+        {"field": "data.mBox.series[0].data", "payload": [[epoch, metrics["train/box_loss"]]], "append": True},
+        {"field": "data.mBox.series[1].data", "payload": [[epoch, metrics["val/box_loss"]]], "append": True},
 
-        {"field": "data.mObjectness[0].data", "payload": [[epoch, metrics["train/obj_loss"]]], "append": True},
-        {"field": "data.mObjectness[1].data", "payload": [[epoch, metrics["val/obj_loss"]]], "append": True},
+        {"field": "data.mObjectness.series[0].data", "payload": [[epoch, metrics["train/obj_loss"]]], "append": True},
+        {"field": "data.mObjectness.series[1].data", "payload": [[epoch, metrics["val/obj_loss"]]], "append": True},
 
-        {"field": "data.mClassification[0].data", "payload": [[epoch, metrics["train/cls_loss"]]], "append": True},
-        {"field": "data.mClassification[1].data", "payload": [[epoch, metrics["val/cls_loss"]]], "append": True},
+        {"field": "data.mClassification.series[0].data", "payload": [[epoch, metrics["train/cls_loss"]]], "append": True},
+        {"field": "data.mClassification.series[1].data", "payload": [[epoch, metrics["val/cls_loss"]]], "append": True},
 
-        {"field": "data.mPR[0].data", "payload": [[epoch, metrics["metrics/precision"]]], "append": True},
-        {"field": "data.mPR[1].data", "payload": [[epoch, metrics["metrics/recall"]]], "append": True},
+        {"field": "data.mPR.series[0].data", "payload": [[epoch, metrics["metrics/precision"]]], "append": True},
+        {"field": "data.mPR.series[1].data", "payload": [[epoch, metrics["metrics/recall"]]], "append": True},
 
-        {"field": "data.mMAP[0].data", "payload": [[epoch, metrics["metrics/mAP_0.5"]]], "append": True},
-        {"field": "data.mMAP[1].data", "payload": [[epoch, metrics["metrics/mAP_0.5:0.95"]]], "append": True},
+        {"field": "data.mMAP.series[0].data", "payload": [[epoch, metrics["metrics/mAP_0.5"]]], "append": True},
+        {"field": "data.mMAP.series[1].data", "payload": [[epoch, metrics["metrics/mAP_0.5:0.95"]]], "append": True},
     ]
     globals.api.app.set_fields(globals.task_id, fields)
