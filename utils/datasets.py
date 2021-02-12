ffe8a01339014e51c459633b8497ecdb5b9768ec
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-11 19:43:38 -0800

    update

--- utils/datasets.py
+++ utils/datasets.py
@@ -389,6 +389,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Read cache
         cache.pop('hash')  # remove hash
+        cache.pop('segments')  # remove segments boolean
         labels, shapes, self.segments = zip(*cache.values())
         self.labels = list(labels)
         self.shapes = np.array(shapes, dtype=np.float64)
