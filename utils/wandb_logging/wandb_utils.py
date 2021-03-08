143848333b4e195f42ade93291879bc957dd0fb4
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-03-08 22:27:56 +0530

    FIX:resume

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -138,8 +138,7 @@ class WandbLogger():
 
     def download_model_artifact(self, opt):
         if opt.resume.startswith(WANDB_ARTIFACT_PREFIX):
-            model_artifact_name = 'run_' + Path(opt.resume).stem + '_model'
-            model_artifact = wandb.use_artifact(model_artifact_name + ":latest")
+            model_artifact = wandb.use_artifact(remove_prefix(opt.resume, WANDB_ARTIFACT_PREFIX) + ":latest")
             assert model_artifact is not None, 'Error: W&B model artifact doesn\'t exist'
             modeldir = model_artifact.download()
             epochs_trained = model_artifact.metadata.get('epochs_trained')
