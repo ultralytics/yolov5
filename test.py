149a03828ce09145360d2ffd345a6fd0cc33b9be
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-12 13:17:33 -0800

    Integer printout

--- test.py
+++ test.py
@@ -223,7 +223,7 @@ def test(data,
         nt = torch.zeros(1)
 
     # Print results
-    pf = '%20s' + '%12.3g' * 6  # print format
+    pf = '%20s' + '%12i' * 2 + '%12.3g' * 4  # print format
     print(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
 
     # Print results per class
