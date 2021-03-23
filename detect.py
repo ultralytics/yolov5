871d6ea6f31664dbf60b144da46b6fbd3059a5a6
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-23 14:17:34 +0100

    check_requirements() exclude pycocotools, thop
    Exclude non-critical packages from dependency checks in detect.py. pycocotools and thop in particular are not required for inference.
    
    Issue first raised in https://github.com/ultralytics/yolov5/issues/1944 and also raised in https://github.com/ultralytics/yolov5/discussions/2556

--- detect.py
+++ detect.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     opt = parser.parse_args()
     print(opt)
-    check_requirements()
+    check_requirements(exclude=('pycocotools', 'thop'))
 
     with torch.no_grad():
         if opt.update:  # update all models (to fix SourceChangeWarning)
