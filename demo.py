70ffb981c7fd796888aedf9f0c2cd3b08839f9de
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-28 13:53:21 +0200

    Update demo.py
    PEP8, remove unused results.img line (has no effect), and updated article and description.

--- demo.py
+++ demo.py
@@ -1,36 +1,39 @@
-import torch
-from PIL import Image
-from torchvision import transforms
-import gradio as gr
-from io import BytesIO
-import base64
 import os
 
+import gradio as gr
+import torch
+from PIL import Image
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
+
 def yolo(img):
-  img.save("test_image_hack.png")
-  img = Image.open("test_image_hack.png")
-  results = model(img)  # inference
-  os.remove("test_image_hack.png")
+    img.save("test_image_hack.png")
+    img = Image.open("test_image_hack.png")
+    results = model(img)  # inference
+    os.remove("test_image_hack.png")
 
-  results.imgs # array of original images (as np array) passed to model for inference
-  results.render()  # updates results.imgs with boxes and labels
-  for img in results.imgs:
-    return Image.fromarray(img)
+    results.render()  # updates results.imgs with boxes and labels
+    for img in results.imgs:
+        return Image.fromarray(img)
 
 
-inputs =  gr.inputs.Image(type='pil', label="Original Image")
-outputs = gr.outputs.Image(type="pil",label="Output Image")
+inputs = gr.inputs.Image(type='pil', label="Original Image")
+outputs = gr.outputs.Image(type="pil", label="Output Image")
 
 title = "YOLOv5"
-description = "demo for YOLOv5 which takes in a single image for object detection. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
-article = "<a href='https://github.com/ultralytics/yolov5'>Github Repo</a></p>"
+description = "YOLOv5 demo for object detection. Upload an image or click an example image to use. " \
+              "Source code: https://github.com/ultralytics/yolov5"
+article = "YOLOv5 is a family of compound-scaled object detection models trained on the COCO dataset, and includes " \
+          "simple functionality for Test Time Augmentation (TTA), model ensembling, hyperparameter evolution, " \
+          "and export to ONNX, CoreML and TFLite. Source code: https://github.com/ultralytics/yolov5, " \
+          "iOS App: https://apps.apple.com/app/id1452689527, PyTorch Hub: https://pytorch.org/hub/ultralytics_yolov5"
 
 examples = [
     ['bird.jpg'],
     ['fox.jpg']
 ]
-gr.Interface(yolo, inputs, outputs, title=title, description=description, article=article, examples=examples).launch(debug=True)
+
+gr.Interface(yolo, inputs, outputs, title=title, description=description, article=article, examples=examples).launch(
+    debug=True)
