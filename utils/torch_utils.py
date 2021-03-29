2469c74123d6bf0429ac4dfd765d2480a0a241fe
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-29 14:24:55 +0200

    Speed profiling improvements

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -216,7 +216,7 @@ def model_info(model, verbose=False, img_size=640):
         from thop import profile
         stride = max(int(model.stride.max()), 32) if hasattr(model, 'stride') else 32
         img = torch.zeros((1, model.yaml.get('ch', 3), stride, stride), device=next(model.parameters()).device)  # input
-        flops = profile(deepcopy(model), inputs=(img,), verbose=False)[0] / 1E9 * 2  # stride GFLOPS
+        flops = profile(model, inputs=(img,), verbose=False)[0] / 1E9 * 2  # stride GFLOPS
         img_size = img_size if isinstance(img_size, list) else [img_size, img_size]  # expand if int/float
         fs = ', %.1f GFLOPS' % (flops * img_size[0] / stride * img_size[1] / stride)  # 640x640 GFLOPS
     except (ImportError, Exception):
