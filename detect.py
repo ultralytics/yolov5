47ff35a72aeb1dcc3a7b774b636bbd8bde2e78ba
burhany60 <burhr2@gmail.com>
2021-04-17 13:30:31 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -17,7 +17,7 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
 def detect(save_img=False):
-    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    source, weights, view_img, save_obj, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_obj, opt.save_txt, opt.img_size
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
