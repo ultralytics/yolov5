7502143fa0d074827bd23ea717022a438781f378
fcakyon <34196005+fcakyon@users.noreply.github.com>
2021-04-22 19:03:33 +0300

    remove default value

--- detect.py
+++ detect.py
@@ -15,7 +15,7 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
-def detect(opt=None):
+def detect(opt):
     source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
