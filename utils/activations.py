ef107538f48920595e5a008882f385d6db0bb69d
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-25 22:51:46 +0200

    Update activations.py

--- utils/activations.py
+++ utils/activations.py
@@ -84,8 +84,8 @@ class MetaAconC(nn.Module):
         c2 = max(r, c1 // r)
         self.p1 = nn.Parameter(torch.randn(1, c1, 1, 1))
         self.p2 = nn.Parameter(torch.randn(1, c1, 1, 1))
-        self.fc1 = nn.Conv2d(c1, c2, k, s, bias=False)
-        self.fc2 = nn.Conv2d(c2, c1, k, s, bias=False)
+        self.fc1 = nn.Conv2d(c1, c2, k, s, bias=True)
+        self.fc2 = nn.Conv2d(c2, c1, k, s, bias=True)
         # self.bn1 = nn.BatchNorm2d(c2)
         # self.bn2 = nn.BatchNorm2d(c1)
 
