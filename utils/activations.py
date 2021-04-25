22fd7fc61448131b155e2c447f7ead225a2a7ac5
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-25 22:06:54 +0200

    Update activations.py

--- utils/activations.py
+++ utils/activations.py
@@ -92,7 +92,7 @@ class MetaAconC(nn.Module):
     def forward(self, x):
         y = x.mean(dim=2, keepdims=True).mean(dim=3, keepdims=True)
         # batch-size 1 bug/instabilities https://github.com/ultralytics/yolov5/issues/2891
-        # beta = torch.sigmoid(self.bn2(self.fc2(self.bn1(self.fc1(y)))))
+        # beta = torch.sigmoid(self.bn2(self.fc2(self.bn1(self.fc1(y)))))  # bug/unstable
         beta = torch.sigmoid(self.fc2(self.fc1(y)))  # bug patch BN layers removed
         dpx = (self.p1 - self.p2) * x
         return dpx * torch.sigmoid(beta * dpx) + self.p2 * x
