184991672636838453e796f72268833dff788d07
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-25 23:33:25 +0200

    Check_requirements() enclosing apostrophe bug fix (#2929)
    This fixes a bug where the '>' symbol in python package requirements was not running correctly with subprocess.check_output() commands.

--- utils/general.py
+++ utils/general.py
@@ -117,8 +117,8 @@ def check_requirements(requirements='requirements.txt', exclude=()):
             pkg.require(r)
         except Exception as e:  # DistributionNotFound or VersionConflict if requirements not met
             n += 1
-            print(f"{prefix} {e.req} not found and is required by YOLOv5, attempting auto-update...")
-            print(subprocess.check_output(f"pip install {e.req}", shell=True).decode())
+            print(f"{prefix} {r} not found and is required by YOLOv5, attempting auto-update...")
+            print(subprocess.check_output(f"pip install '{r}'", shell=True).decode())
 
     if n:  # if packages updated
         source = file.resolve() if 'file' in locals() else requirements
