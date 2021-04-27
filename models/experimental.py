3dbe82da5fd949049b7d975f7b49128872350b38
Jonathan Lunt <jlunt@amazon.com>
2021-04-27 21:43:11 +0000

    Scope Detect/Model import to avoid circular dependency

--- models/experimental.py
+++ models/experimental.py
@@ -6,7 +6,6 @@ import torch.nn as nn
 
 from models.common import Conv, DWConv
 from utils.google_utils import attempt_download
-from models.yolo import Detect, Model
 
 
 class CrossConv(nn.Module):
@@ -112,6 +111,9 @@ class Ensemble(nn.ModuleList):
 
 
 def attempt_load(weights, map_location=None, inplace=True):
+
+    from models.yolo import Detect, Model
+
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
