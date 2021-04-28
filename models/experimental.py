b5d85db7133752fd6b8eea8499ab618433b0346e
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-29 00:40:09 +0200

    replace lost space

--- models/experimental.py
+++ models/experimental.py
@@ -126,6 +126,7 @@ def attempt_load(weights, map_location=None, inplace=True):
             m.inplace = inplace  # pytorch 1.7.0 compatibility
         elif type(m) is Conv:
             m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
+
     if len(model) == 1:
         return model[-1]  # return model
     else:
