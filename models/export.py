c39a6d994acf6462f4188a01f55c474474ecd657
jylink <crackedviolin9034@gmail.com>
2021-04-30 17:01:59 +0800

    Update export.py

--- models/export.py
+++ models/export.py
@@ -59,6 +59,7 @@ if __name__ == '__main__':
         # elif isinstance(m, models.yolo.Detect):
         #     m.forward = m.forward_export  # assign forward (optional)
     model.model[-1].export = not opt.grid  # set Detect() layer grid export
+    model.model[-1].dynamic = opt.dynamic
     for _ in range(2):
         y = model(img)  # dry runs
     print(f"\n{colorstr('PyTorch:')} starting from {opt.weights} ({file_size(opt.weights):.1f} MB)")
