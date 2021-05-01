58ac7a50748b4b74f66b4cccba9c627fb1d0c3da
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-01 18:04:00 +0200

    Hubconf.py bug fix

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -21,6 +21,7 @@ def attempt_download(file, repo='ultralytics/yolov5'):
     file = Path(str(file).strip().replace("'", ''))
 
     if not file.exists():
+        file.parent.mkdir(parents=True, exist_ok=True)  # make parent dir (if required)
         try:
             response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()  # github api
             assets = [x['name'] for x in response['assets']]  # release assets, i.e. ['yolov5s.pt', 'yolov5m.pt', ...]
