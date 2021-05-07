7a41c28ef6ba8e7ff4355c1ae9f6eb5cae82f22a
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-05-07 23:23:56 +0530

    Explicitly convert artifact path to posix_path (#3067)
    * Explicitly convert artifact path to posix_path
    
    * Remove redudant str() casting

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -158,7 +158,8 @@ class WandbLogger():
 
     def download_dataset_artifact(self, path, alias):
         if isinstance(path, str) and path.startswith(WANDB_ARTIFACT_PREFIX):
-            dataset_artifact = wandb.use_artifact(remove_prefix(path, WANDB_ARTIFACT_PREFIX) + ":" + alias)
+            artifact_path = Path(remove_prefix(path, WANDB_ARTIFACT_PREFIX) + ":" + alias)
+            dataset_artifact = wandb.use_artifact(artifact_path.as_posix())
             assert dataset_artifact is not None, "'Error: W&B dataset artifact doesn\'t exist'"
             datadir = dataset_artifact.download()
             return datadir, dataset_artifact
