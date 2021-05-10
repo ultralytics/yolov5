60fe54449d67827132c56d65db722525fe78f4c9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-10 15:26:23 +0200

    Update train.py (#3099)

--- train.py
+++ train.py
@@ -367,8 +367,6 @@ def train(hyp, opt, device, tb_writer=None):
             # Write
             with open(results_file, 'a') as f:
                 f.write(s + '%10.4g' * 7 % results + '\n')  # append metrics, val_loss
-            if len(opt.name) and opt.bucket:
-                os.system('gsutil cp %s gs://%s/results/results%s.txt' % (results_file, opt.bucket, opt.name))
 
             # Log
             tags = ['train/box_loss', 'train/obj_loss', 'train/cls_loss',  # train loss
