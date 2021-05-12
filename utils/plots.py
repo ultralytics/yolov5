71fb9ef897f944869796bba8b974d893667ee0c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-12 16:57:42 +0200

    update colors

--- utils/plots.py
+++ utils/plots.py
@@ -29,8 +29,8 @@ class Colors:
     # Ultralytics color palette https://ultralytics.com/
     def __init__(self):
         # hex = matplotlib.colors.TABLEAU_COLORS.values()
-        hex = ('FF3838', 'FF9D97', 'FF701F', 'F3D09A', 'FFF172', 'FFC804', 'B8E45A', '5AE499', '1A9334', '80FFF0',
-               '2C99A8', 'CCF3FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
+        hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', 'FFC804', '92CC17', '5AE499', '1A9334', '00D4BB',
+               '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
         self.palette = [self.hex2rgb('#' + c) for c in hex]
         self.n = len(self.palette)
 
