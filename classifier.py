95109a0ee3e2a2f218d65973db4ae3172bbf2129
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-19 13:25:49 +0200

    add comment

--- classifier.py
+++ classifier.py
@@ -85,7 +85,7 @@ def train():
         c = Classify(ch, nc)  # Classify()
         c.i, c.f, c.type = m.i, m.f, 'models.common.Classify'  # index, from, type
         model.model[-1] = c  # replace
-    elif opt.model in torch.hub.list('rwightman/gen-efficientnet-pytorch'):
+    elif opt.model in torch.hub.list('rwightman/gen-efficientnet-pytorch'):  # i.e. efficientnet_b0
         model = torch.hub.load('rwightman/gen-efficientnet-pytorch', opt.model, pretrained=True)
         model.classifier = nn.Linear(model.classifier.in_features, nc)
     else:  # try torchvision
