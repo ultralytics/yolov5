2a8487891453916d8403d33063255dfee4338782
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-26 13:31:11 +0200

    Update cache v0.2 to include parent hash
    Possible fix for https://github.com/ultralytics/yolov5/issues/3349

--- utils/datasets.py
+++ utils/datasets.py
@@ -36,9 +36,9 @@ for orientation in ExifTags.TAGS.keys():
         break
 
 
-def get_hash(files):
-    # Returns a single hash value of a list of files
-    return sum(os.path.getsize(f) for f in files if os.path.isfile(f))
+def get_hash(paths):
+    # Returns a single hash value of a list of paths (files or dirs)
+    return sum(os.path.getsize(p) for p in paths if os.path.exists(p))
 
 
 def exif_size(img):
@@ -383,7 +383,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')  # cached labels
         if cache_path.is_file():
             cache, exists = torch.load(cache_path), True  # load
-            if cache['hash'] != get_hash(self.label_files + self.img_files) or 'version' not in cache:  # changed
+            if cache['hash'] != get_hash(self.label_files + self.img_files + [str(cache_path.parent)]):  # changed
                 cache, exists = self.cache_labels(cache_path, prefix), False  # re-cache
         else:
             cache, exists = self.cache_labels(cache_path, prefix), False  # cache
@@ -499,11 +499,11 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         if nf == 0:
             logging.info(f'{prefix}WARNING: No labels found in {path}. See {help_url}')
 
-        x['hash'] = get_hash(self.label_files + self.img_files)
+        x['hash'] = get_hash(self.label_files + self.img_files + [str(path.parent)])
         x['results'] = nf, nm, ne, nc, i + 1
-        x['version'] = 0.1  # cache version
+        x['version'] = 0.2  # cache version
         try:
-            torch.save(x, path)  # save for next time
+            torch.save(x, path)  # save cache for next time
             logging.info(f'{prefix}New cache created: {path}')
         except Exception as e:
             logging.info(f'{prefix}WARNING: Cache directory {path.parent} is not writeable: {e}')  # path not writeable
