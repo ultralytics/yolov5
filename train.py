ba6f3f974bfc4a2968964dbe5eedea73c9f5efcb
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-28 15:18:44 +0200

    Enable direct `--weights URL` definition (#3373)
    * Enable direct `--weights URL` definition
    
    @KalenMike this PR will enable direct --weights URL definition. Example use case:
    ```
    python train.py --weights https://storage.googleapis.com/bucket/dir/model.pt
    ```
    
    * cleanup
    
    * bug fixes
    
    * weights = attempt_download(weights)
    
    * Update experimental.py
    
    * Update hubconf.py
    
    * return bug fix
    
    * comment mirror
    
    * min_bytes

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ def train(hyp, opt, device, tb_writer=None):
     pretrained = weights.endswith('.pt')
     if pretrained:
         with torch_distributed_zero_first(rank):
-            attempt_download(weights)  # download if not found locally
+            weights = attempt_download(weights)  # download if not found locally
         ckpt = torch.load(weights, map_location=device)  # load checkpoint
         model = Model(opt.cfg or ckpt['model'].yaml, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
         exclude = ['anchor'] if (opt.cfg or hyp.get('anchors')) and not opt.resume else []  # exclude keys
