57065200d1e5cf30c04841470119ad3f47e1add5
SkalskiP <piotr.skalski92@gmail.com>
2021-05-28 18:20:21 +0200

    update ci-testing.yml and setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,12 @@ setuptools.setup(
         "scipy>=1.4.1",
         "torch>=1.7.0",
         "torchvision>=0.8.1",
-        "tqdm>=4.41.0"
+        "tqdm>=4.41.0",
+        "tensorboard>=2.4.1",
+        "seaborn>=0.11.0",
+        "pandas",
+        "pycocotools>=2.0",
+        "thop"
     ],
     extras_require={
         'tests': [
