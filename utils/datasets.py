21a9607e00f1365b21d8c4bd81bdbf5fc0efea24
tudoulei <34886368+tudoulei@users.noreply.github.com>
2021-05-30 03:12:01 +0800

    `cv2.imread(img, -1)` for IMREAD_UNCHANGED (#3379)
    * Update datasets.py
    
    * comment
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -181,7 +181,7 @@ class LoadImages:  # for inference
         else:
             # Read image
             self.count += 1
-            img0 = cv2.imread(path)  # BGR
+            img0 = cv2.imread(path, -1)  # BGR (-1 is IMREAD_UNCHANGED)
             assert img0 is not None, 'Image Not Found ' + path
             print(f'image {self.count}/{self.nf} {path}: ', end='')
 
