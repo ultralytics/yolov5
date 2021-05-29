ea3a287a285823e4d59b230a92a62bc35c65bee7
SkalskiP <piotr.skalski92@gmail.com>
2021-05-29 11:47:42 +0200

    temporarily to check compatibility with different versions of python

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development",
         "Topic :: Scientific/Engineering",
