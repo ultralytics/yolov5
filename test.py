17e406386d5f44bee5e9fc53facfb78d1cbab756
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-04 22:08:10 +0200

    revert space

--- test.py
+++ test.py
@@ -307,7 +307,6 @@ if __name__ == '__main__':
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--half', type=bool, default=False, help='use FP16 half-precision inference')
-
     opt = parser.parse_args()
     opt.save_json |= opt.data.endswith('coco.yaml')
     opt.data = check_file(opt.data)  # check file
