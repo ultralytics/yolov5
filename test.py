47ccc8213764db737bf48bd5fb737236f6a6803a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-08 18:46:21 +0200

    Update FP16 `--half` argument for test.py and detect.py

--- test.py
+++ test.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
     parser.add_argument('--project', default='runs/test', help='save to project/name')
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--half', type=bool, default=False, help='use FP16 half-precision inference')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     opt = parser.parse_args()
     opt.save_json |= opt.data.endswith('coco.yaml')
     opt.data = check_file(opt.data)  # check file
