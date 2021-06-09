8b5086c21ba227c0257d94ea34cb46124a9c559a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-09 15:31:47 +0200

    Multi-GPU default to single device 0 (#3554)
    * Multi-GPU default to single device 0
    
    * Multi-GPU default to single device 0
    
    * add space

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -72,11 +72,11 @@ def select_device(device='', batch_size=None):
 
     cuda = not cpu and torch.cuda.is_available()
     if cuda:
-        devices = device.split(',') if device else range(torch.cuda.device_count())  # i.e. 0,1,6,7
+        devices = device.split(',') if device else '0'  # range(torch.cuda.device_count())  # i.e. 0,1,6,7
         n = len(devices)  # device count
         if n > 1 and batch_size:  # check batch_size is divisible by device_count
             assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
-        space = ' ' * len(s)
+        space = ' ' * (len(s) + 1)
         for i, d in enumerate(devices):
             p = torch.cuda.get_device_properties(i)
             s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"  # bytes to MB
