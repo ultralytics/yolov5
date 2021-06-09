d40617e3d83809e155795dd32bd25bd6f2480a48
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-09 15:19:49 +0200

    add space

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -76,7 +76,7 @@ def select_device(device='', batch_size=None):
         n = len(devices)  # device count
         if n > 1 and batch_size:  # check batch_size is divisible by device_count
             assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
-        space = ' ' * len(s)
+        space = ' ' * (len(s) + 1)
         for i, d in enumerate(devices):
             p = torch.cuda.get_device_properties(i)
             s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"  # bytes to MB
