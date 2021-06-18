c886538f9d5c54d114506031d87ff57132dcdf59
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-18 13:16:09 +0200

    print

--- train.py
+++ train.py
@@ -531,6 +531,7 @@ if __name__ == '__main__':
     if LOCAL_RANK != -1:
         assert torch.cuda.device_count() > LOCAL_RANK
         torch.cuda.set_device(LOCAL_RANK)
+        print({'RANK': RANK, 'LOCAL_RANK': LOCAL_RANK, 'WORLD_SIZE': WORLD_SIZE})
         device = torch.device('cuda', LOCAL_RANK)
         dist.init_process_group(backend="gloo|nccl")  # distributed backend
         assert opt.batch_size % WORLD_SIZE == 0, '--batch-size must be multiple of CUDA device count'
