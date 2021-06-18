382ce4ffdd7ca7e575e8102f8e2949f479a80dfd
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-18 14:04:51 +0200

    fix getenv

--- train.py
+++ train.py
@@ -37,9 +37,9 @@ from utils.torch_utils import ModelEMA, select_device, intersect_dicts, torch_di
 from utils.wandb_logging.wandb_utils import WandbLogger, check_wandb_resume
 
 logger = logging.getLogger(__name__)
-LOCAL_RANK = int(getattr(os.environ, 'LOCAL_RANK', -1))  # https://pytorch.org/docs/stable/elastic/run.html
-RANK = int(getattr(os.environ, 'RANK', -1))
-WORLD_SIZE = int(getattr(os.environ, 'WORLD_SIZE', 1))
+LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytorch.org/docs/stable/elastic/run.html
+RANK = int(os.getenv('RANK', -1))
+WORLD_SIZE = int(os.getenv('WORLD_SIZE', 1))
 
 
 def train(hyp,  # path/to/hyp.yaml or hyp dictionary
