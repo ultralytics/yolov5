c6257bf17abb5599c8fe55165944dc74354759d0
SpongeBab <2078825250@qq.com>
2021-06-18 14:15:58 +0800

    Fixed Fixed judgment rules. (#3656)
    If any of xmin, ymin, xmax, or ymax is out of bounds, it will be treated as out of bounds.

--- utils/datasets.py
+++ utils/datasets.py
@@ -1075,8 +1075,8 @@ def verify_image_label(args):
                 l = np.array(l, dtype=np.float32)
             if len(l):
                 assert l.shape[1] == 5, 'labels require 5 columns each'
-                assert (l >= 0).all(), 'negative labels'
-                assert (l[:, 1:] <= 1).all(), 'non-normalized or out of bounds coordinate labels'
+                assert (l >= 0).any(), 'negative labels'
+                assert (l[:, 1:] <= 1).any(), 'non-normalized or out of bounds coordinate labels'
                 assert np.unique(l, axis=0).shape[0] == l.shape[0], 'duplicate labels'
             else:
                 ne = 1  # label empty
