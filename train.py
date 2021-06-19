d91734163271233aefb6cd9192b69b143f3c3de2
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-19 15:25:14 +0200

    gloo

--- train.py
+++ train.py
@@ -539,8 +539,8 @@ def main(opt):
         assert torch.cuda.device_count() > LOCAL_RANK, 'too few GPUS for DDP command'
         torch.cuda.set_device(LOCAL_RANK)
         device = torch.device('cuda', LOCAL_RANK)
-        # dist.init_process_group(backend="gloo", timeout=timedelta(seconds=10))  # distributed backend
-        dist.init_process_group(backend="nccl", init_method='env://')  # distributed backend
+        dist.init_process_group(backend="gloo", timeout=timedelta(seconds=10))  # distributed backend
+        # dist.init_process_group(backend="nccl", init_method='env://')  # distributed backend
         assert opt.batch_size % WORLD_SIZE == 0, '--batch-size must be multiple of CUDA device count'
         assert not opt.image_weights, '--image-weights argument is not compatible with DDP training'
         opt.batch_size = opt.total_batch_size // WORLD_SIZE
