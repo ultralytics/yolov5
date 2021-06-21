ec999c63f1dbfcd1f05f875fd8d3b63ca0880f0f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-21 19:29:36 +0200

    fix, 1

--- utils/datasets.py
+++ utils/datasets.py
@@ -348,7 +348,7 @@ class LoadStreams:  # multiple IP or RTSP cameras
 def img2label_paths(img_paths):
     # Define label paths as a function of image paths
     sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
-    return [sb.join(x.rsplit(sa, 1)).rsplit('.')[0] + '.txt' for x in img_paths]
+    return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
 
 class LoadImagesAndLabels(Dataset):  # for training/testing
