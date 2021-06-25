adfb185ea6c66429e6872a597a0100f9ebdd3c40
SkalskiP <piotr.skalski92@gmail.com>
2021-06-25 21:14:53 +0200

    more changes
    (cherry picked from commit 0eb95618cdaf9efef386d67f71956e2c4ed1dbed)

--- yolov5/utils/general.py
+++ yolov5/utils/general.py
@@ -642,9 +642,10 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
     return output
 
 
-def strip_optimizer(f='best.pt', s=''):  # from utils.general import *; strip_optimizer()
+def strip_optimizer(f='best.pt', s=''):
     # Strip optimizer from 'f' to finalize training, optionally save as 's'
-    x = torch.load(f, map_location=torch.device('cpu'))
+    with yolov5_in_syspath():
+        x = torch.load(f, map_location=torch.device('cpu'))
     if x.get('ema'):
         x['model'] = x['ema']  # replace model with ema
     for k in 'optimizer', 'training_results', 'wandb_id', 'ema', 'updates':  # keys
@@ -653,7 +654,8 @@ def strip_optimizer(f='best.pt', s=''):  # from utils.general import *; strip_op
     x['model'].half()  # to FP16
     for p in x['model'].parameters():
         p.requires_grad = False
-    torch.save(x, s or f)
+    with yolov5_in_syspath():
+        torch.save(x, s or f)
     mb = os.path.getsize(s or f) / 1E6  # filesize
     print(f"Optimizer stripped from {f},{(' saved as %s,' % s) if s else ''} {mb:.1f}MB")
 
