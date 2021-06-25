b77693f13b9775f7a7ee32598a886b0450cc6ce0
Yonghye Kwon <developer.0hye@gmail.com>
2021-06-25 09:50:49 +0900

    formatting

--- utils/general.py
+++ utils/general.py
@@ -396,10 +396,10 @@ def xywhn2xyxy(x, w=640, h=640, padw=0, padh=0):
 def xyxy2xywhn(x, w=640, h=640):
     # Convert nx4 boxes from [x, y, w, h] normalized to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-    y[:, 0] = ((x[:, 0] + x[:, 2]) / 2)/w  # x center
-    y[:, 1] = ((x[:, 1] + x[:, 3]) / 2)/h  # y center
-    y[:, 2] = (x[:, 2] - x[:, 0])/w  # width
-    y[:, 3] = (x[:, 3] - x[:, 1])/h  # height
+    y[:, 0] = ((x[:, 0] + x[:, 2]) / 2) / w  # x center
+    y[:, 1] = ((x[:, 1] + x[:, 3]) / 2) / h  # y center
+    y[:, 2] = (x[:, 2] - x[:, 0]) / w  # width
+    y[:, 3] = (x[:, 3] - x[:, 1]) / h  # height
     return y
 
 
