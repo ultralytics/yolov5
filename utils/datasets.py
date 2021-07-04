f71bd5db67f6eb5a2e33ddab9e45b8b856f809ef
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-04 18:13:48 +0200

    cleanup

--- utils/datasets.py
+++ utils/datasets.py
@@ -784,7 +784,6 @@ def flatten_recursive(path='../datasets/coco128'):
 
 def extract_boxes(path='../datasets/coco128'):  # from utils.datasets import *; extract_boxes()
     # Convert detection dataset into classification dataset, with one directory per class
-
     path = Path(path)  # images dir
     shutil.rmtree(path / 'classifier') if (path / 'classifier').is_dir() else None  # remove existing
     files = list(path.rglob('*.*'))
