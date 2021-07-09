02558424258b47432b6621dd4ccf2dc222a7aee6
Yonghye Kwon <developer.0hye@gmail.com>
2021-07-09 19:02:24 +0900

    read img contains unicode characters in file path
    https://stackoverflow.com/questions/59823318/read-load-images-with-unicode-characters-in-image-path-chinese-japanese-korea

--- utils/datasets.py
+++ utils/datasets.py
@@ -625,7 +625,9 @@ def load_image(self, index):
     img = self.imgs[index]
     if img is None:  # not cached
         path = self.img_files[index]
-        img = cv2.imread(path)  # BGR
+        img = np.fromfile(path, np.uint8)
+        img = cv2.imdecode(img)
+        
         assert img is not None, 'Image Not Found ' + path
         h0, w0 = img.shape[:2]  # orig hw
         r = self.img_size / max(h0, w0)  # ratio
