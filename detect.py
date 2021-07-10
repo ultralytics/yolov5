3ff5e6d520db43c897293a9fe35fccfb252a3a13
Mark Kurtz <mark.kurtz@neuralmagic.com>
2021-07-10 11:10:18 -0400

    fix loading for detect.py

--- detect.py
+++ detect.py
@@ -6,7 +6,7 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
-from models.experimental import attempt_load
+from models.export import load_checkpoint
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
@@ -30,7 +30,7 @@ def detect(opt):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
+    model, extras = load_checkpoint('ensemble', weights, device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
     names = model.module.names if hasattr(model, 'module') else model.names  # get class names
