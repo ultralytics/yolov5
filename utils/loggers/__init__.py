3dca145b375a3fc26ee1cd88c90ba3007a89607d
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-24 20:12:53 +0200

    fix6

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -35,6 +35,8 @@ class Loggers():
             setattr(self, k, None)  # init empty logger dictionary
 
     def start(self):
+        self.txt = True  # always log to txt
+
         # TensorBoard
         s = self.save_dir
         if 'tb' in self.include and not self.opt.evolve:
@@ -50,7 +52,6 @@ class Loggers():
             self.wandb = WandbLogger(self.opt, s.stem, run_id, self.data_dict)
         except:
             self.include = [x for x in self.include if x != 'wandb']
-            self.wandb = None
 
         return self
 
@@ -60,23 +61,23 @@ class Loggers():
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore')  # suppress jit trace warning
                 self.tb.add_graph(torch.jit.trace(de_parallel(model), imgs[0:1], strict=False), [])
-        if 'wandb' in self.include and ni == 10:
+        if self.wandb and ni == 10:
             files = self.save_dir.glob('train*.jpg')
             wandb.log({'Mosaics': [wandb.Image(str(f), caption=f.name) for f in files if f.exists()]})
 
     def on_train_epoch_end(self, epoch):
         # Callback runs on train epoch end
-        if 'wandb' in self.include:
+        if self.wandb:
             self.wandb.current_epoch = epoch + 1
 
     def on_val_batch_end(self, pred, predn, path, names, im):
         # Callback runs on train batch end
-        if 'wandb' in self.include:
+        if self.wandb:
             self.wandb.val_one_image(pred, predn, path, names, im)
 
     def on_val_end(self):
         # Callback runs on val end
-        if 'wandb' in self.include:
+        if self.wandb:
             files = sorted(self.save_dir.glob('val*.jpg'))
             wandb.log({"Validation": [wandb.Image(str(f), caption=f.name) for f in files]})
 
@@ -87,20 +88,20 @@ class Loggers():
                 'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',
                 'val/box_loss', 'val/obj_loss', 'val/cls_loss',  # val loss
                 'x/lr0', 'x/lr1', 'x/lr2']  # params
-        if 'txt' in self.include:
+        if self.txt:
             with open(self.results_file, 'a') as f:
                 f.write(s + '%10.4g' * 7 % results + '\n')  # append metrics, val_loss
-        if 'tb' in self.include:
+        if self.tb:
             for x, tag in zip(vals, tags):
                 self.tb.add_scalar(tag, x, epoch)  # TensorBoard
-        if 'wandb' in self.include:
+        if self.wandb:
             self.wandb.end_epoch(best_result=best_fitness == fi)
             for x, tag in zip(vals, tags):
                 wandb.log({tag: x})
 
     def on_model_save(self, last, epoch, final_epoch, best_fitness, fi):
         # Callback runs on model save event
-        if 'wandb' in self.include:
+        if self.wandb:
             if ((epoch + 1) % self.opt.save_period == 0 and not final_epoch) and self.opt.save_period != -1:
                 self.wandb.log_model(last.parent, self.opt, epoch, fi, best_model=best_fitness == fi)
 
@@ -108,7 +109,7 @@ class Loggers():
         # Callback runs on training end
         files = ['results.png', 'confusion_matrix.png', *[f'{x}_curve.png' for x in ('F1', 'PR', 'P', 'R')]]
         files = [(self.save_dir / f) for f in files if (self.save_dir / f).exists()]  # filter
-        if 'wandb' in self.include:
+        if self.wandb:
             wandb.log({"Results": [wandb.Image(str(f), caption=f.name) for f in files]})
             wandb.log_artifact(str(best if best.exists() else last), type='model',
                                name='run_' + self.wandb.wandb_run.id + '_model',
@@ -117,5 +118,5 @@ class Loggers():
 
     def log_images(self, paths):
         # Log images
-        if 'wandb' in self.include:
+        if self.wandb:
             wandb.log({"Labels": [wandb.Image(str(x), caption=x.name) for x in paths]}, commit=False)
