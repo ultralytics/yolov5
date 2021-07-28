5d6124259c8cab7c8d33e4a5d2b168a7974a9a46
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-07-28 16:32:26 +0000

    Remove redundant try catch

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -48,12 +48,11 @@ class Loggers():
             self.tb = SummaryWriter(str(s))
 
         # W&B
-        try:
-            assert 'wandb' in self.include and wandb
+        if wandb:
             run_id = torch.load(self.weights).get('wandb_id') if self.opt.resume else None
             self.opt.hyp = self.hyp  # add hyperparameters
             self.wandb = WandbLogger(self.opt, run_id)
-        except:
+        else:
             self.wandb = None
 
         return self
