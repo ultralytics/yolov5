43f687878cfba2dd6e7001d392efe54fa484b0e7
Benjamin Fineran <bfineran@users.noreply.github.com>
2021-07-28 13:41:25 -0400

    fix inputs to os.path.join in ONNX export
    should be separate str-like arguments instead of `List[str]`

--- models/export.py
+++ models/export.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
             print(f'{prefix} starting export with onnx {onnx.__version__}...')
             f = opt.weights.replace('.pt', '.onnx')  # filename
             # export through SparseML so quantized and pruned graphs can be corrected
-            save_dir = os.path.join(f.split(os.path.sep)[:-1])
+            save_dir = os.path.join(*f.split(os.path.sep)[:-1])
             save_name = f.split(os.path.sep)[-1]
 
             # get the number of outputs so we know how to name and change dynamic axes
