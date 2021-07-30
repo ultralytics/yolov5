2b48f3d66db68efa0030eca6e967ae6110ec4cc8
IneovaAI <67843470+IneovaAI@users.noreply.github.com>
2021-07-30 16:56:24 +0200

    Update train.py

--- train.py
+++ train.py
@@ -442,7 +442,7 @@ def parse_opt(known=False):
     parser.add_argument('--save_period', type=int, default=-1, help='Log model after every "save_period" epoch')
     parser.add_argument('--artifact_alias', type=str, default="latest", help='version of dataset artifact to be used')
     parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
-    parser.add_argument('--freeze', type=int, default=0, help='Amount of layers to freeze. backbone=10 all=24')
+    parser.add_argument('--freeze', type=int, help='Amount of layers to freeze. backbone=10 all=24')
     opt = parser.parse_known_args()[0] if known else parser.parse_args()
     return opt
 
