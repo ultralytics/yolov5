5dd883618f68249964432ea9424e8b0c04d9a9b6
IneovaAI <67843470+IneovaAI@users.noreply.github.com>
2021-07-30 16:54:46 +0200

    Update train.py

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
         model = Model(cfg, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
 
     # Freeze
-    freeze = ['model.%s.' % x for x in range(freeze)]  # parameter names to freeze (full or partial)
+    freeze = ['model.%s.' % x for x in range(freeze)] if freeze is not NONE else []  # parameter names to freeze (full or partial)
     for k, v in model.named_parameters():
         v.requires_grad = True  # train all layers
         if any(x in k for x in freeze):
