4c48819601ab3154ddf34decc1dd899ae9bd8560
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-04 16:41:24 +0200

    plot_results() fix

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -122,7 +122,7 @@ class Loggers():
     def on_train_end(self, last, best, plots, epoch):
         # Callback runs on training end
         if plots:
-            plot_results(dir=self.save_dir)  # save results.png
+            plot_results(file=self.save_dir / 'results.csv')  # save results.png
         files = ['results.png', 'confusion_matrix.png', *[f'{x}_curve.png' for x in ('F1', 'PR', 'P', 'R')]]
         files = [(self.save_dir / f) for f in files if (self.save_dir / f).exists()]  # filter
 
