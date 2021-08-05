9a4fe2e067dfa577a9e42618f2b9f2c084d962c8
Yonghye Kwon <developer.0hye@gmail.com>
2021-08-05 11:23:25 +0900

    Replace DWConvClass with DWConv

--- models/yolo.py
+++ models/yolo.py
@@ -202,7 +202,7 @@ class Model(nn.Module):
     def fuse(self):  # fuse model Conv2d() + BatchNorm2d() layers
         LOGGER.info('Fusing layers... ')
         for m in self.model.modules():
-            if isinstance(m, (Conv, DWConvClass)) and hasattr(m, 'bn'):
+            if isinstance(m, (Conv, DWConv)) and hasattr(m, 'bn'):
                 m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
                 delattr(m, 'bn')  # remove batchnorm
                 m.forward = m.forward_fuse  # update forward
