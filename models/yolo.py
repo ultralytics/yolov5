42fc32257a31a3e5587bdc795d0d3ce926999c20
Yonghye Kwon <developer.0hye@gmail.com>
2021-08-12 00:03:15 +0900

    edit comment
    change the line

--- models/yolo.py
+++ models/yolo.py
@@ -51,7 +51,8 @@ class Detect(nn.Module):
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
             bs, _, ny, nx = x[i].shape  
-            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous() # x(bs,255,20,20) to x(bs,3,20,20,85)
+            # x(bs,255,20,20) to x(bs,3,20,20,85)
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous() 
 
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4] or self.onnx_dynamic:
