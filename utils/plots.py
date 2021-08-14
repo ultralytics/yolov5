e086347377923076fb469a401b65980b98cd871b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-14 12:50:27 +0200

    Update plots.py (#4407)

--- utils/plots.py
+++ utils/plots.py
@@ -79,7 +79,7 @@ def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
         cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
-def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=None):
+def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=3):
     # Plots one bounding box on image 'im' using PIL
     im = Image.fromarray(im)
     draw = ImageDraw.Draw(im)
