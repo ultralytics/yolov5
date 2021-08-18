1d65e8194d183f4e1537cf64a7292f8ab57d1d55
Huu Quan, CAP <huuquan1994@users.noreply.github.com>
2021-08-18 19:07:09 +0900

    Fix missing labels after albumentations (#4455)
    * fix missing labels after augmentation
    
    * Update datasets.py
    
    Cleanup
    
    Co-authored-by: Huu Quan <huuquan@HuuQuans-MacBook.local>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -568,6 +568,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         if self.augment:
             # Albumentations
             img, labels = self.albumentations(img, labels)
+            nl = len(labels) # update after albumentations
 
             # HSV color-space
             augment_hsv(img, hgain=hyp['hsv_h'], sgain=hyp['hsv_s'], vgain=hyp['hsv_v'])
