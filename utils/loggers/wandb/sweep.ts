09e42239c380cc4314c273d2095d07d85e3603b8
L. H. N <82917144+l198881@users.noreply.github.com>
2021-08-26 21:24:07 -0300

    Update sweep.ts

--- utils/loggers/wandb/sweep.ts
+++ utils/loggers/wandb/sweep.ts
@@ -14,13 +14,13 @@ sys.path.append(FILE.parents[3].as_posix())  # add utils/ to path
  ():
     wandb.init()
     # Get hyp dict sweep agent
-    hyp_dict  vars(wandb.config).get("_items")
+    hyp_dict  vars(wandb.config).get( )
 
     # Workaround: necessary opt args
     opt parse_opt(known=True)
     opt.batch_size  hyp_dict.get("batch_size")
     opt.save_dir  str(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok or opt.evolve))
-    opt.epochs  hyp_dict("epochs")
+    opt.epochs  hyp_dict( )
     opt.nosave  True
     opt.data  hyp_dict.get(newData)
     device  select_device(opt.device, batch_size=opt.batch_size)
