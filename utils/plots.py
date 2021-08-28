4d0ec24764295b3bfe399987438195d9b028557a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-29 00:39:24 +0200

    2x for loop

--- utils/plots.py
+++ utils/plots.py
@@ -160,58 +160,54 @@ def output_to_target(output):
 
 def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max_size=640, max_subplots=16):
     # Plot image grid with labels
-
     if isinstance(images, torch.Tensor):
         images = images.cpu().float().numpy()
     if isinstance(targets, torch.Tensor):
         targets = targets.cpu().numpy()
 
-    # un-normalise
-    if np.max(images[0]) <= 1:
-        images *= 255
-
+    images *= 255.0 if np.max(images[0]) <= 1 else 1.0  # de-normalise (optional)
     bs, _, h, w = images.shape  # batch size, _, height, width
     bs = min(bs, max_subplots)  # limit plot images
     ns = np.ceil(bs ** 0.5)  # number of subplots (square)
 
-    # Check if we should resize
-    scale_factor = max_size / max(h, w)
-    if scale_factor < 1:
-        h = math.ceil(scale_factor * h)
-        w = math.ceil(scale_factor * w)
+    # Check if resize
+    scale = max_size / max(h, w)
+    if scale < 1:
+        h = math.ceil(scale * h)
+        w = math.ceil(scale * w)
 
     fs = int(h * ns * 0.02)  # font size
     mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
-    for i, img in enumerate(images):
+    for i, im in enumerate(images):
         if i == max_subplots:  # if last batch has fewer images than we expect
             break
+        x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
+        im = im.transpose(1, 2, 0)
+        if scale < 1:
+            im = cv2.resize(im, (w, h))
+        mosaic[y:y + h, x:x + w, :] = im
 
-        block_x = int(w * (i // ns))
-        block_y = int(h * (i % ns))
+    annotator = Annotator(mosaic, line_width=round(fs / 10), font_size=fs)
+    for i in range(i + 1):
+        x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
 
-        img = img.transpose(1, 2, 0)
-        if scale_factor < 1:
-            img = cv2.resize(img, (w, h))
-
-        mosaic[block_y:block_y + h, block_x:block_x + w, :] = img
-        annotator = Annotator(mosaic, line_width=round(fs / 10), font_size=fs)
         if len(targets) > 0:
-            image_targets = targets[targets[:, 0] == i]
-            boxes = xywh2xyxy(image_targets[:, 2:6]).T
-            classes = image_targets[:, 1].astype('int')
-            labels = image_targets.shape[1] == 6  # labels if no conf column
-            conf = None if labels else image_targets[:, 6]  # check for confidence presence (label vs pred)
+            ti = targets[targets[:, 0] == i]  # image targets
+            boxes = xywh2xyxy(ti[:, 2:6]).T
+            classes = ti[:, 1].astype('int')
+            labels = ti.shape[1] == 6  # labels if no conf column
+            conf = None if labels else ti[:, 6]  # check for confidence presence (label vs pred)
 
             if boxes.shape[1]:
                 if boxes.max() <= 1.01:  # if normalized with tolerance 0.01
                     boxes[[0, 2]] *= w  # scale to pixels
                     boxes[[1, 3]] *= h
-                elif scale_factor < 1:  # absolute coords need scale if image scales
-                    boxes *= scale_factor
-            boxes[[0, 2]] += block_x
-            boxes[[1, 3]] += block_y
+                elif scale < 1:  # absolute coords need scale if image scales
+                    boxes *= scale
+            boxes[[0, 2]] += x
+            boxes[[1, 3]] += y
             for j, box in enumerate(boxes.T):
-                cls = int(classes[j])
+                cls = classes[j]
                 color = colors(cls)
                 cls = names[cls] if names else cls
                 if labels or conf[j] > 0.25:  # 0.25 conf thresh
@@ -221,12 +217,12 @@ def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max
         # Draw image filename labels
         if paths:
             label = Path(paths[i]).name[:40]  # trim to 40 char
-            annotator.text((block_x + 5, block_y + 5 + h), label, (220, 220, 220))
+            annotator.text((x + 5, y + 5 + h), label, (220, 220, 220))
 
         # Image border
-        annotator.rectangle((block_x, block_y, block_x + w, block_y + h), None, (255, 255, 255), width=2)
-        mosaic = annotator.result()
+        annotator.rectangle((x, y, x + w, y + h), None, (255, 255, 255), width=2)
 
+    mosaic = annotator.result()
     if fname:
         r = min(1280. / max(h, w) / ns, 1.0)  # ratio to limit image size
         mosaic = cv2.resize(mosaic, (int(ns * w * r), int(ns * h * r)), interpolation=cv2.INTER_AREA)
