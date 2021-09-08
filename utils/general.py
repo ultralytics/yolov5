25a7e1dae59e75d2c401a49cd2c7b76a7cf07139
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-08 16:01:03 +0200

    Update `check_yaml()` comment (#4713)
    * Update `check_yaml()` comment
    
    * Cleanup

--- utils/general.py
+++ utils/general.py
@@ -252,7 +252,7 @@ def check_suffix(file='yolov5s.pt', suffix=('.pt',), msg=''):
 
 
 def check_yaml(file, suffix=('.yaml', '.yml')):
-    # Check YAML file(s) for acceptable suffixes
+    # Search/download YAML file (if necessary) and return path, checking suffix
     return check_file(file, suffix)
 
 
