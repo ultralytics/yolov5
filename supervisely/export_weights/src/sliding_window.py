af00a55650337e88e9efeb1420f352f9a8e232ee
work <dvmixalkin@mail.ru>
2021-09-08 11:25:11 +0300

    reformat to script style

--- supervisely/export_weights/src/sliding_window.py
+++ supervisely/export_weights/src/sliding_window.py
@@ -1,19 +1,19 @@
+import argparse
 import numpy as np
-import onnxruntime as rt
+
 import os
 import supervisely_lib as sly
 import torch
-import yaml
+import torch.nn.functional as F
 
 from pathlib import Path
 from PIL import Image
 from torchvision import transforms
-from app_utils import download_weights, to_numpy
-from models.experimental import attempt_load
+
+from app_utils import download_file, to_numpy, get_image, get_model, get_configs, preprocess
+from supervisely.serve.src.nn_utils import construct_model_meta
 from utils.general import non_max_suppression
 from utils.torch_utils import select_device
-from supervisely.serve.src.nn_utils import construct_model_meta
-
 
 my_app = sly.AppService()
 team_id = int(os.environ['context.teamId'])
@@ -22,43 +22,63 @@ task_id = int(os.environ['TASK_ID'])
 customWeightsPath = os.environ['modal.state.slyFile']
 device = select_device(device='cpu')
 
-kwargs = dict(my_app=my_app,
-            team_id=team_id,
-            workspace_id=workspace_id,
-            task_id=task_id,
-            customWeightsPath=customWeightsPath,
-            device=device)
+
+def infer_torch_model(_model, _tensor):
+    output_ = _model(_tensor)[0]
+    return output_
 
 
-def onnx_inference(path_to_onnx_saved_model):
-    onnx_model = rt.InferenceSession(path_to_onnx_saved_model)
-    input_name = onnx_model.get_inputs()[0].name
-    label_name = onnx_model.get_outputs()[0].name
-    return onnx_model, input_name, label_name
+def infer_onnx_model(_model, _tensor):
+    _model, input_name, label_name = _model
+    onnx_model_inference = _model.run([label_name], {input_name: to_numpy(_tensor).astype(np.float32)})[0]
+    return onnx_model_inference
 
 
-def sliding_window_approach(model_, image, **kwargs):
-    conf_threshold = kwargs['conf_threshold'] if 'conf_threshold' in kwargs else 0.25
-    iou_threshold = kwargs['iou_threshold'] if 'iou_threshold' in kwargs else 0.45
-    agnostic = kwargs['agnostic'] if 'agnostic' in kwargs else False
-    native = kwargs['native'] if 'native' in kwargs else True
-    sliding_window_step = kwargs['sliding_window_step'] if 'sliding_window_step' in kwargs else None
-    input_iamge_size = kwargs['input_iamge_size'] if 'input_iamge_size' in kwargs else None
+def infer_model(model_, img, conf_thresh=0.25, iou_thresh=0.45, agnostic=False, input_img_size=None):
+    infer_fn = infer_onnx_model if isinstance(model_, tuple) else infer_torch_model
+    if hasattr(model_, 'img_size'):
+        height, width = model_.img_size
+    else:
+        if input_img_size:
+            height, width = input_img_size
+        else:
+            raise Exception('input_image_size must be passed for TorchScript and ONNx models')
+    resize = transforms.Resize(size=(height, width))
+    prepared_image = resize(img)
 
+    output_ = infer_fn(model_, prepared_image)
+    output_ = non_max_suppression(output_,
+                                  conf_thres=conf_thresh,
+                                  iou_thres=iou_thresh,
+                                  agnostic=agnostic)
+
+    original_img_sz = img.shape[2:]
+    reshaped_img_sz = prepared_image.shape[2:]
+    output_ = preprocess(output_, original_img_sz, reshaped_img_sz)
+    return output_
+
+
+def sliding_window(model_, img, conf_thresh=0.25, iou_thresh=0.45,
+                   agnostic=False, native=False,
+                   sliding_window_step=None, input_img_size=None):
+    # infer_fn = infer_onnx_model if isinstance(model_, tuple) else infer_torch_model
     if isinstance(model_, tuple):
         onnx_model, input_name, label_name = model_
-    img_h, img_w = image.shape[-2:]
+
+    img_h, img_w = img.shape[-2:]
     try:
         sw_h, sw_w = model_.img_size
     except:
-        assert input_iamge_size is not None, 'For torchScript and ONNX models input image size should be passed!'
-        sw_h, sw_w = input_iamge_size
+        assert input_img_size is not None, 'For torchScript and ONNX models input image size should be passed!'
+        sw_h, sw_w = input_img_size
 
     if sliding_window_step:
         sws_h, sws_w = sliding_window_step
     else:
-        sws_h = (img_h - sw_h + 1) // 4
-        sws_w = (img_w - sw_w + 1) // 4
+        sws_h = (img_h - sw_h + 1) // 5
+        # sws_h = round((img_h - sw_h + 1) / 5)
+        sws_w = (img_w - sw_w + 1) // 5
+        # sws_w = round((img_w - sw_w + 1) / 5)
 
     possible_height_steps = (img_h - sw_h + 1) // sws_h
     possible_width_steps = (img_w - sw_w + 1) // sws_w
@@ -71,139 +91,97 @@ def sliding_window_approach(model_, image, **kwargs):
             left = w * sws_w
             bot = top + sw_h
             right = left + sw_w
-            cropped_image = image[..., top:bot, left:right].unsqueeze(0) / 255
+            cropped_image = img[..., top:bot, left:right]
+            # padding_left, padding_right, padding_top, padding_bottom
+            cropped_image = F.pad(cropped_image,
+                                  pad=(0, sw_w - cropped_image.shape[3], 0, sw_h - cropped_image.shape[2]))
+
+            # inf_res = infer_fn(model_, cropped_image)
             if not isinstance(model_, tuple):
                 inf_res = model_(cropped_image)[0]
             else:
                 inf_res = onnx_model.run([label_name], {input_name: to_numpy(cropped_image).astype(np.float32)})[0]
 
-            inf_res = inf_res[inf_res[..., 4] > conf_threshold]
+            inf_res = inf_res[inf_res[..., 4] > conf_thresh]
             inf_res[:, 0] += left
             inf_res[:, 1] += top
             if native:
                 inf_res = inf_res if len(inf_res.shape) == 3 else np.expand_dims(inf_res, axis=0)
                 inf_res = non_max_suppression(inf_res,
-                                              conf_thres=conf_threshold,
-                                              iou_thres=iou_threshold,
+                                              conf_thres=conf_thresh,
+                                              iou_thres=iou_thresh,
                                               agnostic=agnostic)[0]
             candidates.append(inf_res)
 
     if isinstance(candidates[0], np.ndarray):
-        candidates = [torch.as_tensor(element) for element in candidates]  # if not isinstance(element, torch.Tensor)
+        candidates = [torch.as_tensor(element) for element in candidates]
     detections = torch.cat(candidates).unsqueeze_(0)
 
     if not native:
-        detections = non_max_suppression(detections, conf_thres=conf_threshold, iou_thres=iou_threshold,
-                                         agnostic=agnostic)
+        detections = non_max_suppression(detections, conf_thres=conf_thresh, iou_thres=iou_thresh, agnostic=agnostic)
     return detections
 
 
-def visualize_dets(img0, output, save_path, **kwargs):
+def visualize_dets(img_, output_, save_path_, names_, meta_):
     labels = []
-    names = kwargs['model'].module.names if hasattr(model, 'module') else kwargs['model'].names
-    for i, det in enumerate(output):  # replace output with "torchScript_output" or "onnx_output"
+    for i, det in enumerate(output_):
         if det is not None and len(det):
             for *xyxy, conf, cls in reversed(det):
                 left, top, right, bottom = int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])
                 rect = sly.Rectangle(top, left, bottom, right)
-                obj_class = meta.get_obj_class(names[int(cls)])
-                tag = sly.Tag(meta.get_tag_meta("confidence"), round(float(conf), 4))
+                obj_class = meta_.get_obj_class(names_[int(cls)])
+                tag = sly.Tag(meta_.get_tag_meta("confidence"), round(float(conf), 4))
                 label = sly.Label(rect, obj_class, sly.TagCollection([tag]))
                 labels.append(label)
 
-    # height, width = img0.shape[2:]
-    width, height = img0.size
+    width, height = img_.size
     ann = sly.Annotation(img_size=(height, width), labels=labels)
 
-    vis = np.copy(img0)
+    vis = np.copy(img_)
     ann.draw_contour(vis, thickness=2)
-    sly.image.write(os.path.join(save_path, "vis_detection.jpg"), vis)
+    sly.image.write(save_path_, vis)
     return vis
 
 
-def prepare_model(weights_path, **kwargs):
-    cwp = os.path.join(Path(weights_path).parents[1], 'opt.yaml')
-    cfgs_path = download_weights(cwp, **kwargs)
-    kwargs['configs_path'] = cfgs_path
-    path_to_saved_model = download_weights(weights_path, **kwargs)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str,
+                        default='/home/work/PycharmProjects/app_debug_data/data/best.pt',
+                        help='initial weights path')
+    parser.add_argument('--cfgs', type=str,
+                        default='/home/work/PycharmProjects/app_debug_data/data/opt.yaml',
+                        help='path to model cfgs (required for ONNX anf TorchScript models)')
+    parser.add_argument('--image', type=str,
+                        default='./IMG_0748_big.jpeg',
+                        help='initial image path')
+    parser.add_argument('--mode',
+                        choices=['direct', 'sliding_window'],
+                        default='direct',
+                        help='inference mode')
+    parser.add_argument('--save_path', type=str,
+                        default=os.getcwd(),
+                        help='initial image path')
 
-    if 'pt' in weights_path:
-        if 'torchscript' in weights_path:
-            model = torch.jit.load(path_to_saved_model)
-            return model
-        else:
-            model = attempt_load(weights=path_to_saved_model)  # , map_location=device
-            kwargs['model'] = model
-            return model, kwargs
-    if 'onnx' in weights_path:
-        model = onnx_inference(path_to_saved_model)
-        return model
+    opt = parser.parse_args()
 
+    cfgs = get_configs(opt.cfgs)
+    input_img_size = cfgs['img_size']
 
-def infer_torch_model(torch_script_model, tensor):
-    # simple inference for torchScript:
-    torch_script_model_inference = torch_script_model(tensor)[0]
-    return torch_script_model_inference
+    # load and prepare image
+    tensor, image = get_image(opt.image)
+    # load and prepare models
+    model = get_model(opt.weights)
+    # infer prepared image
+    infer_fn = infer_model if opt.mode == 'direct' else sliding_window
+    output = infer_fn(model, tensor, input_img_size=input_img_size)
 
-
-def infer_onnx_model(onnx_model, tensor):
-    # simple inference for ONNX:
-    onnx_model, input_name, label_name = onnx_model
-    onnx_model_inference = onnx_model.run([label_name], {input_name: to_numpy(tensor).astype(np.float32)})[0]
-    return onnx_model_inference
-
-
-def infer_model(model_, image, simple_inference=True, **kwargs):
-    if simple_inference:
-        infer_fn = infer_onnx_model if isinstance(model, tuple) else infer_torch_model
-        if len(image.shape) == 3:
-            slice = image.unsqueeze(0)
-        if slice.max() > 1:
-            slice = slice / 255
-        height, width = kwargs['input_iamge_size']
-        if slice.shape[2] > height or slice.shape[3] > width:
-            slice = slice[..., :height, :width]
-
-        model_inference = infer_fn(model_, slice)
-        output = non_max_suppression(model_inference,
-                                     conf_thres=kwargs['conf_threshold'],
-                                     iou_thres=kwargs['iou_threshold'],
-                                     agnostic=kwargs['agnostic'])
-    else:
-        output = sliding_window_approach(model, image, **kwargs)
-    return output
+    # # metadata construction stage
+    model = get_model(opt.weights)
+    meta = construct_model_meta(model)
+    names = model.module.names if hasattr(model, 'module') else model.names
+    save_path = os.path.join(opt.save_path, "vis_detection.jpg")
+    visualize_dets(image, output, save_path, names, meta)
 
 
-customWeightsPath = '/yolov5_train/Lemons-aug/8342_008/weights/best.pt'
-customWeightsPath_torchScript = '/yolov5_train/Lemons-aug/8342_008/weights/best.torchscript.pt'
-customWeightsPath_onnx = "/yolov5_train/Lemons-aug/8342_008/weights/best.onnx"
-
-# models init stage
-model, kwargs = prepare_model(customWeightsPath, **kwargs)
-torch_script_model = prepare_model(customWeightsPath_torchScript, **kwargs)
-onnx_model = prepare_model(customWeightsPath_onnx, **kwargs)
-
-# metadata construction stage
-meta = construct_model_meta(model)
-
-# download target image for inference
-path_to_image = './IMG_0748_big.jpeg'
-big_image = Image.open(path_to_image)
-tensor = transforms.PILToTensor()(big_image)
-image = transforms.ToPILImage()(tensor)
-
-# set additional cfgs
-try:
-    H, W = model.img_size
-    kwargs['input_iamge_size'] = [H, W]
-except:
-    with open(kwargs['configs_path'], 'r') as yaml_file:
-        cfgs = yaml.load(yaml_file)
-    kwargs['input_iamge_size'] = cfgs['img_size']  # custom model input image size
-
-kwargs['conf_threshold'] = 0.25
-kwargs['iou_threshold'] = 0.45
-kwargs['agnostic'] = False
-
-rez = infer_model(onnx_model, tensor, simple_inference=False, **kwargs)
-visualize_dets(img0=image, output=rez, save_path=os.getcwd(), **kwargs)
+if __name__ == '__main__':
+    main()
