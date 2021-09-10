f44a6e4d530a6709627f8ac89adaf3998f66202f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-10 14:24:53 +0200

    #Save section

--- detect.py
+++ detect.py
@@ -245,10 +245,10 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
     print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
 
+    # Save
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         print(f"Results saved to {colorstr('bold', save_dir)}{s}")
-
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
 
