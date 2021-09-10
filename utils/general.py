6c09812fcede5c5edd7ff85ce6052b16c0da6ccd
positive666 <286040359@qq.com>
2021-09-10 17:36:16 +0800

    update with  ultralytics yolov5

--- utils/general.py
+++ utils/general.py
@@ -101,6 +101,10 @@ def get_latest_run(search_dir='.'):
     # Return path to most recent 'last.pt' in /runs (i.e. to --resume from)
     last_list = glob.glob(f'{search_dir}/**/last*.pt', recursive=True)
     return max(last_list, key=os.path.getctime) if last_list else ''
+    
+def is_writeable(path):
+    # Return True if path has write permissions (Warning: known issue on Windows)
+    return os.access(path, os.R_OK)
 
 
 def is_docker():
