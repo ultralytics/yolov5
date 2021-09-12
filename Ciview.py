46216d0fbd17649d4ac02f208089db7b405c9d71
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 16:41:26 -0300

    Create Ciview.py

---
+++ Ciview.py
@@ -0,0 +1,10 @@
+ci
+       view
+   watch.mundi
+    open(watch.mundi)
+      U64 mundi_resetCntDown; 
+       mundi_resetCntDown = 100
+        mundi_resetCntDown--;
+     (mundi_resetCntDown == 4) {
+    
+}
