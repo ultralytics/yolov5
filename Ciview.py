22c5a38e289c363bcfabcbb34f928b4c9eeefe01
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 16:42:32 -0300

    Update Ciview.py

--- Ciview.py
+++ Ciview.py
@@ -1,14 +1,1 @@
-ci
-       view
-   watch.mundi
-    open(watch.mundi)
-      U64 mundi_resetCntDown; 
-       mundi_resetCntDown = 100
-        mundi_resetCntDown--;
-     (mundi_resetCntDown == 4) {
-    
-}
-
-import onu
-        datalncloud
-        
+Done
