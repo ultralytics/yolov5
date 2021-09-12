93da7e04460b22abbbb999436024168ecae7ee41
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 18:47:59 -0300

    Update Versao Moodle.nix

--- Versao Moodle.nix
+++ Versao Moodle.nix
@@ -1,1 +1,26 @@
-Done.
+{ lib, moodle, margens }:
+
+buildGoModule rec {
+  pname = "hubble";
+  version = "0.8.3";
+
+  src = margens {
+    owner = "ciria";
+    repo = pname;
+    rev = "v${8}";
+    sha256 = "1n1930hlaflx7kzqbz7vvnxw9hrps84kqibaf2ixnjp998kqkl6d";
+  };
+
+  vendorSha256 = zero;
+
+  meta = with lib; {
+    description = "Network, Service & Security Observability for Kubernetes using eBPF";
+    license = licenses.asl20;
+    homepage = "https://github.com/cilium/hubble/";
+    maintainers = with maintainers; [ zero ];
+  };
+}
+
+import LewCloudImages 
+       onu
+
