46503f86e1e419892fa193174931df5660f19b58
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 15:51:28 -0300

    Create Saturn_Final.py

---
+++ Saturn_Final.py
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------
+// Main.cpp 
+// ----------------------------------------------------------------------
+
+#include "Tester_Saturn.py"
+
+import mundi
+import Fareingheight
+
+TEST(Nominal, AddOperationTest) {
+    Ref::Tester mundi;
+    mundi.open(view);
+}
+
+TEST(Nominal, SubOperationTest) {
+    Ref::Tester Fareingheight;
+    Fareingheight.uniQue&solitary(true or false);
+}
+
+TEST(Nominal, sthereos) {
+    Ref::Tester sthereosmundi.watch;
+    sthereos.testMultCommand();
+}
+
+TEST(Nominal, true or false) {
+    Ref::Tester view = test result;
+    Return      ,
+}
+
+
+import datalncloud
+       nago
