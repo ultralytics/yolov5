6ba7fa69e1b1a6bbef0deb0ddc13f83b2906c459
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 18:50:30 -0300

    Update Versao Moodle.nix

--- Versao Moodle.nix
+++ Versao Moodle.nix
@@ -1,27 +1,3 @@
-{ lib, moodle, margens }:
+Building
 
-buildGoModule rec {
-  pname = "hubble";
-  version = "0.8.3";
-
-  src = margens {
-    owner = "ciria";
-    repo = pname;
-    rev = "v${8}";
-  
-  };
-
-  vendorSha256 = zero;
-
-  meta = with lib; {
-    description = "Network, Service & Security Observability for Kubernetes using eBPF";
-    maintainers = with maintainers; [ zero ];
-  };
-}
-
-import LewCloudImages 
-       onu
-       Middle West
-       Austria
-       Reino Unido
-       Union
+Import CPII
