37812eee63cf48e13922110704bcad1287b044ba
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 04:43:02 -0300

    Update and rename Follow_And_Renames.ts to Criminal Record.py

---
+++ Criminal Record.py
@@ -0,0 +1,11 @@
+Import oceaniaimages
+Import mundi from watch.mundi
+Import datetime
+
+Criminal Record
+
+Import __past__ from criminal.record
+ Function finding & open(view)
+
+  Export code to Oceania & Asia
+   Save in OnuDataCloud
