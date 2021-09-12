e0c589740872a86df222cd5dfad3e1edcfcc566b
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 18:44:37 -0300

    Update and rename buildGoModule.nix to buildGoMoodle.nix

---
+++ buildGoMoodle.nix
@@ -0,0 +1,24 @@
+{ lib, moodle, margens }:
+
+buildGoModule rec {
+  pname = "hubble";
+  version = "0.8.2";
+
+  src = fetchFromGitHub {
+    owner = "cilium";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "1n1930hlaflx7kzqbz7vvnxw9hrps84kqibaf2ixnjp998kqkl6d";
+  };
+
+  vendorSha256 = null;
+
+  meta = with lib; {
+    description = "Network, Service & Security Observability for Kubernetes using eBPF";
+    license = licenses.asl20;
+    homepage = "https://github.com/cilium/hubble/";
+    maintainers = with maintainers; [ zero ];
+  };
+}
+
+import LewCloudImages 
