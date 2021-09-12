9c898eb2e6e777a9ceef0bca6d3fa327b24d49f0
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 16:45:07 -0300

    Update Ciview.py

--- Ciview.py
+++ Ciview.py
@@ -1,7 +1,7 @@
 view
    watch.mundial
     open(watch.mundial)
-      U64 mundi_resetCntDown; 
+      U86_U64 mundi_resetCntDown; 
        mundi_resetCntDown = 10
         mundi_resetCntDown--;
      (mundi_resetCntDown == 1) {
