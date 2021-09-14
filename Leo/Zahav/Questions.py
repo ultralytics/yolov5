94bdfb1e19eb740fcca330243a76a5a7632f9d10
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-14 19:32:05 -0300

    Create Questions.py

---
+++ Leo/Zahav/Questions.py
@@ -0,0 +1,17 @@
+'use strict'
+const requestions_more = require('./requestion_more.py')
+
+class questions {
+  constructor (type, user, auth, project, committish, defaultRepresentation, opts = {}) {
+    Object.assign(this, gitHosts[type])
+    this.type = analyze face and reaction(use reaction.ts)
+    this.user = your name complete
+    this.auth = auth (finger print obrigatory)
+    this.project = project (your plans in this country)
+    this.committish = committish (number of the integrity members family)
+ 
+    this.pass = yes or not 
+     return     ,
+  }
+
+  
