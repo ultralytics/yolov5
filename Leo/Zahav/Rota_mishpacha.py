7eb930fc41a2d37ad922d9f5db8788530f213a00
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:52:03 -0300

    Rename Rota_mish.py to Rota_mishpacha.py

---
+++ Leo/Zahav/Rota_mishpacha.py
@@ -0,0 +1,10 @@
+import serio
+import requests_more from questions
+import to Monaco
+          UnitedKingdom
+          USA
+          Austria
+          Australia
+          Israel
+          Canada
+          Cuba
