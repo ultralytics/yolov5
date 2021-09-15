19c5ed2e8ed6aeac1cc26469f802f3a5122c09cd
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:13:28 -0300

    Create Router.brl.py

---
+++ Leo/Zahav/Router.brl.py
@@ -0,0 +1,14 @@
+Import mundi
+ Export to onucloud
+
+Const mundi = ('/mundi.py')
+ New Gps = mapamundi Brazil.router.mundi
+  Brazil.router.mundi = new file(router.brl.py)
+
+Const router.brl = ('/router.brl.py')
+  Import to DRF and DPF
+ 
+Export to oceania
+          PolinterCloud
+          Union
+
