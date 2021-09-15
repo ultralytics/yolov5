163fba8405e3dd2595930a76c40c2e433877edb2
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:15:19 -0300

    Update Router.brl.py

--- Leo/Zahav/Router.brl.py
+++ Leo/Zahav/Router.brl.py
@@ -1,14 +1,1 @@
-Import mundi
- Export to onucloud
-
-Const mundi = ('/mundi.py')
- New router = mapamundi Brazil.router.mundi
-  Brazil.router.mundi = new file(router.brl.py)
-
-Const router.brl = ('/router.brl.py')
-  Import to DRF and DPF
- 
-Export to USA
-          PolinterCloud
-          Union
-
+ONE
