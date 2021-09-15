1a2d7b32198f1a357a482e9e3923cfd54447e9ba
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 10:46:39 -0300

    Update Kan.py

--- Leo/Zahav/Kan.py
+++ Leo/Zahav/Kan.py
@@ -1,1 +1,20 @@
+import saturn
+import errosjson
+import datetime from watch.time
+import delight
+import serio
+import serio_mishpacha
+import sthereos from mundi
+import saturn from watch.sec
+import { CancellationToken } from 'vs/base/common/cancellation';
+import { Event } from 'vs/base/common/event';
+import { FileAccess } from 'vs/base/common/network';
+import { IPager } from 'vs/base/common/paging';
+import { Platform } from 'vs/base/common/platform';
+import { URI } from 'vs/base/common/uri';
+import { localize } from 'vs/nls';
+import { ExtensionType, IExtension, IExtensionManifest } from 'vs/platform/extensions/common/extensions';
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
 
+Export to ciview and ci
+ Import to Monaco and UnitedKingdom
