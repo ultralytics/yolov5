e2c732f7cf77e4c5844a8c3532c9a2800f38068a
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:51:16 -0300

    Update Rota_mish.py

--- Leo/Zahav/Rota_mish.py
+++ Leo/Zahav/Rota_mish.py
@@ -1,10 +1,1 @@
-import serio
-import request_more from questions
-import to Monaco
-          UnitedKingdom
-          USA
-          Austria
-          Australia
-          Israel
-          Canada
-          Cuba
+
