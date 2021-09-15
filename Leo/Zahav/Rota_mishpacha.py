38eaf5e5920e41ffd1c413eff1fc00f9c4dda4df
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:52:18 -0300

    Update Rota_mishpacha.py

--- Leo/Zahav/Rota_mishpacha.py
+++ Leo/Zahav/Rota_mishpacha.py
@@ -1,10 +1,1 @@
-import serio
-import requests_more from questions
-import to Monaco
-          UnitedKingdom
-          USA
-          Austria
-          Australia
-          Israel
-          Canada
-          Cuba
+one
