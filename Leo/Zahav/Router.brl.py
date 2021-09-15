d275c145457a2eae6ff0a7c0409ccd6d6ed29869
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:50:27 -0300

    Update Router.brl.py

--- Leo/Zahav/Router.brl.py
+++ Leo/Zahav/Router.brl.py
@@ -1,1 +1,10 @@
-ONE
+import serio
+import questions
+import to Monaco
+          UnitedKingdom
+          USA
+          Austria
+          Australia
+          Israel
+          Canada
+          Cuba
