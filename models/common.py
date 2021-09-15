b34658e0a2e9285d812d81f8204d48e9b1216a46
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-15 15:46:51 +0200

    `Focus.forward()` restructure
    Focus layer restructure to reduce memory usage and eliminate ops in ONNX and CoreML exports (TFLite unchanged). 
    
    Before/after profiling code:
    ```python
    class Focus(nn.Module):
        # Focus wh information into c-space
        def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
            super().__init__()
            self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
    
        def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
            return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
    
    class FocusAlternate(nn.Module):
        # Focus wh information into c-space
        def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
            super().__init__()
            self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
    
        def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
            a, b = x[:, :, ::2], x[:, :, 1::2]
            return self.conv(torch.cat([a[..., ::2], b[..., ::2], a[..., 1::2], b[..., 1::2]], 1))
    
    
    m1 = Focus(3, 64, 3)  # YOLOv5 Focus layer
    m2 = FocusAlternate(3, 64, 3)
    
    results = profile(input=torch.randn(16, 3, 640, 640), ops=[m1, m2], n=10, device=0)  # profile both 10 times at batch-size 16
    ```
    
    Results:
    ```python
    YOLOv5 🚀 v5.0-433-g621b6d5 torch 1.9.0+cu102 CUDA:0 (Tesla T4, 15109.75MB)
    
          Params      GFLOPs  GPU_mem (GB)  forward (ms) backward (ms)                   input                  output
            7040       23.07         2.280          16.2         50.71       (16, 3, 640, 640)      (16, 64, 320, 320)
            7040       23.07         1.919         15.21          48.6       (16, 3, 640, 640)      (16, 64, 320, 320)
    ```

--- models/common.py
+++ models/common.py
@@ -196,15 +196,15 @@ class SPPF(nn.Module):
 
 
 class Focus(nn.Module):
-    # Focus wh information into c-space
+    # Focus spatial information to channel space
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super().__init__()
         self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
         # self.contract = Contract(gain=2)
 
     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
-        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
-        # return self.conv(self.contract(x))
+        a, b = x[:, :, ::2], x[:, :, 1::2]
+        return self.conv(torch.cat([a[..., ::2], b[..., ::2], a[..., 1::2], b[..., 1::2]], 1))
 
 
 class GhostConv(nn.Module):
