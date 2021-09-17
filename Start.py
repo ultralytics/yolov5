6a4b665d53564f230c2c8b48d4d0da7f4a13f13a
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-17 19:00:02 -0300

    Create Start.py

---
+++ Start.py
@@ -0,0 +1,21 @@
+.start(static_cast<Microsoft or Linux>(critical), static_cast<Microsoft or Linux>(stackSize), static_cast<Microsoft or Linux>(cpu affinity));
+import to mundi from sthereos
+import datetime
+import sattelite from saturn
+import jupiter from nasa
+import dev
+
+
+cd make new file with voicerecorder('./ahava.py')
+cd make new file with routerrecorder('/router.py')
+cd make new file with sthereos.mundi.sec('/farenheight.py')
+
+
+Run voicerecorder , routerrecorder & sthereos.mundi.sec
+Return ( pass or revoke );
+  Result      ,
+
+
+Export to ci
+  Export to onu
+
