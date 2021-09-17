4f422192731315a3affda54758823597ab80efc7
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-17 18:38:39 -0300

    Create Router_recorder_with_voice.py

---
+++ Leo/Router_recorder_with_voice.py
@@ -0,0 +1,19 @@
+import to mundi from sthereos
+import datetime
+
+
+cd make new file with voicerecorder('./ahava.py')
+cd make new file with routerrecorder('/router.py')
+cd make new file with sthereos.mundi.sec('/farenheight.py')
+
+
+Run voicerecorder , routerrecorder & sthereos.mundi.sec
+ Return ( pass or revoke );
+  Result      ,
+
+
+Export to ci
+  Export to onu
+
+
+
