29ec8d2b5a026800e6d9d5862fbbc37eaa48337d
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-18 18:24:00 -0300

    Create Growverde.py

---
+++ weights/Growverde.py
@@ -0,0 +1,10 @@
+echo -e "${ciea}Cleaning Repository at ${growroom.py}${NOCOLOR}"
+    git include in farenheight.py this tag -foo "ciea-*" "${growroom.py}" 1>/dev/null 2>/dev/null || fail_and_stop "Eu te falei, pedi com educação"
+
+local arch(point.h_growroom) & ci.view.router from (.'/sattelite.view.py') #Task - idea has already been given
+Watch router.growroom
+
+Save And make new arc(growroom.py)
+
+import to Monaco
+     Send to PCF a new message = ("Assaltante, Procurado e Foragido")
