d6e34cdb757873c3ba6e622b1888449b7905deb5
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-21 17:46:10 -0300

    Create É_Gol.py

---
+++ É_Gol.py
@@ -0,0 +1,14 @@
+import mundi from nasa
+import saturn from ci view
+import sthereos from watch.sec
+import ci
+import onu
+
+
+goal.xeta_start = 1.46
+goal.xota_start = 0
+goal.depth = 0.01
+goal.length = 0.1
+goal.ground_position = -0.155
+
+export and make new arc(choal.py) to leocloud
