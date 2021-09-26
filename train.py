a716174e714b75a56d0de301d093d8ad19639261
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-26 11:46:36 -0700

    add newline

--- train.py
+++ train.py
@@ -406,7 +406,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             if f.exists():
                 strip_optimizer(f)  # strip optimizers
                 if f is best:
-                    LOGGER.info(f'Validating {f}...')
+                    LOGGER.info(f'\nValidating {f}...')
                     results, _, _ = val.run(data_dict,
                                             batch_size=batch_size // WORLD_SIZE * 2,
                                             imgsz=imgsz,
