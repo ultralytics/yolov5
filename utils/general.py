e9e92f2a5977e3a6c7e49c4691886e965316448b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-26 16:35:24 -0700

    Scope `check_file()` search space
    `check_file()` now limited to searching opt-in directories: /data, /models, /utils. This prevents large non-project directories like /.git and /venv from being searched, which may cause `check_file()` to slow significantly.

--- utils/general.py
+++ utils/general.py
@@ -315,7 +315,9 @@ def check_file(file, suffix=''):
         assert Path(file).exists() and Path(file).stat().st_size > 0, f'File download failed: {url}'  # check
         return file
     else:  # search
-        files = glob.glob('./**/' + file, recursive=True)  # find file
+        files = []
+        for d in 'data', 'models', 'utils':  # search directories
+            files.extend(glob.glob(str(ROOT / d / '**' / file), recursive=True))  # find file
         assert len(files), f'File not found: {file}'  # assert file was found
         assert len(files) == 1, f"Multiple files match '{file}', specify exact path: {files}"  # assert unique
         return files[0]  # return file
