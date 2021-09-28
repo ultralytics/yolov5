a14403496d51935023ef79e9c5a77239237153ee
Jirka <jirka.borovec@seznam.cz>
2021-09-29 00:37:21 +0200

    detect typing

--- detect.py
+++ detect.py
@@ -10,6 +10,7 @@ import argparse
 import os
 import sys
 from pathlib import Path
+from typing import Union, Optional, Sequence
 
 import cv2
 import numpy as np
@@ -31,31 +32,31 @@ from utils.torch_utils import load_classifier, select_device, time_sync
 
 
 @torch.no_grad()
-def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
-        source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
-        imgsz=640,  # inference size (pixels)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project=ROOT / 'runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
+def run(weights: Union[str, Path] = ROOT / 'yolov5s.pt',  # model.pt path(s)
+        source: Union[str, Path] = ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
+        imgsz: int = 640,  # inference size (pixels)
+        conf_thres: float = 0.25,  # confidence threshold
+        iou_thres: float = 0.45,  # NMS IOU threshold
+        max_det: int = 1000,  # maximum detections per image
+        device: str = '',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img: bool = False,  # show results
+        save_txt: bool = False,  # save results to *.txt
+        save_conf: bool = False,  # save confidences in --save-txt labels
+        save_crop: bool = False,  # save cropped prediction boxes
+        nosave: bool = False,  # do not save images/videos
+        classes: Optional[Sequence]=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms: bool = False,  # class-agnostic NMS
+        augment: bool = False,  # augmented inference
+        visualize: bool = False,  # visualize features
+        update: bool = False,  # update all models
+        project: Union[str, Path] = ROOT / 'runs/detect',  # save results to project/name
+        name: str = 'exp',  # save results to project/name
+        exist_ok: bool = False,  # existing project/name ok, do not increment
+        line_thickness: int = 3,  # bounding box thickness (pixels)
+        hide_labels: bool = False,  # hide labels
+        hide_conf: bool = False,  # hide confidences
+        half: bool = False,  # use FP16 half-precision inference
+        dnn: bool = False,  # use OpenCV DNN for ONNX inference
         ):
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
