5ed28603cf94185c28da02b3d8bb433118ac33d4
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-27 18:26:41 -0700

    Update relative `ROOT` logic (#4955)
    * Update relative `ROOT` logic
    
    * python 3.9 Path().is_relative_to() removal

--- models/tf.py
+++ models/tf.py
@@ -20,6 +20,7 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = ROOT.relative_to(Path.cwd())  # relative
 
 import numpy as np
 import tensorflow as tf
