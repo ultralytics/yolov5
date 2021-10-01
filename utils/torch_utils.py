190547a728e7e5d32cc5368c3e937b2efb3670a9
zhiqiang <zhiqwang@outlook.com>
2021-10-02 03:19:30 +0800

    Fixing exception in thop

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -112,10 +112,10 @@ def profile(input, ops, n=10, device=None):
             m = m.to(device) if hasattr(m, 'to') else m  # device
             m = m.half() if hasattr(m, 'half') and isinstance(x, torch.Tensor) and x.dtype is torch.float16 else m
             tf, tb, t = 0., 0., [0., 0., 0.]  # dt forward, backward
-            try:
-                flops = thop.profile(m, inputs=(x,), verbose=False)[0] / 1E9 * 2  # GFLOPs
-            except:
+            if thop is None:
                 flops = 0
+            else:
+                flops = thop.profile(m, inputs=(x,), verbose=False)[0] / 1E9 * 2  # GFLOPs
 
             try:
                 for _ in range(n):
