ccd47a06a75c3ab33e98ee1f848c27814515798c
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-06 20:14:22 -0700

    Update

--- utils/autobatch.py
+++ utils/autobatch.py
@@ -15,7 +15,7 @@ from utils.torch_utils import de_parallel, profile
 def autobatch(model, imgsz=640, fraction=0.9):
     # Automatically compute optimal batch size to use `fraction` of available CUDA memory
     prefix = colorstr('autobatch: ')
-    print(f'\n{prefix} Computing optimal batch size for --imgsz {imgsz}')
+    print(f'{prefix}Computing optimal batch size for --imgsz {imgsz}')
 
     device = next(model.parameters()).device  # get model device
     t = torch.cuda.get_device_properties(device).total_memory / 1024 ** 3  # (GB)
@@ -23,7 +23,7 @@ def autobatch(model, imgsz=640, fraction=0.9):
     a = torch.cuda.memory_allocated(device) / 1024 ** 3  # (GB)
     f = t - (r + a)  # free inside reserved
     # f = 15.8
-    print(f'\n{prefix} {t:.3g}G total, {r:.3g}G reserved, {a:.3g}G allocated, {f:.3g}G free')
+    print(f'{prefix}{t:.3g}G total, {r:.3g}G reserved, {a:.3g}G allocated, {f:.3g}G free')
 
     batch_sizes = [1, 2, 4, 8]
     model = deepcopy(de_parallel(model)).train()
@@ -35,6 +35,7 @@ def autobatch(model, imgsz=640, fraction=0.9):
         print()
 
     p = np.polyfit(batch_sizes, y, deg=1)  # first degree polynomial fit
+    print(batch_sizes, y, p)
     f_intercept = int((f * fraction - p[0]) / p[1])  # optimal batch size
     return f_intercept
 
