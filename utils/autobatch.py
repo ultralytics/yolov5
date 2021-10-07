9bab7579c660e5a49b55e7cbe311e9f5b304dffa
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-06 19:48:26 -0700

    fix mem2

--- utils/autobatch.py
+++ utils/autobatch.py
@@ -21,17 +21,16 @@ def autobatch(model, imgsz=640, fraction=0.8, device=0):
     t = torch.cuda.get_device_properties(device).total_memory / 1024 ** 3  # (GB)
     r = torch.cuda.memory_reserved(device) / 1024 ** 3  # (GB)
     a = torch.cuda.memory_allocated(device) / 1024 ** 3  # (GB)
-    f = r - a  # free inside reserved
+    f = t - (r + a)  # free inside reserved
 
+    batch_sizes = [1, 2, 4, 8]
+    x = []
     try:
-        batch_sizes = [1, 2, 4, 8]
         print(f'\n{prefix} {t:.3g}G total, {r:.3g}G reserved, {a:.3g}G allocated, {f:.3g}G free')
     except Exception as e:
         print()
 
-
-    #x, y = zip(*x)
-    #p = np.polyfit(x, y)
-
+    # x, y = zip(*x)
+    # p = np.polyfit(x, y)
 
     return None
