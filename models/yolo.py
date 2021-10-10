1030165b5e31d3ce9b91b1a10fbbfc7c654b7f3f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-10 14:07:48 -0700

    Only apply if Detect() is last layer
    Co-authored-by: Jebastin Nadar <njebastin10@gmail.com>

--- models/yolo.py
+++ models/yolo.py
@@ -235,6 +235,7 @@ class Model(nn.Module):
     def _apply(self, fn):
         self = super()._apply(fn)
         m = self.model[-1]  # Detect()
+        if isinstance(m, Detect):
         m.stride = fn(m.stride)
         m.grid = list(map(fn, m.grid))
         return self
