153873e9e4f42a10bab9e3f2467344b857bc05a2
Malte Lorbach <49158835+maltelorbach@users.noreply.github.com>
2021-10-11 18:47:24 +0200

    Fix `ROOT` as relative path (#5129)
    * use os.path.relpath instead of relative_to
    
    * use os.path.relpath instead of relative_to
    
    * Remove os.path from val.py
    
    * Remove os.path from train.py
    
    * Update detect.py import to os
    
    * Update export.py import to os
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- detect.py
+++ detect.py
@@ -7,6 +7,7 @@ Usage:
 """
 
 import argparse
+import os
 import sys
 from pathlib import Path
 
@@ -19,7 +20,7 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = ROOT.relative_to(Path.cwd())  # relative
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.experimental import attempt_load
 from utils.datasets import LoadImages, LoadStreams
