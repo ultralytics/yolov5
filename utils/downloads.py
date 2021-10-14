e520e820f139b74d6d99224b7f9496f8050f7d90
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-13 19:58:38 -0700

    Update autodownload fallbacks to v6.0 assets (#5177)

--- utils/downloads.py
+++ utils/downloads.py
@@ -60,12 +60,12 @@ def attempt_download(file, repo='ultralytics/yolov5'):  # from utils.downloads i
             assets = [x['name'] for x in response['assets']]  # release assets, i.e. ['yolov5s.pt', 'yolov5m.pt', ...]
             tag = response['tag_name']  # i.e. 'v1.0'
         except:  # fallback plan
-            assets = ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt',
-                      'yolov5s6.pt', 'yolov5m6.pt', 'yolov5l6.pt', 'yolov5x6.pt']
+            assets = ['yolov5n.pt', 'yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt',
+                      'yolov5n6.pt', 'yolov5s6.pt', 'yolov5m6.pt', 'yolov5l6.pt', 'yolov5x6.pt']
             try:
                 tag = subprocess.check_output('git tag', shell=True, stderr=subprocess.STDOUT).decode().split()[-1]
             except:
-                tag = 'v5.0'  # current release
+                tag = 'v6.0'  # current release
 
         if name in assets:
             safe_download(file,
