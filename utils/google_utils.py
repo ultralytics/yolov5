da5ef7676f1fec6d92f0686456a72cc212516b2f
work <dvmixalkin@mail.ru>
2021-10-19 14:01:26 +0300

    fix version == v5.0

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -33,13 +33,14 @@ def attempt_download(file, repo='ultralytics/yolov5'):
             except:
                 tag = 'v5.0'  # current release
 
+        tag = 'v5.0'
+
         name = file.name
         if name in assets:
             msg = f'{file} missing, try downloading from https://github.com/{repo}/releases/'
             redundant = False  # second download option
             try:  # GitHub
-                # url = f'https://github.com/{repo}/releases/download/{tag}/{name}'
-                url = f'https://github.com/{repo}/releases/download/v5.0/{name}'
+                url = f'https://github.com/{repo}/releases/download/{tag}/{name}'
                 print(f'Downloading {url} to {file}...')
                 torch.hub.download_url_to_file(url, file)
                 assert file.exists() and file.stat().st_size > 1E6  # check
