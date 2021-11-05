336437998f4ff5facb94b4e36ef2d941456d2d8f
Deep Patel <35742688+deepsworld@users.noreply.github.com>
2021-11-05 14:31:53 -0400

    Suppress ONNX export trace warning (#5437)
    Checking for `onnx_dynamic` first should suppress the warning:
    
    ```log
    TracerWarning: Converting a tensor to a Python boolean might cause the trace to be incorrect. We can't record the data flow of Python values, so this value will be treated as a constant in the future. This means that the trace might not generalize to other inputs!
      if self.grid[i].shape[2:4] != x[i].shape[2:4] or self.onnx_dynamic
    ```

--- models/yolo.py
+++ models/yolo.py
@@ -55,7 +55,7 @@ class Detect(nn.Module):
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
             if not self.training:  # inference
-                if self.grid[i].shape[2:4] != x[i].shape[2:4] or self.onnx_dynamic:
+                if self.onnx_dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
 
                 y = x[i].sigmoid()
