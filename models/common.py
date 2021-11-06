cc992c5f63d6f06b57ffb2bf0984ba7ea3e0877d
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-06 19:16:01 +0100

    Fix `save_one_box()`

--- models/common.py
+++ models/common.py
@@ -18,9 +18,8 @@ from PIL import Image
 from torch.cuda import amp
 
 from utils.datasets import exif_transpose, letterbox
-from utils.general import (colorstr, increment_path, make_divisible, non_max_suppression, save_one_box, scale_coords,
-                           xyxy2xywh)
-from utils.plots import Annotator, colors
+from utils.general import colorstr, increment_path, make_divisible, non_max_suppression, scale_coords, xyxy2xywh
+from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import time_sync
 
 LOGGER = logging.getLogger(__name__)
