0b07c0c4b539ee6cf10616e894434b69b096d7ff
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-07 16:47:58 +0100

    detect fix

--- detect.py
+++ detect.py
@@ -102,7 +102,9 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         t1 = time_sync()
         if pt:
             im = torch.from_numpy(im).to(device)
-        im = im.half() if half else im.float()  # uint8 to fp16/32
+            im = im.half() if half else im.float()  # uint8 to fp16/32
+        else:
+            im = im.astype('float32')
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
