71b320aa836aa86706ac89ff255181c76d301bea
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-07 15:53:30 +0100

    warmup fix

--- detect.py
+++ detect.py
@@ -96,7 +96,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
     # Run inference
     if pt and device.type != 'cpu':
-        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # run once
+        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
     for path, im, im0s, vid_cap, s in dataset:
         t1 = time_sync()
