063ab6af1844fdf1ee10601059900657596e0a2a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-09 22:47:43 +0100

    notebook_init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,17 +1,18 @@
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
 """
-utils/ initialization
+utils/initialization
 """
 
 
 def notebook_init():
-    # For use in YOLOv5 notebooks
+    # For YOLOv5 notebooks
     print('Checking setup...')
-    from IPython.display import clear_output  # to display images
+    from IPython import display  # to display images and clear console output
 
     from utils.general import emojis
     from utils.torch_utils import select_device  # YOLOv5 imports
 
-    clear_output()
+    display.clear_output()
     select_device(newline=False)
     print(emojis('Setup complete ✅'))
+    return display
