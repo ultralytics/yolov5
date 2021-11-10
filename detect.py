9809d261449768983aae3de5847d9b51295f3f20
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-10 13:49:31 +0100

    simplify

--- detect.py
+++ detect.py
@@ -118,7 +118,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         dt[1] += t3 - t2
 
         # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det, multi_label=multi_label)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, multi_label, max_det=max_det)
         dt[2] += time_sync() - t3
 
         # Second-stage classifier (optional)
