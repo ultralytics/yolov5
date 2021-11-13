590ecfac01bb3ec0f0200447f43b84c3457c0315
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-13 15:29:39 +0100

    Update autoanchor.py

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -21,7 +21,7 @@ def check_anchor_order(m):
     da = a[-1] - a[0]  # delta a
     ds = m.stride[-1] - m.stride[0]  # delta s
     if da.sign() != ds.sign():  # same order
-        LOGGER.info('Reversing anchor order')
+        LOGGER.info(f'{PREFIX}Reversing anchor order')
         m.anchors[:] = m.anchors.flip(0)
 
 
