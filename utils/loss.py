baff390dd7a547fca35f9d0147add27d043b8dbf
lwxnba <lwxnba@gmail.com>
2021-11-15 16:08:22 +0800

    update multi-label training implementation

--- utils/loss.py
+++ utils/loss.py
@@ -254,9 +254,3 @@ class ComputeLoss:
             tcls.append(c)  # class
 
         return tcls, tbox, indices, anch
-
-class Dictlist(dict):
-    def __setitem__(self, key, value):
-        if key not in self:
-            super().__setitem__(key, [])
-        self[key].append(value)
