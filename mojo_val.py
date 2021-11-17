0aeb8343a9e59900b0e5ee9133345ef7f86c2f11
vnoblet <vincent@nanovare.com>
2021-11-17 15:09:46 +0000

    Generate 3 plots worst TP / FN / FP

--- mojo_val.py
+++ mojo_val.py
@@ -4,6 +4,9 @@ import cv2
 from aisa_utils.dl_utils.utils import plot_object_count_difference_ridgeline
 from torchvision.ops import box_iou
 
+from plotly.subplots import make_subplots
+import plotly.express as px
+
 from utils.general import save_one_box
 from utils.general import scale_coords, xywh2xyxy, clip_coords, xyxy2xywh
 
@@ -82,47 +85,57 @@ def draw_targets(frame, bboxes, matches, mask, rect=False):
 
 def generate_crop(extra_stats, predn_with_path):
     crop_box = predn_with_path[:4]
-    frame_idx = int(predn_with_path[6])
+    frame_idx = int(predn_with_path[-1])  # paths idx should be at the end
     frame_path = extra_stats[frame_idx][0]
     frame = cv2.imread(str(frame_path), 1)
 
-    frame_crop = save_one_box(crop_box, frame, BGR=True, save=False)
+    frame_crop = save_one_box(crop_box, frame, BGR=False, save=False)
     return frame_crop
+
+
+def plot_crops(crop_fp, title):
+    fig = make_subplots(rows=len(crop_fp) // 5 + 1, cols=5)
+
+    for n, image in enumerate(crop_fp):
+        fig.add_trace(px.imshow(image).data[0], row=int(n / 5) + 1, col=n % 5 + 1)
+
+    # the layout gets lost, so we have to carry it over - but we cannot simply do
+    # fig.layout = layout since the layout has to be slightly different for subplots
+    # fig.layout.yaxis in a subplot refers only to the first axis for example
+    # update_yaxes updates *all* axis on the other hand
+    if len(crop_fp):
+        layout = px.imshow(crop_fp[0], color_continuous_scale='gray').layout
+        fig.layout.coloraxis = layout.coloraxis
+        fig.update_xaxes(**layout.xaxis.to_plotly_json())
+        fig.update_yaxes(**layout.yaxis.to_plotly_json())
+    fig.layout.title = title
+    return fig
 
 
 def process_fp_fn(extra_stats):
     iouv = torch.linspace(0.5, 0.95, 10)
     single_cls = True
     niou = len(iouv)
+    false_neg, false_pos, true_pos = [], [], []
     for image_idx, (path, pred, labels, img1_shape, img0_shape, ratio_pad) in enumerate(extra_stats):
-        high_conf_fp = torch.zeros(0, pred.shape[1])
         nl = len(labels)
 
-        if len(pred) == 0:
-            continue
-
-        # Predictions
         if single_cls:
             pred[:, 5] = 0
         predn = pred.clone()
         scale_coords(img1_shape, predn[:, :4], img0_shape, ratio_pad)  # native-space pred
-
         # Evaluate
         if nl:
             tbox = xywh2xyxy(labels[:, 1:5])  # target boxes
             scale_coords(img1_shape, tbox, img0_shape, ratio_pad)  # native-space labels
             labelsn = torch.cat((labels[:, 0:1], tbox), 1)  # native-space labels
-            correct, false_positive = process_batch_with_missed_labels(predn, labelsn, iouv)
-
+            correct, false_negative = process_batch_with_missed_labels(predn, labelsn, iouv)
         else:
-            correct, false_positive = torch.zeros(pred.shape[0], niou, dtype=torch.bool)
-
-        targets_miss = false_positive[:, 0]  # iouv == 0.5
+            correct, false_negative = torch.zeros(pred.shape[0], niou, dtype=torch.bool)
+        targets_miss = false_negative[:, 0]  # iouv == 0.5
         pred_correct = correct[:, 0].numpy()
-        labelsn = labelsn[:, 1:] == 0.5
-        frame = cv2.imread(str(path), 1)
+        labelsn = labelsn[:, 1:]
 
-        # torch.stack(torch.ones(pred.shape[0]) * image_idx, pred, axis=0)
         if False:
             # Draw targets that are missed or not
             draw_targets(frame, labelsn.numpy(), np.logical_not(targets_miss.numpy()), np.ones(labels.shape[0]),  rect=True)
@@ -134,32 +147,28 @@ def process_fp_fn(extra_stats):
             cv2.destroyAllWindows()
 
         predn_with_path = torch.cat([predn, torch.ones((predn.shape[0], 1)) * image_idx], dim=1) # append image_idx
-        true_pos = predn_with_path[pred_correct]
-        false_pos = predn_with_path[np.logical_not(pred_correct)]
-        false_neg = labelsn[targets_miss][:, 1]
+        labelsn_with_path = torch.cat([labelsn, torch.ones((labelsn.shape[0], 1)) * image_idx], dim=1)  # append image_idx
 
-        if high_conf_fp.shape[0] != 0:
-            high_conf_fp = torch.cat([high_conf_fp, false_pos])
-            high_conf_fp = high_conf_fp[torch.argsort(high_conf_fp[:, 4], descending=True)]
-            high_conf_fp = high_conf_fp[:5]
-        else:
-            high_conf_fp = false_pos
+        true_pos.append(predn_with_path[pred_correct])
+        false_pos.append(predn_with_path[np.logical_not(pred_correct)])
+        false_neg.append(labelsn_with_path[targets_miss])
+    true_pos = torch.cat(true_pos)
+    false_pos = torch.cat(false_pos)
+    false_neg = torch.cat(false_neg)
+    n_worst = 10
 
-        from plotly.subplots import make_subplots
-        import plotly.express as px
-        crop_fp = [generate_crop(extra_stats, crop) for crop in high_conf_fp]
+    fig = []
 
-        fig = make_subplots(rows=len(crop_fp) // 5 + 1, cols=5)
+    true_pos = true_pos[torch.argsort(true_pos[:, 4], descending=False)][:n_worst]
+    crop_tp = [generate_crop(extra_stats, crop) for crop in true_pos]
+    fig.append(plot_crops(crop_tp, "True Positive with lowest confidence threshold"))
 
-        for n, image in enumerate(crop_fp):
-            fig.add_trace(px.imshow(255 - image).data[0], row=int(n / 5) + 1, col=n % 5 + 1)
+    false_pos = false_pos[torch.argsort(false_pos[:, 4], descending=True)][:n_worst]
+    crop_fp = [generate_crop(extra_stats, crop) for crop in false_pos]
+    fig.append(plot_crops(crop_fp, "False Positive with highest confidence threshold"))
 
-        # the layout gets lost, so we have to carry it over - but we cannot simply do
-        # fig.layout = layout since the layout has to be slightly different for subplots
-        # fig.layout.yaxis in a subplot refers only to the first axis for example
-        # update_yaxes updates *all* axis on the other hand
-        layout = px.imshow(crop_fp[0], color_continuous_scale='gray').layout
-        fig.layout.coloraxis = layout.coloraxis
-        fig.update_xaxes(**layout.xaxis.to_plotly_json())
-        fig.update_yaxes(**layout.yaxis.to_plotly_json())
-        fig.show()
+    false_neg = false_neg[torch.argsort(false_neg[:, 4], descending=False)][:n_worst]
+    crop_fn = [generate_crop(extra_stats, crop) for crop in false_neg]
+    fig.append(plot_crops(crop_fn, "False Negative with lowest confidence threshold"))
+
+    return fig
