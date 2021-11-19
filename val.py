f79db383378422c33f88b393ada481453fc8d838
imyhxy <imyhxy@gmail.com>
2021-11-19 17:55:44 +0800

    feat: support detect with fp16 engine

--- val.py
+++ val.py
@@ -126,7 +126,7 @@ def run(data,
         model = DetectMultiBackend(weights, device=device, dnn=dnn)
         stride, pt, engine = model.stride, model.pt, model.engine
         imgsz = check_img_size(imgsz, s=stride)  # check image size
-        half &= pt and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+        half &= (pt or engine) and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
         if pt:
             model.model.half() if half else model.model.float()
         elif engine:
