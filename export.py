13728d39f003dcfdae99ac124b22d048e7fdf50b
Diego Montes <d57montes@hotmail.com>
2021-12-09 17:14:44 -0500

    fix agnostic implies nms

--- export.py
+++ export.py
@@ -383,7 +383,7 @@ def run(data=ROOT / 'data/coco128.yaml',  # 'dataset.yaml path'
     if any(tf_exports):
         pb, tflite, tfjs = tf_exports[1:]
         assert not (tflite and tfjs), 'TFLite and TF.js models must be exported separately, please pass only one type.'
-        model = export_saved_model(model, im, file, dynamic, tf_nms=nms or tfjs, agnostic_nms=agnostic_nms or tfjs,
+        model = export_saved_model(model, im, file, dynamic, tf_nms=nms or agnostic_nms or tfjs, agnostic_nms=agnostic_nms or tfjs,
                                    topk_per_class=topk_per_class, topk_all=topk_all, conf_thres=conf_thres,
                                    iou_thres=iou_thres)  # keras model
         if pb or tfjs:  # pb prerequisite to tfjs
