e6b91fad345543b1b4ed97694dffa7ed530c0f0a
Awsaf <awsaf49@gmail.com>
2021-12-24 21:53:20 +0600

    fix `dimension` for epoch
    ValueError: all the input arrays must have same number of dimensions

--- train.py
+++ train.py
@@ -371,7 +371,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             if fi > best_fitness:
                 best_fitness = fi
                 if loggers.wandb:
-                    br = np.concatenate([np.array(results).reshape(-1)[:4], epoch])
+                    br = np.concatenate([np.array(results).reshape(-1)[:4], [epoch]])
                     for i, name in enumerate(['P', 'R', 'mAP@.5', 'mAP@.5-.95', 'epoch']):
                         loggers.wandb.wandb_run.summary[f'best/{name}'] = br[i]  # log best results in the summary
             log_vals = list(mloss) + list(results) + lr
