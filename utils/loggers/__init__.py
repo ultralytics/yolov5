3a552ff3f92ae515e2c9a4cd464bbe704b497a81
ayush chaurasia <ayush.chaurarsia@gmail.com>
2021-12-28 18:43:21 +0530

    update

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -151,7 +151,7 @@ class Loggers():
             self.wandb.log(x)
             self.wandb.log({"Results": [wandb.Image(str(f), caption=f.name) for f in files]})
             # Calling wandb.log. TODO: Refactor this into WandbLogger.log_model
-            if not self.opt.evolve: 
+            if not self.opt.evolve:
                 wandb.log_artifact(str(best if best.exists() else last), type='model',
                                    name='run_' + self.wandb.wandb_run.id + '_model',
                                    aliases=['latest', 'best', 'stripped'])
