c47a2d6e1305d1bb61ba1cd6e3e3ae047732caed
syurenuko <57836793+syurenuko@users.noreply.github.com>
2022-01-01 18:46:57 +0900

    Update metrics.py
    Recall重視

--- utils/metrics.py
+++ utils/metrics.py
@@ -14,7 +14,7 @@ import torch
 
 def fitness(x):
     # Model fitness as a weighted combination of metrics
-    w = [0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
+    w = [0.0, 1.0, 0.0, 0.0]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
     return (x[:, :4] * w).sum(1)
 
 
