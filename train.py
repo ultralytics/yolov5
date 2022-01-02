5895bb78306ee00dbabaf79c78d4ef1d477e202a
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-02 15:57:27 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
 """
-Train a YOLOv5 model on a custom dataset. Models and datasets download automatically from the latest YOLOv5 release.
+Train a YOLOv5 model on a custom dataset.
+
+Models and datasets download automatically from the latest YOLOv5 release.
 Models: https://github.com/ultralytics/yolov5/tree/master/models
 Datasets: https://github.com/ultralytics/yolov5/tree/master/data
 Tutorial: https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data
