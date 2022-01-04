a35aa72bd375341170bdc0d614bfa848202bdb98
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-03 20:22:09 -0800

    Cleanup

--- export.py
+++ export.py
@@ -75,11 +75,8 @@ def export_torchscript(model, im, file, optimize, prefix=colorstr('TorchScript:'
         ts = torch.jit.trace(model, im, strict=False)
         d = {"shape": im.shape, "stride": int(max(model.stride)), "names": model.names}
         extra_files = {'config.txt': json.dumps(d)}  # torch._C.ExtraFilesMap()
-        if optimize:
-            ts = optimize_for_mobile(ts)
-            # pytorch BETA
-            # https://pytorch.org/tutorials/recipes/mobile_interpreter.html
-            ts._save_for_lite_interpreter(str(f), _extra_files=extra_files)
+        if optimize:  # https://pytorch.org/tutorials/recipes/mobile_interpreter.html
+            optimize_for_mobile(ts)._save_for_lite_interpreter(str(f), _extra_files=extra_files)
         else:
             ts.save(str(f), _extra_files=extra_files)
 
