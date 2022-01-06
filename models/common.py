b94b644bc041f27c2859d9af7ecb79ec8c6c0bb9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-06 11:01:39 -0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -306,7 +306,7 @@ class DetectMultiBackend(nn.Module):
 
         if pt:  # PyTorch
             model = attempt_load(weights if isinstance(weights, list) else w, map_location=device)
-            stride = int(model.stride.max())  # model stride
+            stride = max(int(model.stride.max()), 32)  # model stride
             names = model.module.names if hasattr(model, 'module') else model.names  # get class names
             self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
         elif jit:  # TorchScript
