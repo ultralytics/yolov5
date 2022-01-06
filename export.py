2079ecbd8c594f6958ad120010b5de4da3b66e6f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-05 20:47:26 -0800

    Update export.py

--- export.py
+++ export.py
@@ -311,16 +311,14 @@ def export_edgetpu(keras_model, im, file, prefix=colorstr('Edge TPU:')):
         cmd = 'edgetpu_compiler --version'
         help_url = 'https://coral.ai/docs/edgetpu/compiler/'
         assert platform.system() == 'Linux', f'export only supported on Linux. See {help_url}'
-        # assert subprocess.run(cmd, shell=True).returncode == 0, f'export requires edgetpu-compiler. {help}'
         if subprocess.run(cmd, shell=True).returncode != 0:
-            LOGGER.info(f'\n{prefix} export requires edgetpu-compiler. Installing from {help_url} ...')
+            LOGGER.info(f'\n{prefix} export requires Edge TPU compiler. Attempting install from {help_url}')
             cmds = ['curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -',
                     'echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list',
                     'sudo apt-get update',
                     'sudo apt-get install edgetpu-compiler']
             for c in cmds:
                 subprocess.run(c, shell=True, check=True)
-            LOGGER.info(f'\n{prefix} edgetpu-compiler install complete.')
         ver = subprocess.run(cmd, shell=True, capture_output=True, check=True).stdout.decode().split()[-1]
 
         LOGGER.info(f'\n{prefix} starting export with Edge TPU compiler {ver}...')
