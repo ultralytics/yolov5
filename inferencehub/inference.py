927df6d623b5c1442f8689a59208bc25622f548a
Jorge Machado <4804546+jomach@users.noreply.github.com>
2022-01-14 15:02:12 +0100

    wip

--- inferencehub/inference.py
+++ inferencehub/inference.py
@@ -1,9 +1,27 @@
 import torch
 import numpy as np
+from utils.augmentations import letterbox
 from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size
+import cv2
 
-def preprocess_function(input_pre: dict,  model, input_parameters: dict) -> dict:
-    img = torch.from_numpy(np.array(input_pre))
+
+def preprocess_function(input_pre: dict, model, input_parameters: dict) -> dict:
+    # Validate image size
+    imgsz = input_parameters.get('imgsz', 640)
+    stride = int(model.model.stride.max())
+    imgsz = check_img_size(imgsz, s=stride)
+    input_parameters['imgsz'] = imgsz
+    nparr = np.asarray(input_pre, dtype="uint8")
+
+    # Padded resize
+    img = letterbox(nparr, imgsz, stride=stride)[0]
+
+    # Convert
+    img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    img = np.ascontiguousarray(img)
+
+    img = torch.from_numpy(img)
     img = img / 255.0  # 0 - 255 to 0.0 - 1.0
     if len(img.shape) == 3:
         img = img[None]  # expand for batch dim
--- inferencehub/model.py
+++ inferencehub/model.py
@@ -3,25 +3,35 @@ from models.experimental import attempt_load
 import torch
 import torch.nn as nn
 
+
 class ModelWrapper(nn.Module):
     def __init__(self, network_pkl):
         super().__init__()
         # network parameters
         self.network_pkl = network_pkl
+        self.warmup = False
 
         # model loading
         self.model = attempt_load(network_pkl, map_location="cpu")
         self.add_module("model", self.model)
 
     def forward(self, x: dict, input_parameters: dict) -> torch.tensor:
-       pred = self.model(x, augment=input_parameters.get('augment', False), visualize=input_parameters.get('visualize', False))[0]
-       pred = non_max_suppression(pred,
-                                input_parameters.get('conf_thres', 0.25),
-                                input_parameters.get('iou_thres', 0.45),
-                                input_parameters.get('classes'),
-                                input_parameters.get('agnostic_nms', False),
-                                max_det=input_parameters.get('max_det', 1000))
-       return pred
+        self.warmup_forward(x, input_parameters)
+        pred = self.model(x, augment=input_parameters.get('augment', False),
+                          visualize=input_parameters.get('visualize', False))[0]
+        pred = non_max_suppression(pred,
+                                   input_parameters.get('conf_thres', 0.25),
+                                   input_parameters.get('iou_thres', 0.45),
+                                   input_parameters.get('classes'),
+                                   input_parameters.get('agnostic_nms', False),
+                                   max_det=input_parameters.get('max_det', 1000))
+        return pred
+
+    def warmup_forward(self, x: dict, input_parameters: dict):
+        if not self.warmup:
+            self.model(torch.zeros(1, 3, input_parameters.get("imgsz", 64)).to("cpu").type_as(next(self.model.parameters())))  # run once
+            self.warmup = True
+
 
 def get_model(weights_path=None, map_location="cpu", model_initialization_parameters={}) -> torch.nn.Module:
     return ModelWrapper(network_pkl=weights_path)
