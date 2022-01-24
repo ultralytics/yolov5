704b2d32aa6c4fac2a8e02dfb168320938103e0d
YI <751417527@qq.com>
2022-01-24 14:43:25 +0800

    Add compatibility with GIF images

--- utils/datasets.py
+++ utils/datasets.py
@@ -669,7 +669,11 @@ def load_image(self, i):
         else:  # read image
             path = self.img_files[i]
             im = cv2.imread(path)  # BGR
-            assert im is not None, f'Image Not Found {path}'
+            if im is None:
+                gif = cv2.VideoCapture(path) # handle gif
+                ret, im = gif.read()
+                gif.release()
+                assert im is not None, f'Image Not Found {path}'
         h0, w0 = im.shape[:2]  # orig hw
         r = self.img_size / max(h0, w0)  # ratio
         if r != 1:  # if sizes are not equal
