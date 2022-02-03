a95218be5b2a2a2c9d10fc308792e2e3f0388943
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-03 20:22:55 +0100

    Edge TPU compiler sudo fix
    Allows for auto-install of Edge TPU compiler on non-sudo systems like the YOLOv5 Docker image.
    
    @kalenmike

--- export.py
+++ export.py
@@ -318,13 +318,14 @@ def export_edgetpu(keras_model, im, file, prefix=colorstr('Edge TPU:')):
         cmd = 'edgetpu_compiler --version'
         help_url = 'https://coral.ai/docs/edgetpu/compiler/'
         assert platform.system() == 'Linux', f'export only supported on Linux. See {help_url}'
+        sudo = subprocess.run('sudo --help', shell=True).returncode == 0  # sudo installed on system
         if subprocess.run(cmd, shell=True).returncode != 0:
             LOGGER.info(f'\n{prefix} export requires Edge TPU compiler. Attempting install from {help_url}')
             for c in ['curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -',
                       'echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list',
                       'sudo apt-get update',
                       'sudo apt-get install edgetpu-compiler']:
-                subprocess.run(c, shell=True, check=True)
+                subprocess.run(c if sudo else c.replace('sudo ', ''), shell=True, check=True)
         ver = subprocess.run(cmd, shell=True, capture_output=True, check=True).stdout.decode().split()[-1]
 
         LOGGER.info(f'\n{prefix} starting export with Edge TPU compiler {ver}...')
