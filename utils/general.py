d3498ae5c3ce6eeca384f60cedfabc8eb53dbb20
lkno0705 <lkno0705@gmail.com>
2022-02-05 17:04:05 +0100

    rewrote gtsrb path

--- utils/general.py
+++ utils/general.py
@@ -29,8 +29,8 @@ import torch
 import torchvision
 import yaml
 
-from utils.downloads import gsutil_getsize
-from utils.metrics import box_iou, fitness
+from downloads import gsutil_getsize
+from metrics import box_iou, fitness
 
 # Settings
 FILE = Path(__file__).resolve()
