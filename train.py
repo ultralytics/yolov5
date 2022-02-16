6769021901099969a69609040a946327e9ccad27
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-16 14:44:44 +0100

    Validate with 2x `--workers` (#6658)

--- train.py
+++ train.py
@@ -232,7 +232,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     if RANK in [-1, 0]:
         val_loader = create_dataloader(val_path, imgsz, batch_size // WORLD_SIZE * 2, gs, single_cls,
                                        hyp=hyp, cache=None if noval else opt.cache, rect=True, rank=-1,
-                                       workers=workers, pad=0.5,
+                                       workers=workers * 2, pad=0.5,
                                        prefix=colorstr('val: '))[0]
 
         if not resume:
