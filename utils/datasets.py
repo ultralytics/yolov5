1ff43702a8dea05b0d1140d4bb2cf6a2fe3e3ad4
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-16 14:50:37 +0100

    Validate with 2x `--workers` single-GPU/CPU fix (#6659)
    Fix for #6658 for single-GPU and CPU training use cases

--- utils/datasets.py
+++ utils/datasets.py
@@ -110,7 +110,7 @@ def create_dataloader(path, imgsz, batch_size, stride, single_cls=False, hyp=Non
 
     batch_size = min(batch_size, len(dataset))
     nd = torch.cuda.device_count()  # number of CUDA devices
-    nw = min([2 * os.cpu_count() // max(nd, 1), batch_size if batch_size > 1 else 0, workers])  # number of workers
+    nw = min([os.cpu_count() // max(nd / 2, 1), batch_size if batch_size > 1 else 0, workers])  # number of workers
     sampler = None if rank == -1 else distributed.DistributedSampler(dataset, shuffle=shuffle)
     loader = DataLoader if image_weights else InfiniteDataLoader  # only DataLoader allows for attribute updates
     return loader(dataset,
