1001a7f925eee3f9113a7b547fe31b148658c1bb
zkf331 <zheng.kaifu@zte.com.cn>
2022-02-21 17:03:11 +0800

    Add distillation loss function

--- utils/loss.py
+++ utils/loss.py
@@ -220,3 +220,171 @@ class ComputeLoss:
             tcls.append(c)  # class
 
         return tcls, tbox, indices, anch
+
+
+def distillation_loss(output_student, output_teacher, num_classes, temperature=3.0):
+    """object detection distillation loss
+
+    :param output_student: the output of student model
+    :param output_teacher:  the output of teacher model
+    :param num_classes:  classes number
+    :param batch_size:  batch size
+    :return: loss_st * Lambda_ST
+    """
+
+    lambda_st = 0.0001
+    batch_size = output_student[0].shape[0]
+    criterion_st = torch.nn.KLDivLoss(reduction='sum')
+    output_student = torch.cat([i.view(-1, num_classes + 5) for i in output_student])
+    output_teacher = torch.cat([i.view(-1, num_classes + 5) for i in output_teacher])
+    log_softmax_student = nn.functional.log_softmax(output_student/temperature, dim=1)
+    softmax_teacher = nn.functional.softmax(output_teacher/temperature, dim=1)
+    loss_st = criterion_st(log_softmax_student, softmax_teacher) * (temperature * temperature) / batch_size
+
+    return loss_st * lambda_st
+
+
+def split_distill_loss(student_outs, teacher_outs, weight):
+    """
+    Add fine grained distillation losses.
+    Each loss is composed by distill_reg_loss, distill_cls_loss and distill_obj_loss
+    From the paper ‘Object detection at 200 Frames Per Second’
+    """
+    distill_reg_loss, distill_cls_loss, distill_obj_loss = [], [], []
+
+    teacher_outs = feature_map_nms(teacher_outs)
+
+    for s_out, t_out in zip(student_outs, teacher_outs):
+        student_x, student_y, student_w, student_h, student_obj, \
+        student_cls = s_out[..., 0], s_out[..., 1], s_out[..., 2], s_out[..., 3], s_out[..., 4], s_out[..., 5:]
+        teacher_x, teacher_y, teacher_w, teacher_h, teacher_obj, \
+        teacher_cls = t_out[..., 0], t_out[..., 1], t_out[..., 2], t_out[..., 3], t_out[..., 4], t_out[..., 5:]
+
+        distill_reg_loss.append(
+            obj_weighted_reg(student_x, student_y, student_w, student_h,
+                             teacher_x, teacher_y, teacher_w, teacher_h, teacher_obj))
+
+        distill_cls_loss.append(
+            obj_weighted_cls(student_cls, teacher_cls, teacher_obj))
+
+        distill_obj_loss.append(obj_loss(student_obj, teacher_obj))
+
+    distill_reg_loss = sum(distill_reg_loss)
+    distill_cls_loss = sum(distill_cls_loss)
+    distill_obj_loss = sum(distill_obj_loss)
+    loss = (distill_reg_loss + distill_cls_loss + distill_obj_loss) * weight
+
+    return loss
+
+
+def obj_weighted_reg(sx, sy, sw, sh, tx, ty, tw, th, tobj):
+    """distill_reg_loss"""
+
+    loss_x = F.binary_cross_entropy_with_logits(sx, torch.sigmoid(tx))
+    loss_y = F.binary_cross_entropy_with_logits(sy, torch.sigmoid(ty))
+    loss_w = torch.abs(sw - tw)
+    loss_h = torch.abs(sh - th)
+    loss = sum([loss_x, loss_y, loss_w, loss_h])
+    weighted_loss = torch.mean(loss * torch.sigmoid(tobj))
+    return weighted_loss
+
+
+def obj_weighted_cls(scls, tcls, tobj):
+    """distill_cls_loss"""
+
+    loss = F.binary_cross_entropy_with_logits(scls, torch.sigmoid(tcls))
+    weighted_loss = torch.mean(torch.mul(loss, torch.sigmoid(tobj)))
+    return weighted_loss
+
+
+def obj_loss(sobj, tobj):
+    """distill_obj_loss"""
+
+    obj_mask = (tobj > 0.).float()
+    loss = torch.mean(F.binary_cross_entropy_with_logits(sobj, obj_mask))
+    return loss
+
+
+def feature_map_nms(teacher_outs):
+    """faature map nms
+
+    :param teacher_outs: the outputs of teacher model
+    :return: outputs
+    """
+    outputs = []
+    for teacher_out in teacher_outs:
+        for x in range(0, teacher_out.shape[2] - 3 + 1, 3):
+            for y in range(0, teacher_out.shape[3] - 3 + 1, 3):
+                grid_3x3 = teacher_out[:, :, x:x+3, y:y+3, :]
+                grid_max = grid_3x3.reshape(teacher_out.shape[0], -1, teacher_out.shape[-1])
+                max_index = grid_max.argmax(dim=1)
+                mask = torch.zeros_like(grid_max)
+                for i in range(teacher_out.shape[0]):
+                    mask[i, max_index[i, 4]] = grid_max[i, max_index[i, 4]]
+                teacher_out[:, :, x:x+3, y:y+3, :] = mask.reshape(grid_3x3.shape)
+
+        outputs.append(teacher_out)
+    return outputs
+
+
+def compute_distillation_output_loss(p, t_p, model, d_weight=1):
+    t_ft = torch.cuda.FloatTensor if t_p[0].is_cuda else torch.Tensor
+    t_lcls, t_lbox, t_lobj = t_ft([0]), t_ft([0]), t_ft([0])
+    h = model.hyp  # hyperparameters
+    red = 'mean'  # Loss reduction (sum or mean)
+    if red != "mean":
+        raise NotImplementedError("reduction must be mean in distillation mode!")
+
+    DboxLoss = nn.MSELoss(reduction="none")
+    DclsLoss = nn.MSELoss(reduction="none")
+    DobjLoss = nn.MSELoss(reduction="none")
+    # per output
+    for i, pi in enumerate(p):  # layer index, layer predictions
+        t_pi = t_p[i]
+        t_obj_scale = t_pi[..., 4].sigmoid()
+
+        # BBox
+        b_obj_scale = t_obj_scale.unsqueeze(-1).repeat(1, 1, 1, 1, 4)
+        t_lbox += torch.mean(DboxLoss(pi[..., :4], t_pi[..., :4]) * b_obj_scale)
+
+        # Class
+        if model.nc > 1:  # cls loss (only if multiple classes)
+            c_obj_scale = t_obj_scale.unsqueeze(-1).repeat(1, 1, 1, 1, model.nc)
+            # t_lcls += torch.mean(c_obj_scale * (pi[..., 5:] - t_pi[..., 5:]) ** 2)
+            t_lcls += torch.mean(DclsLoss(pi[..., 5:], t_pi[..., 5:]) * c_obj_scale)
+
+        # t_lobj += torch.mean(t_obj_scale * (pi[..., 4] - t_pi[..., 4]) ** 2)
+        t_lobj += torch.mean(DobjLoss(pi[..., 4], t_pi[..., 4]) * t_obj_scale)
+    t_lbox *= h['box']
+    t_lobj *= h['obj']
+    t_lcls *= h['cls']
+    # bs = p[0].shape[0]  # batch size
+    loss = (t_lobj + t_lbox + t_lcls) * d_weight
+    return loss
+
+
+def compute_distillation_feature_loss(s_f, t_f, model, f_weight=0.1):
+    """
+    Feature Map distillation.
+    Args:
+        s_f: student feature
+        t_f: teacher feature
+        model: model
+
+    Returns: distillation feature loss
+    """
+    h = model.hyp  # hyperparameters
+    ft = torch.cuda.FloatTensor if s_f[0].is_cuda else torch.Tensor
+    dl_1, dl_2, dl_3 = ft([0]), ft([0]), ft([0])
+
+    loss_func1 = nn.MSELoss(reduction="mean")
+    loss_func2 = nn.MSELoss(reduction="mean")
+    loss_func3 = nn.MSELoss(reduction="mean")
+
+    dl_1 += loss_func1(s_f[0], t_f[0])
+    dl_2 += loss_func2(s_f[1], t_f[1])
+    dl_3 += loss_func3(s_f[2], t_f[2])
+
+    # bs = s_f[0].shape[0]
+
+    return (dl_1 + dl_2 + dl_3) * f_weight
