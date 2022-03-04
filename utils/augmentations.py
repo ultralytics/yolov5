82a54416e040037d902c40d065bafd26a83eb199
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-04 16:58:27 +0100

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -118,7 +118,6 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
         interpolation = random.choice([cv2.INTER_NEAREST, cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INTER_AREA,
                                        cv2.INTER_LANCZOS4]) if random_interpolation else cv2.INTER_LINEAR
         im = cv2.resize(im, new_unpad, interpolation=interpolation)
-
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
     im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
