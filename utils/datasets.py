ca29dbe5875db972221b929d71a5e2053e2a4ac0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-06 14:55:49 +0100

    Updates

--- utils/datasets.py
+++ utils/datasets.py
@@ -377,7 +377,7 @@ def img2label_paths(img_paths):
 
 class LoadImagesAndLabels(Dataset):
     # YOLOv5 train_loader/val_loader, loads images and labels for training and validation
-    cache_version = 0.6  # dataset labels *.cache version
+    cache_version = 0.7  # dataset labels *.cache version
 
     def __init__(self, path, img_size=640, batch_size=16, augment=False, hyp=None, rect=False, image_weights=False,
                  cache_images=False, single_cls=False, stride=32, pad=0.0, prefix=''):
