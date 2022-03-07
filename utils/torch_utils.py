fb6bd6a48305686bb09fed153fdbf8373a0907cb
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-07 14:07:20 +0100

    Update bytes to GB with bitshift

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -86,7 +86,7 @@ def select_device(device='', batch_size=0, newline=True):
         space = ' ' * (len(s) + 1)
         for i, d in enumerate(devices):
             p = torch.cuda.get_device_properties(i)
-            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2:.0f}MiB)\n"  # bytes to MB
+            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / (1 << 20):.0f}MiB)\n"  # bytes to MB
     else:
         s += 'CPU\n'
 
