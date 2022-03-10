5d6d63ee75d6f3531028df5898c5f0fc56dbbf97
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-03-10 18:18:31 +0300

    upgrade supervisely version, add inference decorator (#77)
    * upgrade supervisely version, add inference decorator
    
    * remove torchvision from requirements.txt
    
    * add supervisely[apps] version

--- supervisely/serve/src/sly_serve.py
+++ supervisely/serve/src/sly_serve.py
@@ -3,8 +3,10 @@ import json
 import yaml
 import pathlib
 import sys
-import supervisely_lib as sly
+import supervisely as sly
 from pathlib import Path
+
+from supervisely.app.v1.app_service import AppService
 
 root_source_path = str(pathlib.Path(sys.argv[0]).parents[3])
 sly.logger.info(f"Root source directory: {root_source_path}")
@@ -12,7 +14,7 @@ sys.path.append(root_source_path)
 
 from nn_utils import construct_model_meta, load_model, inference
 
-my_app = sly.AppService()
+my_app = AppService()
 
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
@@ -23,7 +25,6 @@ modelWeightsOptions = os.environ['modal.state.modelWeightsOptions']
 pretrained_weights = os.environ['modal.state.selectedModel'].lower()
 custom_weights = os.environ['modal.state.weightsPath']
 
-
 DEVICE_STR = os.environ['modal.state.device']
 final_weights = None
 model = None
@@ -31,7 +32,6 @@ half = None
 device = None
 imgsz = None
 stride = None
-
 
 settings_path = os.path.join(root_source_path, "supervisely/serve/custom_settings.yaml")
 sly.logger.info(f"Custom inference settings path: {settings_path}")
@@ -71,7 +71,8 @@ def get_custom_inference_settings(api: sly.Api, task_id, context, state, app_log
     my_app.send_response(request_id, data={"settings": default_settings_str})
 
 
-def inference_image_path(image_path, context, state, app_logger):
+@sly.process_image_with_crop
+def inference_image_path(image_path, project_meta, context, state, app_logger):
     app_logger.debug("Input path", extra={"path": image_path})
 
     rect = None
@@ -118,7 +119,8 @@ def inference_image_url(api: sly.Api, task_id, context, state, app_logger):
     local_image_path = os.path.join(my_app.data_dir, sly.rand_str(15) + ext)
 
     sly.fs.download(image_url, local_image_path)
-    ann_json = inference_image_path(local_image_path, context, state, app_logger)
+    ann_json = inference_image_path(image_path=local_image_path, project_meta=meta,
+                                    context=context, state=state, app_logger=app_logger)
     sly.fs.silent_remove(local_image_path)
 
     request_id = context["request_id"]
@@ -133,7 +135,8 @@ def inference_image_id(api: sly.Api, task_id, context, state, app_logger):
     image_info = api.image.get_info_by_id(image_id)
     image_path = os.path.join(my_app.data_dir, sly.rand_str(10) + image_info.name)
     api.image.download_path(image_id, image_path)
-    ann_json = inference_image_path(image_path, context, state, app_logger)
+    ann_json = inference_image_path(image_path=image_path, project_meta=meta,
+                                    context=context, state=state, app_logger=app_logger)
     sly.fs.silent_remove(image_path)
     request_id = context["request_id"]
     my_app.send_response(request_id, data=ann_json)
@@ -152,7 +155,8 @@ def inference_batch_ids(api: sly.Api, task_id, context, state, app_logger):
 
     results = []
     for image_path in paths:
-        ann_json = inference_image_path(image_path, context, state, app_logger)
+        ann_json = inference_image_path(image_path=image_path, project_meta=meta,
+                                        context=context, state=state, app_logger=app_logger)
         results.append(ann_json)
         sly.fs.silent_remove(image_path)
 
@@ -166,7 +170,7 @@ def debug_inference():
     print(json.dumps(ann, indent=4))
 
 
-#@my_app.callback("preprocess")
+# @my_app.callback("preprocess")
 @sly.timeit
 def preprocess():
     global model, half, device, imgsz, meta, final_weights
@@ -206,12 +210,12 @@ def main():
     })
 
     preprocess()
-    #my_app.run(initial_events=[{"command": "preprocess"}])
+    # my_app.run(initial_events=[{"command": "preprocess"}])
     my_app.run()
 
 
-#@TODO: move inference methods to SDK
-#@TODO: augment inference
-#@TODO: https://pypi.org/project/cachetools/
+# @TODO: move inference methods to SDK
+# @TODO: augment inference
+# @TODO: https://pypi.org/project/cachetools/
 if __name__ == "__main__":
     sly.main_wrapper("main", main)
