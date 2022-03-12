ab2b1c05ebc4c8b26b2452f549a5481e9d8b8121
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-12 20:02:26 +0100

    Add TRT requirements

--- utils/benchmarks.py
+++ utils/benchmarks.py
@@ -19,6 +19,7 @@ TensorFlow.js               | `tfjs`                        | yolov5s_web_model/
 Requirements:
     $ pip install -r requirements.txt coremltools onnx onnx-simplifier onnxruntime openvino-dev tensorflow-cpu  # CPU
     $ pip install -r requirements.txt coremltools onnx onnx-simplifier onnxruntime-gpu openvino-dev tensorflow  # GPU
+    $ pip install -U nvidia-tensorrt --index-url https://pypi.ngc.nvidia.com  # TensorRT
 
 Usage:
     $ python utils/benchmarks.py --weights yolov5s.pt --img 640
