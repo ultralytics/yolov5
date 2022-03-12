5ddd304e5148496c7db81e00e1d491cce25207b7
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-12 13:36:38 +0100

    Updates

--- detect.py
+++ detect.py
@@ -109,7 +109,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
     dt, seen = [0.0, 0.0, 0.0], 0
     for path, im, im0s, vid_cap, s in dataset:
         t1 = time_sync()
-        im = torch.from_numpy(im).to(device)
+        im = torch.tensor(im, device=device)
         im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
