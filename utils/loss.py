c8ba07d90534ba71e034180e42ca594a56bba395
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-14 18:23:10 +0100

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -132,7 +132,7 @@ class ComputeLoss:
                 pxy = ps[:, :2].sigmoid() * 2 - 0.5
                 pwh = (ps[:, 2:4].sigmoid() * 2) ** 2 * anchors[i]
                 pbox = torch.cat((pxy, pwh), 1)  # predicted box
-                iou = bbox_iou(pbox.T, tbox[i], x1y1x2y2=False, CIoU=True)  # iou(prediction, target)
+                iou = bbox_iou(pbox.T, tbox[i], x1y1x2y2=False, ICIoU=True)  # iou(prediction, target)
                 lbox += (1.0 - iou).mean()  # iou loss
 
                 # Objectness
