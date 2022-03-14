e4911864dbc4a0252583330361a1d8fb1b735c52
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-14 16:34:28 +0100

    Fix check_img_size() tuple bug

--- utils/general.py
+++ utils/general.py
@@ -344,6 +344,7 @@ def check_img_size(imgsz, s=32, floor=0):
     if isinstance(imgsz, int):  # integer i.e. img_size=640
         new_size = max(make_divisible(imgsz, int(s)), floor)
     else:  # list i.e. img_size=[640, 480]
+        imgsz = list(imgsz)  # convert to list if tuple
         new_size = [max(make_divisible(x, int(s)), floor) for x in imgsz]
     if new_size != imgsz:
         LOGGER.warning(f'WARNING: --img-size {imgsz} must be multiple of max stride {s}, updating to {new_size}')
