c04eeb6ed599662ce0feb6a6ddab90cdf7ab71b8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-20 16:38:53 +0100

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -108,9 +108,9 @@ class Model(nn.Module):
         m = self.model[-1]  # Detect()
         if isinstance(m, Detect):
             s = 256  # 2x min stride
+            check_anchor_order(m)  # must be in pixel-space (not grid-space)
             m.inplace = self.inplace
             m.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.zeros(1, ch, s, s))])  # forward
-            check_anchor_order(m)  # must be in pixel-space (not grid-space)
             m.anchors /= m.stride.view(-1, 1, 1)
             self.stride = m.stride
             self._initialize_biases()  # only run once
