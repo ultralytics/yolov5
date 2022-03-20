ea98baab50e17b3383d9887a7a4b266761103880
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-20 18:31:15 +0100

    Add non-zero `da` `check_anchor_order()` condition

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -20,7 +20,7 @@ def check_anchor_order(m):
     a = m.anchors.prod(-1).mean(-1).view(-1)  # mean anchor area per output layer
     da = a[-1] - a[0]  # delta a
     ds = m.stride[-1] - m.stride[0]  # delta s
-    if da.sign() != ds.sign():  # same order
+    if da and (da.sign() != ds.sign()):  # same order
         LOGGER.info(f'{PREFIX}Reversing anchor order')
         m.anchors[:] = m.anchors.flip(0)
 
