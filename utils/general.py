eadcecb32b2d13fe82d2cbb19db3510a80bc9dd6
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-23 00:42:54 +0100

    Multi-threaded VOC download

--- utils/general.py
+++ utils/general.py
@@ -491,7 +491,7 @@ def download(url, dir='.', unzip=True, delete=True, curl=False, threads=1):
             if curl:
                 os.system(f"curl -L '{url}' -o '{f}' --retry 9 -C -")  # curl download, retry and resume on fail
             else:
-                torch.hub.download_url_to_file(url, f, progress=True)  # torch download
+                torch.hub.download_url_to_file(url, f, progress=threads == 1)  # torch download
         if unzip and f.suffix in ('.zip', '.gz'):
             LOGGER.info(f'Unzipping {f}...')
             if f.suffix == '.zip':
