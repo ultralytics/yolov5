3fcbd245a6077af2ea22b08ea897e29bf9ff696d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-25 13:19:41 +0100

    Update __init__.py

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -11,7 +11,7 @@ import pkg_resources as pkg
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from utils.general import colorstr, cv2, emojis, imread
+from utils.general import colorstr, cv2, emojis
 from utils.loggers.wandb.wandb_utils import WandbLogger
 from utils.plots import plot_images, plot_results
 from utils.torch_utils import de_parallel
