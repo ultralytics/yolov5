40b40d6efe1bf0adba7dd9457a6557130e5c1401
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-26 13:07:30 +0100

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -8,6 +8,7 @@ Usage:
 
 import argparse
 import os
+import platform
 import sys
 from copy import deepcopy
 from pathlib import Path
@@ -16,7 +17,7 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
-if sys.platform != 'Windows':
+if platform.system() != 'Windows':
     ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import *
