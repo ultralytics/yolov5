35043c0ecb9057dc519083c56f3ac792b6b0d4a0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-28 18:31:26 +0200

    Update wandb_utils.py

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -46,10 +46,10 @@ def check_wandb_dataset(data_file):
     if check_file(data_file) and data_file.endswith('.yaml'):
         with open(data_file, errors='ignore') as f:
             data_dict = yaml.safe_load(f)
-        is_trainset_wandb_artifact = (isinstance(data_dict['train'], str)
-                                      and data_dict['train'].startswith(WANDB_ARTIFACT_PREFIX))
-        is_valset_wandb_artifact = (isinstance(data_dict['val'], str)
-                                    and data_dict['val'].startswith(WANDB_ARTIFACT_PREFIX))
+        is_trainset_wandb_artifact = isinstance(data_dict['train'],
+                                                str) and data_dict['train'].startswith(WANDB_ARTIFACT_PREFIX)
+        is_valset_wandb_artifact = isinstance(data_dict['val'],
+                                              str) and data_dict['val'].startswith(WANDB_ARTIFACT_PREFIX)
     if is_trainset_wandb_artifact or is_valset_wandb_artifact:
         return data_dict
     else:
