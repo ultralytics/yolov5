f802c17a194cc45515056fe11abf47a4d97e21c7
Konstantin <konstantin@neuralmagic.com>
2022-03-31 12:30:31 -0400

    setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+setuptools.setup(
+    name="yolov5",
+    version='6.1.0dev0',
+    author="",
+    license="GPL",
+    description="Packaged version of the Yolov5 object detector",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ultralytics/yolov5",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(exclude=[".github", ".vscode", "dev-venv", "dist", "recipes", "req-test", "runs", "sparse_logs", "test-whl", "wandb", "yolov5.egg-inf"]),
+    python_requires=">=3.6",
+    install_requires=read_requirements(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ]
+)
