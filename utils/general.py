a19406b39dbc45db0bbae8d0b7da9d6281f9af1e
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-02 15:05:00 +0200

    Update minimum Python>=3.7.0 (#7247)

--- utils/general.py
+++ utils/general.py
@@ -295,7 +295,7 @@ def check_git_status():
     LOGGER.info(emojis(s))  # emoji-safe
 
 
-def check_python(minimum='3.6.2'):
+def check_python(minimum='3.7.0'):
     # Check current python version vs. required python version
     check_version(platform.python_version(), minimum, name='Python ', hard=True)
 
