dda669a12c4df7b282a1378e251f8314e6179bcb
Zengyf-CVer <41098760+Zengyf-CVer@users.noreply.github.com>
2022-04-03 19:19:26 +0800

    Fix Flask REST API (#7210)
    * Update restapi.py
    
    * Update restapi.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Cleanup
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/flask_rest_api/restapi.py
+++ utils/flask_rest_api/restapi.py
@@ -1,5 +1,5 @@
 """
-Run a rest API exposing the yolov5s object detection model
+Run a Flask REST API exposing a YOLOv5s model
 """
 import argparse
 import io
@@ -31,7 +31,10 @@ def predict():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
     parser.add_argument("--port", default=5000, type=int, help="port number")
-    args = parser.parse_args()
+    opt = parser.parse_args()
+
+    # Fix known issue urllib.error.HTTPError 403: rate limit exceeded https://github.com/ultralytics/yolov5/pull/7210
+    torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
 
     model = torch.hub.load("ultralytics/yolov5", "yolov5s", force_reload=True)  # force_reload to recache
-    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
+    app.run(host="0.0.0.0", port=opt.port)  # debug=True causes Restarting with stat
