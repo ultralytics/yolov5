89462fc7f68696cd7eca2dba5d2772b470b2a2c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-07 16:06:55 +0200

    Indent args (pytorch convention)

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -30,7 +30,7 @@ class Callbacks:
             'on_model_save': [],
             'on_train_end': [],
             'on_params_update': [],
-            'teardown': [],}
+            'teardown': [], }
         self.stop_training = False  # set True to interrupt training
 
     def register_action(self, hook, name='', callback=None):
@@ -38,9 +38,9 @@ class Callbacks:
         Register a new action to a callback hook
 
         Args:
-            hook        The callback hook name to register the action to
-            name        The name of the action for later reference
-            callback    The callback to fire
+            hook: The callback hook name to register the action to
+            name: The name of the action for later reference
+            callback: The callback to fire
         """
         assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
         assert callable(callback), f"callback '{callback}' is not callable"
@@ -51,7 +51,7 @@ class Callbacks:
         Returns all the registered actions by callback hook
 
         Args:
-            hook The name of the hook to check, defaults to all
+            hook: The name of the hook to check, defaults to all
         """
         return self._callbacks[hook] if hook else self._callbacks
 
@@ -60,9 +60,9 @@ class Callbacks:
         Loop through the registered actions and fire all callbacks
 
         Args:
-            hook The name of the hook to check, defaults to all
-            args Arguments to receive from YOLOv5
-            kwargs Keyword Arguments to receive from YOLOv5
+            hook: The name of the hook to check, defaults to all
+            args: Arguments to receive from YOLOv5
+            kwargs: Keyword Arguments to receive from YOLOv5
         """
 
         assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
