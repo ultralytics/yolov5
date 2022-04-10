94d9a9b58ac6ae4d8f6b63b206cb910f7af2c5df
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-10 13:04:53 +0200

    bbox_ioa speed improvements

--- utils/metrics.py
+++ utils/metrics.py
@@ -279,11 +279,9 @@ def bbox_ioa(box1, box2, eps=1E-7):
     returns:    np.array of shape(n)
     """
 
-    box2 = box2.transpose()
-
     # Get the coordinates of bounding boxes
-    b1_x1, b1_y1, b1_x2, b1_y2 = box1[0], box1[1], box1[2], box1[3]
-    b2_x1, b2_y1, b2_x2, b2_y2 = box2[0], box2[1], box2[2], box2[3]
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2.T
 
     # Intersection area
     inter_area = (np.minimum(b1_x2, b2_x2) - np.maximum(b1_x1, b2_x1)).clip(0) * \
