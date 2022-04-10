9d9628042ca2a980c05e59a39f36a19aa427d0b2
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-10 12:50:03 +0200

    bbox_iou speed improvements

--- utils/loss.py
+++ utils/loss.py
@@ -138,7 +138,7 @@ class ComputeLoss:
                 pxy = pxy.sigmoid() * 2 - 0.5
                 pwh = (pwh.sigmoid() * 2) ** 2 * anchors[i]
                 pbox = torch.cat((pxy, pwh), 1)  # predicted box
-                iou = bbox_iou(pbox.T, tbox[i], x1y1x2y2=False, CIoU=True)  # iou(prediction, target)
+                iou = bbox_iou(pbox, tbox[i], CIoU=True).squeeze()  # iou(prediction, target)
                 lbox += (1.0 - iou).mean()  # iou loss
 
                 # Objectness
