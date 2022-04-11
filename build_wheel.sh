4154b8c2cbbec391712c02b679277f7332714ce9
Konstantin <konstantin@neuralmagic.com>
2022-04-11 10:17:59 -0400

    Update: bash __init__

--- build_wheel.sh
+++ build_wheel.sh
@@ -1,5 +1,10 @@
 python3 -m pip install --upgrade build
 mkdir yolov5
 cp -r data models models_v5.0 utils .pre-commit-config.yaml $(ls *.py) yolov5/
-touch yolov5/__init__.py
+cat > yolov5/__init__.py << EOF
+from export import run as export
+from train import run as train
+from val import run as val
+EOF
 python3 -m build
+rm -r yolov5
