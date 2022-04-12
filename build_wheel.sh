fb589b9cbb84515811d6a8f588ff3f89bec8a87f
Konstantin <konstantin@neuralmagic.com>
2022-04-12 12:42:00 -0400

    Update: include NM_INTEGRATED

--- build_wheel.sh
+++ build_wheel.sh
@@ -5,6 +5,7 @@ grep --include=\*.py -rnl 'yolov5/' -e "from models" | xargs -i@ sed -i 's/from 
 grep --include=\*.py -rnl 'yolov5/' -e "from utils" | xargs -i@ sed -i 's/from utils/from yolov5.utils/g' @
 sed -i '$d' yolov5/requirements.txt
 cat > yolov5/__init__.py << EOF
+from yolov5.utils.general import NM_INTEGRATED
 EOF
 python3 -m build
 rm -r yolov5
--- utils/general.py
+++ utils/general.py
@@ -40,6 +40,7 @@ DATASETS_DIR = ROOT.parent / 'datasets'  # YOLOv5 datasets directory
 NUM_THREADS = min(8, max(1, os.cpu_count() - 1))  # number of YOLOv5 multiprocessing threads
 VERBOSE = str(os.getenv('YOLOv5_VERBOSE', True)).lower() == 'true'  # global verbose mode
 FONT = 'Arial.ttf'  # https://ultralytics.com/assets/Arial.ttf
+NM_INTEGRATED=True
 
 torch.set_printoptions(linewidth=320, precision=5, profile='long')
 np.set_printoptions(linewidth=320, formatter={'float_kind': '{:11.5g}'.format})  # format short g, %precision=5
