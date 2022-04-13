a4af8139bc5fa5834af7a7e154bb8a76a67a5387
liorgreenbergmh <lior.greenberg@myheritage.com>
2022-04-13 08:06:29 +0000

    detect with docker

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,7 @@ RUN pip install -r requirements.txt
 RUN pip install wandb
 RUN wandb login e30bdfb905a3cf11769d005d2fc0b6d87cb67402
 
-CMD bash -c "sh /usr/src/app/detect.sh & sleep 5 && tail -F /dev/null"
+RUN wandb off
+
+ENTRYPOINT ["/bin/bash", "/usr/src/app/detect.sh"]
+# CMD bash -c "sh /usr/src/app/detect.sh & sleep 5 && tail -F /dev/null"
--- detect.sh
+++ detect.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
-nohup python3 detect.py \
-    --weights /usr/src/weights/best.pt \
-    --img 640 \
+echo "folder $1"
+# python3 detect.py \
+#     --weights /usr/src/weights/singletext.pt /usr/src/weights/graphicalwithtext.pt \
+#     --img 640 \
+#     --conf-thres 0.25 \
+#     --augment\
+#     --source "/usr/src/data/$1" \
+#     --save-txt\
+#     --name $1\
+#     --device 0
+
+python3 detect.py \
+    --weights /usr/src/weights/$2.pt\
+    --img 1280 \
     --conf-thres 0.25 \
-    --source /usr/src/data \
-    --save-txt
+    --augment\
+    --source "/usr/src/data/$1" \
+    --save-txt\
+    --name $1\
+    --device 0
