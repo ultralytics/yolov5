7ea0b52cdfa5ee9d8344ddf96ade0090ea6c6e67
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-19 15:38:38 -0700

    fix

--- val.py
+++ val.py
@@ -156,7 +156,6 @@ def run(
     model.eval()
     cuda = device.type != 'cpu'
     is_coco = isinstance(data.get('val'), str) and data['val'].endswith(f'coco{os.sep}val2017.txt')  # COCO dataset
-    is_coco = isinstance(data.get('val'), str) and data['val'].endswith('coco' + os.sep + 'val2017.txt')  # COCO dataset
     nc = 1 if single_cls else int(data['nc'])  # number of classes
     iouv = torch.linspace(0.5, 0.95, 10, device=device)  # iou vector for mAP@0.5:0.95
     niou = iouv.numel()
