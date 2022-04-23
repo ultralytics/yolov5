c30d49963194fa9ac305544d80e02aa228c53259
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-23 10:36:31 -0700

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -28,14 +28,15 @@ def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbo
     """
     from pathlib import Path
 
+    from utils.general import LOGGER, check_requirements, intersect_dicts, logging
+    if not verbose:
+        LOGGER.setLevel(logging.WARNING)
+    
     from models.common import AutoShape, DetectMultiBackend
     from models.yolo import Model
     from utils.downloads import attempt_download
-    from utils.general import LOGGER, check_requirements, intersect_dicts, logging
     from utils.torch_utils import select_device
 
-    if not verbose:
-        LOGGER.setLevel(logging.WARNING)
     check_requirements(exclude=('tensorboard', 'thop', 'opencv-python'))
     name = Path(name)
     path = name.with_suffix('.pt') if name.suffix == '' else name  # checkpoint path
