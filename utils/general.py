24b0235a7313586881397ca893868e9cfd850eda
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-23 10:53:56 -0700

    Update

--- utils/general.py
+++ utils/general.py
@@ -431,6 +431,7 @@ def check_font(font=FONT, progress=False):
     file = CONFIG_DIR / font.name
     if not font.exists() and not file.exists():
         url = "https://ultralytics.com/assets/" + font.name
+        print(LOGGER.level)
         LOGGER.info(f'Downloading {url} to {file}...')
         torch.hub.download_url_to_file(url, str(file), progress=progress)
 
