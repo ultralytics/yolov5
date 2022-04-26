727c738eaf6b895a5b5d3571a74acd9a934fa9ad
iskanderBalti <iskander.balti@gmail.com>
2022-04-26 21:30:11 +0100

    14 modif

--- detect.py
+++ detect.py
@@ -69,7 +69,7 @@ def run(
         project=ROOT / 'runs/detect',  # save results to project/name
         name='exp',  # save results to project/name
         exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
+        line_thickness=2,  # bounding box thickness (pixels)
         hide_labels=False,  # hide labels
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
