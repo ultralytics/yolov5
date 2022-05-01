248427fe76b1ab1e709b367a114233e55c16e7e5
SylvanDing <37374560+sylvanding@users.noreply.github.com>
2022-05-01 10:06:14 +0800

    Update plot.py
    fix bug in issue #7650

--- utils/plots.py
+++ utils/plots.py
@@ -426,7 +426,7 @@ def plot_results(file='path/to/results.csv', dir=''):
             s = [x.strip() for x in data.columns]
             x = data.values[:, 0]
             for i, j in enumerate([1, 2, 3, 4, 5, 8, 9, 10, 6, 7]):
-                y = data.values[:, j]
+                y = data.values[:, j].astype('float')
                 # y[y == 0] = np.nan  # don't show zero values
                 ax[i].plot(x, y, marker='.', label=f.stem, linewidth=2, markersize=8)
                 ax[i].set_title(s[j], fontsize=12)
