4612e83861b56129b64ab9fcc79e951f5a303f6f
hoonjae <hoonjae@artlab.ai>
2022-05-08 05:38:30 +0000

    fix: add false negative into map calculation by passing gt_classes

--- val.py
+++ val.py
@@ -227,7 +227,7 @@ def run(
 
             if npr == 0:
                 if nl:
-                    stats.append((correct, *torch.zeros((3, 0), device=device)))
+                    stats.append((correct, *torch.zeros((2, 0), device=device), labels[:, 0]))
                 continue
 
             # Predictions
