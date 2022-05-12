e20129a36d0e303a78d3f275dc1c8693453dcdd5
Konstantin Gulin <66528950+KSGulin@users.noreply.github.com>
2022-05-12 13:16:21 +0100

    Auto build and upload wheel for nightly + release (#38)

--- build_wheel.sh
+++ build_wheel.sh
@@ -3,7 +3,7 @@ mkdir yolov5
 cp -r data models models_v5.0 utils .pre-commit-config.yaml $(ls *.py) requirements.txt yolov5/
 grep --include=\*.py -rnl 'yolov5/' -e "from models" | xargs -i@ sed -i 's/from models/from yolov5.models/g' @
 grep --include=\*.py -rnl 'yolov5/' -e "from utils" | xargs -i@ sed -i 's/from utils/from yolov5.utils/g' @
-sed -i '$d' yolov5/requirements.txt
+sed -i '/^sparseml/d' yolov5/requirements.txt
 cat > yolov5/__init__.py << EOF
 from yolov5.utils.general import NM_INTEGRATED
 EOF
