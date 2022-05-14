b3d22c1aa513c9af8d675cbc6bd714a750dafd52
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-14 16:00:13 +0200

    Add `@threaded` decorator

--- utils/general.py
+++ utils/general.py
@@ -14,6 +14,7 @@ import random
 import re
 import shutil
 import signal
+import threading
 import time
 import urllib
 from datetime import datetime
@@ -165,6 +166,16 @@ def try_except(func):
             print(e)
 
     return handler
+
+
+def threaded(func):
+    # Multi-threads a target function and returns thread. Usage: @threaded decorator
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=func, args=args, kwargs=kwargs, daemon=True)
+        thread.start()
+        return thread
+
+    return wrapper
 
 
 def methods(instance):
