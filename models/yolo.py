4d4b029e6f94499a64c4e6d32be2ff477a6aa33a
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-15 19:39:58 +0200

    Refactor modules

--- models/yolo.py
+++ models/yolo.py
@@ -197,7 +197,7 @@ class Model(nn.Module):
             m(x.copy() if c else x)
         dt.append((time_sync() - t) * 100)
         if m == self.model[0]:
-            LOGGER.info(f"{'time (ms)':>10s} {'GFLOPs':>10s} {'params':>10s}  {'module'}")
+            LOGGER.info(f"{'time (ms)':>10s} {'GFLOPs':>10s} {'params':>10s}  module")
         LOGGER.info(f'{dt[-1]:10.2f} {o:10.2f} {m.np:10.0f}  {m.type}')
         if c:
             LOGGER.info(f"{sum(dt):10.2f} {'-':>10s} {'-':>10s}  Total")
