b8e48656f6e3d8f61394e891eb45ebb1bd112de8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-19 17:44:43 +0200

    Update val.py

--- val.py
+++ val.py
@@ -266,7 +266,7 @@ def run(
         tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats, plot=plots, save_dir=save_dir, names=names)
         ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
         mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
-        nt = np.bincount(stats[3].astype(np.int64), minlength=nc)  # number of targets per class
+        nt = np.bincount(stats[3].astype(int), minlength=nc)  # number of targets per class
     else:
         nt = torch.zeros(1)
 
