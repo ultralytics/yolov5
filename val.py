e1d39ae1c1d9d197631a33c7fb2aacfe5ee3cedf
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-27 11:38:08 +0200

    Add warning emoji on `--conf > 0.001`

--- val.py
+++ val.py
@@ -363,7 +363,7 @@ def main(opt):
 
     if opt.task in ('train', 'val', 'test'):  # run normally
         if opt.conf_thres > 0.001:  # https://github.com/ultralytics/yolov5/issues/1466
-            LOGGER.info(f'WARNING: confidence threshold {opt.conf_thres} >> 0.001 will produce invalid mAP values.')
+            LOGGER.info(emojis(f'WARNING: confidence threshold {opt.conf_thres} > 0.001 produces invalid results ⚠️'))
         run(**vars(opt))
 
     else:
