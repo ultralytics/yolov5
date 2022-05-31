258f4c64386d0e2caf73f98e45a46a7482ff4784
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-31 10:55:14 +0200

    `attempt_load()` deserialize fix (#8051)

--- models/experimental.py
+++ models/experimental.py
@@ -77,8 +77,8 @@ def attempt_load(weights, device=None, inplace=True, fuse=True):
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
-        ckpt = torch.load(attempt_download(w))
-        ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()  # FP32 model
+        ckpt = torch.load(attempt_download(w), map_location=device)
+        ckpt = (ckpt.get('ema') or ckpt['model']).float()  # FP32 model
         model.append(ckpt.fuse().eval() if fuse else ckpt.eval())  # fused or un-fused model in eval mode
 
     # Compatibility updates
