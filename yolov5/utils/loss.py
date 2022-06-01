1cb7488e782f1f7aa7f5d617b02b7a606a15350f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-01 22:17:23 +0200

    Update loss.py

--- yolov5/utils/loss.py
+++ yolov5/utils/loss.py
@@ -226,7 +226,7 @@ class ComputeLoss:
             gi, gj = gij.T  # grid indices
 
             # Append
-            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
+            indices.append((b, a, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1)))  # image, anchor, grid
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
