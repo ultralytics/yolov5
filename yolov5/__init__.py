704277c3aeedb205244bedb8ee748e15f5daf1dd
ayush chaurasia <ayush.chaurarsia@gmail.com>
2022-06-02 23:53:12 +0530

    support export as sdk/cli

--- yolov5/__init__.py
+++ yolov5/__init__.py
@@ -1,5 +1,6 @@
 from .detect import run as detect
 from .train import run as train
 from .val import run as val
+from .export import run as export
 
-__all__ = ["train", "detect", "val"]
+__all__ = ["train", "detect", "val", "export"]
--- yolov5/__main__.py
+++ yolov5/__main__.py
@@ -1,7 +1,7 @@
 from .train import run as train
 from .detect import run as detect
 from .val import run as val
-
+from .export import run as export
 import fire
 
 
@@ -9,7 +9,8 @@ def main():
   fire.Fire({
       'train': train,
       'detect': detect,
-      'val': val
+      'val': val,
+      'export': export
   })
 
 if __name__=="__main__":
