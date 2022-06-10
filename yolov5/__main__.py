e3b020c81a11f60d6d919ce149669a9a042d606e
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-06-11 00:46:46 +0530

    path: don't display help after command execution

--- yolov5/__main__.py
+++ yolov5/__main__.py
@@ -3,6 +3,14 @@ import fire
 from yolov5 import benchmark, detect, export, train, val
 
 
+def _PrintResult(component_trace, verbose=False):
+    pass
+
+
+# Patch: fire cli displays help text if the object is not printable
+fire.core._PrintResult = _PrintResult
+
+
 def main():
     fire.Fire({
         'train': train.run,
