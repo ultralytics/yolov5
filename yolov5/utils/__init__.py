227116886bcfb1dc0ea86dde485d12e406434a11
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-06-13 14:34:26 +0530

    relative imports

--- yolov5/utils/__init__.py
+++ yolov5/utils/__init__.py
@@ -11,8 +11,8 @@ def notebook_init(verbose=True):
     import os
     import shutil
 
-    from utils.general import check_requirements, emojis, is_colab
-    from utils.torch_utils import select_device  # imports
+    from yolov5.utils.general import check_requirements, emojis, is_colab
+    from yolov5.utils.torch_utils import select_device  # imports
 
     check_requirements(('psutil', 'IPython'))
     import psutil
