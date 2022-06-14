d752c4cece8d871c55232f612eecf1d247175c02
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-14 14:27:48 +0200

    cleanup

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -322,8 +322,8 @@ class LoadStreams:
                 s = pafy.new(s).getbest(preftype="mp4").url  # YouTube URL
             s = eval(s) if s.isnumeric() else s  # i.e. s = '0' local webcam
             if s == 0:
-                assert not is_colab(), '--source 0 webcam inference is not supported on Colab notebooks'
-                assert not is_kaggle(), '--source 0 webcam inference is not supported on Kaggle notebooks'
+                assert not is_colab(), '--source 0 webcam unsupported on Colab. Rerun command in a local environment.'
+                assert not is_kaggle(), '--source 0 webcam unsupported on Kaggle. Rerun command in a local environment.'
             cap = cv2.VideoCapture(s)
             assert cap.isOpened(), f'{st}Failed to open {s}'
             w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
