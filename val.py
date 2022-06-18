03801ebac5fdb58452b63e398b3f5a878b4f3bfa
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-18 13:35:43 +0200

    remove newline

--- val.py
+++ val.py
@@ -90,7 +90,6 @@ def process_batch(detections, labels, iouv):
                 # matches = matches[matches[:, 2].argsort()[::-1]]
                 matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
             correct[matches[:, 1].astype(int), i] = True
-
     return torch.tensor(correct, dtype=torch.bool, device=iouv.device)
 
 
