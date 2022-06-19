8a6f38d0e8149d8ee7a800ede3c2287af9edd771
Ganesh Jagadeesan <cjagadeesan@dataminr.com>
2022-06-18 18:27:32 -0700

    feat: finished basic logger

--- utils/loggers/mlflow/mlflow_utils.py
+++ utils/loggers/mlflow/mlflow_utils.py
@@ -1,22 +1,18 @@
 """Utilities and tools for tracking runs with Mlflow."""
 import sys
-from contextlib import contextmanager
+from argparse import Namespace
 from pathlib import Path
-from typing import Dict
-
-import yaml
-from tqdm import tqdm
+from typing import Any, Dict
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[3]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
-from utils.general import LOGGER
+from utils.general import LOGGER, colorstr
 
 try:
     import mlflow
-
     assert hasattr(mlflow, "__version__")
 except (ImportError, AssertionError):
     mlflow = None
@@ -24,34 +20,39 @@ except (ImportError, AssertionError):
 
 class MlflowLogger:
 
-    def __init__(self, opt, run_id=None) -> None:
+    def __init__(self, opt: Namespace, run_id: str = None) -> None:
+        prefix = colorstr("Mlflow: ")
         try:
-            LOGGER.info(f"mlflow run_id from init {run_id}")
+            LOGGER.info(f"{prefix}Trying run_id({run_id})")
             self.mlflow, self.mlflow_active_run = mlflow, None if not mlflow else mlflow.start_run(run_id=run_id)
             if self.mlflow_active_run is not None:
                 self.run_id = self.mlflow_active_run.info.run_id
-                LOGGER.info(f"from mlflow logger: {self.run_id}")
+                LOGGER.info(f"{prefix}Using run_id({self.run_id})")
+                if self.run_id != run_id:
+                    self.log_params(vars(opt))
         except Exception as err:
-            LOGGER.error(f"Mlflow couldn't create or find experiment: {str(err)}")
+            LOGGER.error(f"{prefix}Failing init - {str(err)}")
+            LOGGER.warning(f"{prefix}Continuining without Mlflow")
             self.mlflow = None
             self.mlflow_active_run = None
-            self.current_epoch = 0
 
-    def log_artifacts(self, artifact, epoch=None):
-        LOGGER.info(f"from mlflow log_artifacts: {artifact, epoch}")
+    def log_artifacts(self, artifact: Path, epoch: int = None) -> None:
+        artifact_name = artifact.stem()
+        if artifact.is_dir():
+            name = f"{artifact_name}_epoch{str(epoch).zfill(4)}" if epoch is not None else artifact_name
+            self.mlflow.log_artifacts(artifact, artifact_path=name)
+        else:
+            name = f"{artifact_name}_epoch{str(epoch).zfill(4)}{artifact.suffix}" if epoch is not None else None
+            self.mlflow.log_artifact(artifact, artifact_path=name)
 
-    def log_model(self, model):
-        if self.mlflow is not None:
-            self.mlflow.pytorch.log_model(model, code_paths=[ROOT.resolve()])
+    def log_model(self, model) -> None:
+        self.mlflow.pytorch.log_model(model, code_paths=[ROOT.resolve()])
 
-    def log_params(self, params: dict):
-        if self.mlflow is not None:
-            self.mlflow.log_params(params=params)
+    def log_params(self, params: Dict[str, Any]) -> None:
+        self.mlflow.log_params(params=params)
 
-    def log_metrics(self, metrics: dict, epoch: int = None):
-        if self.mlflow is not None:
-            self.mlflow.log_metrics(metrics=metrics, step=epoch)
+    def log_metrics(self, metrics: Dict[str, float], epoch: int = None) -> None:
+        self.mlflow.log_metrics(metrics=metrics, step=epoch)
 
-    def finish_run(self):
-        if self.mlflow is not None and self.mlflow_active_run is not None:
-            self.mlflow.end_run()
+    def finish_run(self) -> None:
+        self.mlflow.end_run()
