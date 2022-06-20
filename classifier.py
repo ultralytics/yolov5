05057aa1134e8b1adfcab4e5c8a8d18dcd0a4420
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-20 12:37:39 +0200

    Update --img argument from train.py
    single line

--- classifier.py
+++ classifier.py
@@ -287,12 +287,7 @@ if __name__ == '__main__':
     parser.add_argument('--hyp', type=str, default='data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--batch-size', type=int, default=128, help='total batch size for all GPUs')
-    parser.add_argument('--img-size',
-                        '--imgsz',
-                        '--img',
-                        type=int,
-                        default=128,
-                        help='train, test image sizes (pixels)')
+    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
     parser.add_argument('--optimizer', type=str, choices=['SGD', 'Adam', 'AdamW'], default='Adam', help='optimizer')
     parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
