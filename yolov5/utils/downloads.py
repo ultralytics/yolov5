20f725a465f5fb6b18fa7ecd32ba278d73e7aba8
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-06-20 22:38:58 +0530

    replace relative import

--- yolov5/utils/downloads.py
+++ yolov5/utils/downloads.py
@@ -33,7 +33,7 @@ def gsutil_getsize(url=''):
 
 def safe_download(file, url, url2=None, min_bytes=1E0, error_msg=''):
     # Attempts to download file from url or url2, checks and removes incomplete downloads < min_bytes
-    from utils.general import LOGGER
+    from yolov5.utils.general import LOGGER
 
     file = Path(file)
     assert_msg = f"Downloaded file '{file}' does not exist or size is < min_bytes={min_bytes}"
@@ -54,7 +54,7 @@ def safe_download(file, url, url2=None, min_bytes=1E0, error_msg=''):
 
 def attempt_download(file, repo='ultralytics/yolov5', release='v6.1'):
     # Attempt file download from GitHub release assets if not found locally. release = 'latest', 'v6.1', etc.
-    from utils.general import LOGGER
+    from yolov5.utils.general import LOGGER
 
     def github_assets(repository, version='latest'):
         # Return GitHub repo tag (i.e. 'v6.1') and assets (i.e. ['yolov5s.pt', 'yolov5m.pt', ...])
