bfc17c5d16919078d21db655438881a1ae8d7c3f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-26 21:15:22 +0200

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -27,7 +27,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
 import torchvision
-import torchvision.transforms as T
 from torch.cuda import amp
 from tqdm import tqdm
 
