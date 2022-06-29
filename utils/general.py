c59d666f71cee3e5dacf7c2ec8fa23e9708662ad
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-29 16:12:06 +0200

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -200,13 +200,14 @@ def init_seeds(seed=0):
     # cudnn seed 0 settings are slower and more reproducible, else faster and less reproducible
     import torch.backends.cudnn as cudnn
 
-    # torch.use_deterministic_algorithms(True)
-    # os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+    torch.use_deterministic_algorithms(True)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'  # for torch.use_deterministic_algorithms(True)
+    # os.environ['PYTHONHASHSEED'] = str(seed)
+
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     cudnn.benchmark, cudnn.deterministic = (False, True) if seed == 0 else (True, False)
-    # os.environ['PYTHONHASHSEED'] = str(seed)
     # torch.cuda.manual_seed(seed)
     # torch.cuda.manual_seed_all(seed)  # for multi GPU, exception safe
 
