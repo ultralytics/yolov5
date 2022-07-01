c59733d3266f7dc872a545d2fa857d0bbaa8dd86
Colin Wong <cwongmath@gmail.com>
2022-07-01 13:55:09 -0500

    Expose OpenVINO batch_size similarly to TensorRT

--- models/common.py
+++ models/common.py
@@ -366,6 +366,7 @@ class DetectMultiBackend(nn.Module):
             if not Path(w).is_file():  # if not *.xml
                 w = next(Path(w).glob('*.xml'))  # get *.xml file from *_openvino_model dir
             network = ie.read_model(model=w, weights=Path(w).with_suffix('.bin'))
+            batch_size = network.batch_size
             executable_network = ie.compile_model(network, device_name="CPU")  # device_name="MYRIAD" for Intel NCS2
             output_layer = next(iter(executable_network.outputs))
             meta = Path(w).with_suffix('.yaml')
