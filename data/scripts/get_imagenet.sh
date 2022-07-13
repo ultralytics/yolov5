892a6a4113e1c72334a4cbc5376092a9e2a36da8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-13 13:53:05 +0200

    Update get_imagenet.sh

--- data/scripts/get_imagenet.sh
+++ data/scripts/get_imagenet.sh
@@ -5,26 +5,30 @@
 # parent
 # ├── yolov5
 # └── datasets
-#     └── ImageNet  ← downloads here
+#     └── imagenet  ← downloads here
 
-## 1. Download data
-# get ILSVRC2012_img_val.tar (about 6.3 GB). MD5: 29b22e2961454d5413ddabcf34fc5622
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
-# get ILSVRC2012_img_train.tar (about 138 GB). MD5: 1d675b47d978889d74fa0da5fadfb00e
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
+# Download
+d='./imagenet' # unzip directory
+mkdir $d && cd $d
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar   # 6.3G, 50000 images
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar # 138G, 1281167 images
 
-## 2. Extract training data:
+# Extract train
 mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
 tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
-find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
+find . -name "*.tar" | while read NAME; do
+  mkdir -p "${NAME%.tar}"
+  tar -xvf "${NAME}" -C "${NAME%.tar}"
+  rm -f "${NAME}"
+done
 cd ..
 
-## 3. Extract validation data and move images to subfolders:
+# Extract val and move images into subdirectories
 mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
 
-## 4. Delete corrupted image (optional: PNG under JPEG name that may cause some dataloaders to fail)
+# Delete corrupted image (optional: PNG under JPEG name that may cause dataloaders to fail)
 # rm train/n04266014/n04266014_10835.JPEG
 
-## 5. TFRecords (optional)
+# TFRecords (optional)
 # wget https://raw.githubusercontent.com/tensorflow/models/master/research/slim/datasets/imagenet_lsvrc_2015_synsets.txt
