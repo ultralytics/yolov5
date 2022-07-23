e1fb3e6f411cd1de5a20119b37d81e2d2f8d7baa
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-23 18:55:32 +0200

    Pass imgsz to classify_transforms()

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1121,7 +1121,7 @@ class ClassificationDataset(torchvision.datasets.ImageFolder):
 
     def __init__(self, root, augment, imgsz, cache=False):
         super().__init__(root=root)
-        self.torch_transforms = classify_transforms()
+        self.torch_transforms = classify_transforms(imgsz)
         self.album_transforms = classify_albumentations(augment, imgsz) if augment else None
         self.cache_ram = cache is True or cache == 'ram'
         self.cache_disk = cache == 'disk'
