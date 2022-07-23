46c1a3ba16d4a2dc6c0a78e55458008f33c0ea84
xyzluo <quantumabacus1@gmail.com>
2022-07-23 14:27:32 -0400

    add custom data training to  loco dataset

--- train.sh
+++ train.sh
@@ -4,4 +4,7 @@
 # 工作服(反光衣) 2类 单独训练
 # docker run -it --gpus all -v /home/epi/gary/:/home/epi/gary nvcr.io/nvidia/pytorch:21.02-py3
 # cd root@dc7f301c3e74:/home/epi/gary/ml_work_space/gary_orig/github/yolov5#
-python train.py --img 640 --batch 8 --epochs 100 --data ./data/Reflective_vests.yaml --cfg ./models/yolov5s_fs.yaml --weights './yolov5s.pt' --device 0
+# python train.py --img 640 --batch 8 --epochs 100 --data ./data/Reflective_vests.yaml --cfg ./models/yolov5s_fs.yaml --weights './yolov5s.pt' --device 0
+
+# loco 5 classes train
+python train.py --img 640 --batch 8 --epochs 100 --data ./data/loco.yaml --cfg ./models/yolov5s_loco.yaml --weights './yolov5s.pt' --device 0
