429238f7cd38c1041d6f2e57fdfbce585afab4f7
UnglvKitDe <unglv@student.kit.edu>
2022-07-23 22:34:48 +0200

    Fix BGR->RGB Bug in albumentations https://github.com/ultralytics/yolov5/issues/8641

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -39,8 +39,10 @@ class Albumentations:
 
     def __call__(self, im, labels, p=1.0):
         if self.transform and random.random() < p:
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
             new = self.transform(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
             im, labels = new['image'], np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
+            im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
         return im, labels
 
 
