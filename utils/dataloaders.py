a06efbda4124bc98ed4734b2229a6a304733c716
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-25 15:09:52 +0200

    Update

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1122,7 +1122,7 @@ class ClassificationDataset(torchvision.datasets.ImageFolder):
     def __init__(self, root, augment, imgsz, cache=False):
         super().__init__(root=root)
         self.torch_transforms = classify_transforms(imgsz)
-        self.album_transforms = classify_albumentations(augment, imgsz) if augment else None
+        self.album_transforms = classify_albumentations(augment, imgsz)  # if augment else None
         self.cache_ram = cache is True or cache == 'ram'
         self.cache_disk = cache == 'disk'
         self.samples = [list(x) + [Path(x[0]).with_suffix('.npy'), None] for x in self.samples]  # file, index, npy, im
