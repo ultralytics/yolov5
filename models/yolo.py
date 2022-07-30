aca25bdcb9572696df65ab49b3fc825d6cd892da
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-30 22:12:27 +0200

    Detect.inplace=False for safe multithread inference

--- models/yolo.py
+++ models/yolo.py
@@ -50,7 +50,7 @@ class Detect(nn.Module):
         self.anchor_grid = [torch.zeros(1)] * self.nl  # init anchor grid
         self.register_buffer('anchors', torch.tensor(anchors).float().view(self.nl, -1, 2))  # shape(nl,na,2)
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
-        self.inplace = inplace  # use in-place ops (e.g. slice assignment)
+        self.inplace = inplace  # use inplace ops (e.g. slice assignment)
 
     def forward(self, x):
         z = []  # inference output
