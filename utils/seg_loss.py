b89a2d65ef72e5042ad66cd0bb9419b2094a3840
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-08-03 20:47:59 +0530

    make compatible with torch 1.12

--- utils/seg_loss.py
+++ utils/seg_loss.py
@@ -294,8 +294,8 @@ class ComputeLoss:
         ], device=targets.device, ).float() * g)  # offsets
 
         for i in range(self.nl):
-            anchors = self.anchors[i]
-            gain[2:6] = torch.tensor(p[i].shape)[[3, 2, 3, 2]]  # xyxy gain
+            anchors, shape = self.anchors[i], p[i].shape
+            gain[2:6] = torch.tensor(shape)[[3, 2, 3, 2]] # xyxy gain
 
             # Match targets to anchors
             t = targets * gain
@@ -328,7 +328,7 @@ class ComputeLoss:
             # Append
             a = t[:, 6].long()  # anchor indices
             tidx = t[:, 7].long()
-            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
+            indices.append((b, a, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1))) # image, anchor, grid
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
