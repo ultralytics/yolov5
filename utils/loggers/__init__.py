2fc333af585f07d4c7e9abef77b72bc29b69419c
Ganesh Jagadeesan <cjagadeesan@dataminr.com>
2022-08-08 09:45:24 -0400

    fix: corrected the LOGGERS tuple

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -17,8 +17,7 @@ from utils.loggers.wandb.wandb_utils import WandbLogger
 from utils.plots import plot_images, plot_results
 from utils.torch_utils import de_parallel
 
-LOGGERS = ('csv', 'tb', 'wandb', 'clearml'
-           'mlflow')  # text-file, TensorBoard, Weights & Biases, Mlflow
+LOGGERS = ('csv', 'tb', 'wandb', 'clearml', 'mlflow')  # text-file, TensorBoard, Weights & Biases, Mlflow
 RANK = int(os.getenv('RANK', -1))
 
 try:
