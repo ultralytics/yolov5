43efec3cc4f27d6962d4bfe6061f908c5a1789d7
Yana <savchenkoyana97@gmail.com>
2022-08-09 17:20:48 +0700

    Fix bug with resume

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 
     # Resume
     best_fitness, start_epoch = 0.0, 0
-    if pretrained:
+    if resume:
         best_fitness, start_epoch, epochs = smart_resume(ckpt, optimizer, ema, weights, epochs, resume)
         del ckpt, csd
 
