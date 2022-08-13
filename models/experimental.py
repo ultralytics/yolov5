9c17a70739341a73e7b8737e48624f9ec77837b5
glennjocher <glenn.jocher@ultralytics.com>
2022-08-13 17:50:23 +0200

    fuse fix for resnet50

--- models/experimental.py
+++ models/experimental.py
@@ -79,7 +79,7 @@ def attempt_load(weights, device=None, inplace=True, fuse=True):
     for w in weights if isinstance(weights, list) else [weights]:
         ckpt = torch.load(attempt_download(w), map_location='cpu')  # load
         ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()  # FP32 model
-        model.append(ckpt.fuse().eval() if fuse else ckpt.eval())  # fused or un-fused model in eval mode
+        model.append(ckpt.fuse().eval() if fuse and hasattr(ckpt, 'fuse') else ckpt.eval())  # model in eval mode
 
     # Compatibility updates
     for m in model.modules():
