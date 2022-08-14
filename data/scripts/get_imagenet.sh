13ec3364c182c791d22335e4ad86b3893d1a96e2
glennjocher <glenn.jocher@ultralytics.com>
2022-08-14 22:59:57 +0200

    remove verbose

--- data/scripts/get_imagenet.sh
+++ data/scripts/get_imagenet.sh
@@ -28,10 +28,10 @@ mkdir -p $d && cd $d
 if [ "$train" == "true" ]; then
   wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar # download 138G, 1281167 images
   mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
-  tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
+  tar -xf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
   find . -name "*.tar" | while read NAME; do
     mkdir -p "${NAME%.tar}"
-    tar -xvf "${NAME}" -C "${NAME%.tar}"
+    tar -xf "${NAME}" -C "${NAME%.tar}"
     rm -f "${NAME}"
   done
   cd ..
@@ -40,7 +40,7 @@ fi
 # Download/unzip val
 if [ "$val" == "true" ]; then
   wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar # download 6.3G, 50000 images
-  mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
+  mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xf ILSVRC2012_img_val.tar
   wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash # move into subdirs
 fi
 
