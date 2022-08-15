fd388d69008ff587adf96beff95d084e023e1d44
glennjocher <glenn.jocher@ultralytics.com>
2022-08-15 23:13:54 +0200

    update opt file printing

--- utils/general.py
+++ utils/general.py
@@ -195,7 +195,10 @@ def print_args(args: Optional[dict] = None, show_file=True, show_fcn=False):
     if args is None:  # get args automatically
         args, _, _, frm = inspect.getargvalues(x)
         args = {k: v for k, v in frm.items() if k in args}
-    file = Path(file).relative_to(ROOT).with_suffix('')
+    try:
+        file = Path(file).resolve().relative_to(ROOT).with_suffix('')
+    except ValueError:
+        file = Path(file).stem
     s = (f'{file}: ' if show_file else '') + (f'{fcn}: ' if show_fcn else '')
     LOGGER.info(colorstr(s) + ', '.join(f'{k}={v}' for k, v in args.items()))
 
