a5a47c52ca2d52d0170138e5449a93559f3f5e26
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-18 20:26:18 +0200

    Remove unused `time_sync` import (#9026)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- val.py
+++ val.py
@@ -42,7 +42,7 @@ from utils.general import (LOGGER, Profile, check_dataset, check_img_size, check
                            scale_coords, xywh2xyxy, xyxy2xywh)
 from utils.metrics import ConfusionMatrix, ap_per_class, box_iou
 from utils.plots import output_to_target, plot_images, plot_val_study
-from utils.torch_utils import select_device, smart_inference_mode, time_sync
+from utils.torch_utils import select_device, smart_inference_mode
 
 
 def save_one_txt(predn, save_conf, shape, file):
