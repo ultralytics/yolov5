f258cf8b37aeb3062230d43e1e9a4bf3b9874588
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-20 17:17:35 +0200

    Add `--save-hybrid` mAP warning (#9050)
    * Add `--save-hybrid` mAP warning
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update val.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- val.py
+++ val.py
@@ -365,6 +365,8 @@ def main(opt):
     if opt.task in ('train', 'val', 'test'):  # run normally
         if opt.conf_thres > 0.001:  # https://github.com/ultralytics/yolov5/issues/1466
             LOGGER.info(f'WARNING: confidence threshold {opt.conf_thres} > 0.001 produces invalid results ⚠️')
+        if opt.save_hybrid:
+            LOGGER.info('WARNING: --save-hybrid will return high mAP from hybrid labels, not from predictions alone ⚠️')
         run(**vars(opt))
 
     else:
