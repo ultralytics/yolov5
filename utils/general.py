3e2312d7b748d0e8dbc9813d14a2edbc4671bc12
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-08-20 20:04:53 +0530

    no deterministic behaviour

--- utils/general.py
+++ utils/general.py
@@ -202,7 +202,7 @@ def init_seeds(seed=0, deterministic=False):
     import torch.backends.cudnn as cudnn
 
     if deterministic and check_version(torch.__version__, '1.12.0'):  # https://github.com/ultralytics/yolov5/pull/8213
-        torch.use_deterministic_algorithms(True)
+        #torch.use_deterministic_algorithms(True)
         os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
         os.environ['PYTHONHASHSEED'] = str(seed)
 
