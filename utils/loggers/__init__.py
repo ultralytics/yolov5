5d0bc4b126f42fa6507014b3a728e3b7656e179b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-21 01:56:21 +0200

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -187,7 +187,7 @@ class Loggers():
 
     def on_model_save(self, last, epoch, final_epoch, best_fitness, fi):
         # Callback runs on model save event
-        if ((epoch + 1) % self.opt.save_period == 0 and not final_epoch) and self.opt.save_period != -1:
+        if (epoch + 1) % self.opt.save_period == 0 and not final_epoch and self.opt.save_period != -1:
             if self.wandb:
                 self.wandb.log_model(last.parent, self.opt, epoch, fi, best_model=best_fitness == fi)
             if self.clearml:
