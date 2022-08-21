6036c51d4ff918dd3b0b5fb1898818247c0b3b01
Ganesh Jagadeesan <ganesh.jcs@gmail.com>
2022-08-21 15:12:08 -0400

    updated mutablemapping import

--- utils/loggers/mlflow/mlflow_utils.py
+++ utils/loggers/mlflow/mlflow_utils.py
@@ -18,10 +18,7 @@ try:
 except (ImportError, AssertionError):
     mlflow = None
 
-if sys.version_info.major == 3 and sys.version_info.minor >= 10:
-    from collections.abc import MutableMapping
-else:
-    from collections.abc import MutableMapping
+from collections.abc import MutableMapping
 
 
 class MlflowLogger:
