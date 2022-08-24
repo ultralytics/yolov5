bf9e19af3c56a2a7af73d69a7c58ca37e1490ced
glennjocher <glenn.jocher@ultralytics.com>
2022-08-24 15:30:17 +0200

    optimize utils/segment/general crop() 2

--- utils/segment/general.py
+++ utils/segment/general.py
@@ -12,20 +12,12 @@ def crop(masks, boxes):
         - masks should be a size [h, w, n] tensor of masks
         - boxes should be a size [n, 4] tensor of bbox coords in relative point form
     """
+
     h, w, n = masks.shape
     x1, y1, x2, y2 = torch.chunk(boxes.T[None], 4, 1)  # x1 shape(1,1,n)
-
-    rows = torch.arange(w, device=masks.device, dtype=x1.dtype).view(1, -1, 1).expand(h, w, n)  # shape(h,w,n)
-    cols = torch.arange(h, device=masks.device, dtype=x1.dtype).view(-1, 1, 1).expand(h, w, n)  # shape(h,w,n)
-
-    # (1, w, 1), (1, 1, n) -> (1, w, n)
-    masks_left = rows >= x1  # shape(h,w,n)
-    masks_right = rows < x2  # shape(h,w,n)
-    # (h, 1, 1), (1, 1, n) -> (h, 1, n)
-    masks_up = cols >= y1  # shape(h,w,n)
-    masks_down = cols < y2  # shape(h,w,n)
-
-    return masks * (masks_left * masks_right * masks_up * masks_down).float()
+    r = torch.arange(w, device=masks.device, dtype=x1.dtype)[None, :, None]  # rows shape(1,w,1)
+    c = torch.arange(h, device=masks.device, dtype=x1.dtype)[:, None, None]  # cols shape(h,1,1)
+    return masks * ((r >= x1) * (r < x2) * (c >= y1) * (c < y2)).float()
 
 
 def process_mask_upsample(proto_out, out_masks, bboxes, shape):
