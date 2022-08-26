cc43b76ac69e44e598c53e354969cac863578ff1
glennjocher <glenn.jocher@ultralytics.com>
2022-08-26 22:39:08 +0200

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -43,6 +43,8 @@ class Detect(nn.Module):
     def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
         super().__init__()
         self.nc = nc  # number of classes
+        self.nm = 0  # number of masks
+        self.npr = 0  # number of protos
         self.no = nc + 5  # number of outputs per anchor
         self.nl = len(anchors)  # number of detection layers
         self.na = len(anchors[0]) // 2  # number of anchors
