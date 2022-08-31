0a03dd581f6bf70d62c6795f4d234a6735274a3f
Konstantin Gulin <66528950+KSGulin@users.noreply.github.com>
2022-08-31 12:56:26 +0100

    Update build script to remove now gone folders (#97)

--- build_wheel.sh
+++ build_wheel.sh
@@ -1,6 +1,6 @@
 python3 -m pip install --upgrade build
 mkdir yolov5
-cp -r data models models_v5.0 utils .pre-commit-config.yaml $(ls *.py) requirements.txt yolov5/
+cp -r models utils .pre-commit-config.yaml $(ls *.py) requirements.txt yolov5/
 grep --include=\*.py -rnl 'yolov5/' -e "from models" | xargs -i@ sed -i 's/from models/from yolov5.models/g' @
 grep --include=\*.py -rnl 'yolov5/' -e "from utils" | xargs -i@ sed -i 's/from utils/from yolov5.utils/g' @
 sed -i '/^sparseml/d' yolov5/requirements.txt
