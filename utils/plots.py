558ee483d11fd4dfffa4ebb0d1c738d619c0ff19
glennjocher <glenn.jocher@ultralytics.com>
2022-09-03 00:06:15 +0200

    Annotator mask optimization

--- utils/plots.py
+++ utils/plots.py
@@ -113,15 +113,14 @@ class Annotator:
                             thickness=tf,
                             lineType=cv2.LINE_AA)
 
-    def masks(self, masks, colors, alpha=0.5):
-        # Add multiple masks of shape(n,h,w) with colors list([r,g,b], [r,g,b], ...)
+    def masks(self, masks, colors, alpha=0.5, eps=1e-7):
+        # Add multiple masks of shape(h,w,n) with colors list([r,g,b], [r,g,b], ...)
         if len(masks):
-            masks = np.ascontiguousarray(masks).astype(np.float32)[..., None] / 255.0  # shape(n,h,w,1)
-            colors = np.array(colors, dtype=np.float32)[:, None, None]  # shape(n,1,1,3)
-            masks_color = masks * (colors * alpha)  # shape(n,h,w,3)
-            inv_alph_masks = (1 - masks * alpha).cumprod(0)
-            mcs = (masks_color * inv_alph_masks).sum(0) * 2  # mask color summand shape(h,w,3)
-            self.im[:] = self.im * inv_alph_masks[-1] + mcs
+            masks = masks.astype(np.float32) / 255.0  # shape(h,w,n)
+            colors = np.array(colors, dtype=np.uint8)  # shape(n,3)
+            s = masks.sum(2, keepdims=True)
+            masks = masks @ colors / (s + eps)  # (h,w,n) @ (n,3) = (h,w,3)
+            self.im[:] = masks * alpha + self.im * (1 - s * alpha)
 
     def rectangle(self, xy, fill=None, outline=None, width=1):
         # Add rectangle to image (PIL-only)
