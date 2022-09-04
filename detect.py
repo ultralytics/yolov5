afb95758165a2fcc7eed8249d58cb33d469a6370
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-04 16:13:07 +0200

    Update detect.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- detect.py
+++ detect.py
@@ -74,7 +74,7 @@ def run(
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
-        skip_frame=1,  # number of frames to skip
+        skip_frame=1,  # number of video frames to skip
 ):
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
@@ -237,7 +237,7 @@ def parse_opt():
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--skip-frame', type=int, default=1, help='number of frames to skip')
+    parser.add_argument('--skip-frame', type=int, default=1, help='number of video frames to skip')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
