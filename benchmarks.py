4016d72807d729228c269bbca2b072753aed899b
glennjocher <glenn.jocher@ultralytics.com>
2022-09-04 19:15:21 +0200

    Add segmentation benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -34,7 +34,7 @@ from pathlib import Path
 import pandas as pd
 
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[1]  # YOLOv5 root directory
+ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 # ROOT = ROOT.relative_to(Path.cwd())  # relative
