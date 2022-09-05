f39ad862e3e2c4f8faadedde88d86815fa72e381
DN6 <dhruv.nair@gmail.com>
2022-09-05 10:45:43 +0000

    set types for integer/float env variables

--- utils/loggers/comet/__init__.py
+++ utils/loggers/comet/__init__.py
@@ -43,11 +43,11 @@ COMET_UPLOAD_DATASET = os.getenv("COMET_UPLOAD_DATASET", "false").lower() == "tr
 # Evaluation Settings
 COMET_LOG_CONFUSION_MATRIX = os.getenv("COMET_LOG_CONFUSION_MATRIX", "true").lower() == "true"
 COMET_LOG_PREDICTIONS = os.getenv("COMET_LOG_PREDICTIONS", "false").lower() == "true"
-COMET_MAX_IMAGE_UPLOADS = os.getenv("COMET_MAX_IMAGE_UPLOADS", 100)
+COMET_MAX_IMAGE_UPLOADS = int(os.getenv("COMET_MAX_IMAGE_UPLOADS", 100))
 
 # Confusion Matrix Settings
-CONF_THRES = os.getenv("CONF_THRES", 0.001)
-IOU_THRES = os.getenv("IOU_THRES", 0.6)
+CONF_THRES = float(os.getenv("CONF_THRES", 0.001))
+IOU_THRES = float(os.getenv("IOU_THRES", 0.6))
 
 # Batch Logging Settings
 COMET_LOG_BATCH_METRICS = os.getenv("COMET_LOG_BATCH_METRICS", "false").lower() == "true"
