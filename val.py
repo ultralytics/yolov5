1b3bacb932322efd91fa0a8d2bd1d28829ad4e20
glennjocher <glenn.jocher@ultralytics.com>
2022-09-10 12:13:57 +0300

    Fix CI error

--- val.py
+++ val.py
@@ -266,7 +266,7 @@ def run(
             plot_images(im, targets, paths, save_dir / f'val_batch{batch_i}_labels.jpg', names)  # labels
             plot_images(im, output_to_target(preds), paths, save_dir / f'val_batch{batch_i}_pred.jpg', names)  # pred
 
-        callbacks.run('on_val_batch_end', batch_i, im, targets, paths, shapes, out)
+        callbacks.run('on_val_batch_end', batch_i, im, targets, paths, shapes, preds)
 
     # Compute metrics
     stats = [torch.cat(x, 0).cpu().numpy() for x in zip(*stats)]  # to numpy
