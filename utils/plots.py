b77257479b9a8a23367f28e1bfd64dcbd67bfe0c
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-10 22:21:24 +0300

    labels.jpg names fix
    Partially resolves https://github.com/ultralytics/yolov5/issues/9360
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/plots.py
+++ utils/plots.py
@@ -364,7 +364,7 @@ def plot_labels(labels, names=(), save_dir=Path('')):
     ax[0].set_ylabel('instances')
     if 0 < len(names) < 30:
         ax[0].set_xticks(range(len(names)))
-        ax[0].set_xticklabels(names, rotation=90, fontsize=10)
+        ax[0].set_xticklabels(list(names.values()), rotation=90, fontsize=10)
     else:
         ax[0].set_xlabel('classes')
     sn.histplot(x, x='x', y='y', ax=ax[2], bins=50, pmax=0.9)
