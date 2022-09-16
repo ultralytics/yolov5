9316e342f793901f4928f0417a2153787b9600ed
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-16 21:32:07 +0200

    Update general.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -223,7 +223,7 @@ def init_seeds(seed=0, deterministic=False):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)  # for Multi-GPU, exception safe
-    torch.backends.cudnn.benchmark = True  # for faster training
+    # torch.backends.cudnn.benchmark = True  # AutoBatch problem https://github.com/ultralytics/yolov5/issues/9287
     if deterministic and check_version(torch.__version__, '1.12.0'):  # https://github.com/ultralytics/yolov5/pull/8213
         torch.use_deterministic_algorithms(True)
         torch.backends.cudnn.deterministic = True
