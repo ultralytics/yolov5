43654e95bbe6c749c357998ac8d2d4faff058235
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-18 13:29:05 +0200

    TensorRT SegmentationModel fix

--- export.py
+++ export.py
@@ -136,16 +136,13 @@ def export_onnx(model, im, file, opset, dynamic, simplify, prefix=colorstr('ONNX
 
     output_names = ['output0']
     if dynamic:
+        dynamic = {'images': {0: 'batch', 2: 'height', 3: 'width'}}  # shape(1,3,640,640)
         if isinstance(model, SegmentationModel):
             output_names = ['output0', 'output1']
-            dynamic = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # shape(1,3,640,640)
-                       'output0': {0: 'batch', 1: 'anchors'},  # shape(1,25200,85)
-                       'output1': {0: 'batch', 2: 'mask_height', 3: 'mask_width'}}  # shape(1,32,160,160)
+            dynamic['output0'] = {0: 'batch', 1: 'anchors'}  # shape(1,25200,85)
+            dynamic['output1'] = {0: 'batch', 2: 'mask_height', 3: 'mask_width'}  # shape(1,32,160,160)
         elif isinstance(model, DetectionModel):
-            dynamic = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # shape(1,3,640,640)
-                       'output0': {0: 'batch', 1: 'anchors'}}  # shape(1,25200,85)
-        else:
-            dynamic = {'images': {0: 'batch', 2: 'height', 3: 'width'}}  # shape(1,3,640,640)
+            dynamic['output0'] = {0: 'batch', 1: 'anchors'}  # shape(1,25200,85)
 
     torch.onnx.export(
         model.cpu() if dynamic else model,  # --dynamic only compatible with cpu
