095f601d9d32ea0f0afd47554c068659939ecf4e
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-20 12:22:02 +0200

    Segmentation `polygons2masks_overlap()` in `np.int32` (#9493)
    * Segmentation `polygons2masks_overlap()` in `np.int32`
    
    May resolve https://github.com/ultralytics/yolov5/issues/9461
    
    WARNING: Masks should be uint8 for fastest speed, change needs profiling results to determine impact.
    
    @AyushExel @Laughing-q 
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update dataloaders.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/segment/dataloaders.py
+++ utils/segment/dataloaders.py
@@ -308,7 +308,8 @@ def polygons2masks(img_size, polygons, color, downsample_ratio=1):
 
 def polygons2masks_overlap(img_size, segments, downsample_ratio=1):
     """Return a (640, 640) overlap mask."""
-    masks = np.zeros((img_size[0] // downsample_ratio, img_size[1] // downsample_ratio), dtype=np.uint8)
+    masks = np.zeros((img_size[0] // downsample_ratio, img_size[1] // downsample_ratio),
+                     dtype=np.int32 if len(segments) > 255 else np.uint8)
     areas = []
     ms = []
     for si in range(len(segments)):
