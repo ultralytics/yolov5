4d8fca81ef898c80b663b90dd53971b05f516b12
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-21 19:14:17 +0200

    Update predict.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- classify/predict.py
+++ classify/predict.py
@@ -102,7 +102,7 @@ def run(
     seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
     for path, im, im0s, vid_cap, s in dataset:
         with dt[0]:
-            im = torch.Tensor(im).to(device)
+            im = torch.Tensor(im).to(mdoel.device)
             im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
             if len(im.shape) == 3:
                 im = im[None]  # expand for batch dim
