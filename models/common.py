1cd988059180a087a84faabd6b107d53116b5bb0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-24 00:35:39 +0200

    Update is_url()

--- models/common.py
+++ models/common.py
@@ -591,7 +591,7 @@ class DetectMultiBackend(nn.Module):
         from export import export_formats
         from utils.downloads import is_url
         sf = list(export_formats().Suffix)  # export suffixes
-        if not is_url(p, check_exists=False):
+        if not is_url(p, check=False):
             check_suffix(p, sf)  # checks
         url = urlparse(p)  # if url may be Triton inference server
         types = [s in Path(p).name for s in sf]
