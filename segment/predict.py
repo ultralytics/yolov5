a9f68856a8b02c9e2e15d32d9d04210a1fccad1d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-25 15:48:34 +0200

    Segment fix

--- segment/predict.py
+++ segment/predict.py
@@ -180,7 +180,7 @@ def run(
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         annotator.box_label(xyxy, label, color=colors(c, True))
-                        annotator.draw.polygon(segments[j], outline=colors(c, True), width=3)
+                        # annotator.draw.polygon(segments[j], outline=colors(c, True), width=3)
                     if save_crop:
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
