bfdf6814abf071fde30a6c59a082d2232cf82ae6
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-26 20:26:39 +0200

    Update export.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -564,7 +564,7 @@ def run(
         dir = Path('segment' if seg else 'classify' if cls else '')
         h = '--half' if half else ''  # --half FP16 inference arg
         s = "WARNING ⚠️ ClassificationModel not yet supported for PyTorch Hub AutoShape inference" if cls else \
-            s = "WARNING ⚠️ SegmentationModel not yet supported for PyTorch Hub AutoShape inference" if seg else ''
+            "WARNING ⚠️ SegmentationModel not yet supported for PyTorch Hub AutoShape inference" if seg else ''
         LOGGER.info(f'\nExport complete ({time.time() - t:.1f}s)'
                     f"\nResults saved to {colorstr('bold', file.parent.resolve())}"
                     f"\nDetect:          python {dir / ('detect.py' if det else 'predict.py')} --weights {f[-1]} {h}"
