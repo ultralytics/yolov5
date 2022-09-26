8e2fe32c2e8d0d43b49720fb6a8e89995fb060f8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-26 20:53:58 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -149,7 +149,7 @@ class BottleneckCSP(nn.Module):
 
 class CrossConv(nn.Module):
     # Cross Convolution Downsample
-    def __init__(self, c1, c2, k=3, s=1, g=1, e=1.0, shortcut=False):
+    def __init__(self, c1, c2, k=5, s=1, g=1, e=1.0, shortcut=False):
         # ch_in, ch_out, kernel, stride, groups, expansion, shortcut
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
