6a1d78b879fd5134beccaeca0cdd7e7a9153d67e
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-26 22:26:55 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -194,7 +194,7 @@ class C3x(C3):
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
         super().__init__(c1, c2, n, shortcut, g, e)
         c_ = int(c2 * e)
-        self.m = nn.Sequential(*(CrossConv(c_, c_, 5, 1, g, 1.0, shortcut) for _ in range(n)))
+        self.m = nn.Sequential(*(CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)))
 
 
 class C3TR(C3):
