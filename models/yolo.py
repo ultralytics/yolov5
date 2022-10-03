bc986e9b76e779666a28d02435f785ab67330454
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-03 15:33:26 +0200

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -112,7 +112,7 @@ class DetectSplit(nn.Module):
         self.shape = (0, 0)  # initial grid shape
         # self.cv1 = nn.ModuleList(Conv(x, x) for x in ch)
         self.cv2 = nn.ModuleList(nn.Sequential(Conv(x, x, 1), nn.Conv2d(x, 4, 1, padding=0)) for x in ch)
-        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, x, 1), nn.Conv2d(x, self.no - 4, 1, padding=0)) for x in ch)
+        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, x, 1), nn.Conv2d(x, self.no - 4, 3, padding=1)) for x in ch)
 
     def forward(self, x):
         for i in range(self.nl):
