acc83e3b2469ea50dc8b078c8ec7dc9d57827e7d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-04 02:41:41 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -190,6 +190,21 @@ class C2(nn.Module):
         return self.cv2(torch.cat((self.m(a), b), 1))
 
 
+class C2a(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * c_, 1, 1)
+        self.cv2 = Conv(c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+        self.c_ = c_
+
+    def forward(self, x):
+        a, b = self.cv1(x).split((self.c_, self.c_), 1)
+        return self.cv2(self.m(a) + b)
+
+
 class C3b(nn.Module):
     # CSP Bottleneck with 3 convolutions
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
