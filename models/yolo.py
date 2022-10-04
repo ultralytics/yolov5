67a5e2f7bd1d065d792c260bd19367b4b7aff806
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-04 13:05:21 +0200

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -333,14 +333,31 @@ class DetectionModel(BaseModel):
         # https://arxiv.org/abs/1708.02002 section 3.3 for 5-80 splits
         # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
         m = self.model[-1]  # Detect() module
-        for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+        for a, c, b, s in zip(m.cv1, m.cv2, m.cv3, m.stride):  # from
             ai = a[-1].bias  # conv.bias(255) to (3,85)
             ai.data[2:4] = -1.38629  # wh = 0.25 + (x - 1.38629).sigmoid() * 3.75
-            ai.data[4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
             a[-1].bias = torch.nn.Parameter(ai, requires_grad=True)
+
+            ci = c[-1].bias  # conv.bias(255) to (3,85)
+            ci.data[0] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
+            ci[-1].bias = torch.nn.Parameter(ci, requires_grad=True)
+
             bi = b[-1].bias  # conv.bias(255) to (3,85)
             bi.data[:m.nc] += math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # cls
             b[-1].bias = torch.nn.Parameter(bi, requires_grad=True)
+
+    # def _initialize_biases_split(self, cf=None):  # initialize biases into Detect(), cf is class frequency
+    #     # https://arxiv.org/abs/1708.02002 section 3.3 for 5-80 splits
+    #     # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
+    #     m = self.model[-1]  # Detect() module
+    #     for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+    #         ai = a[-1].bias  # conv.bias(255) to (3,85)
+    #         ai.data[2:4] = -1.38629  # wh = 0.25 + (x - 1.38629).sigmoid() * 3.75
+    #         ai.data[4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
+    #         a[-1].bias = torch.nn.Parameter(ai, requires_grad=True)
+    #         bi = b[-1].bias  # conv.bias(255) to (3,85)
+    #         bi.data[:m.nc] += math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # cls
+    #         b[-1].bias = torch.nn.Parameter(bi, requires_grad=True)
 
     # def _initialize_biases_split(self, cf=None):  # initialize biases into Detect(), cf is class frequency
     #     # https://arxiv.org/abs/1708.02002 section 3.3 for 4-81 splits
