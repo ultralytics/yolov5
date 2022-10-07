23409ab584ccad4ad04a258dff7a713d3f8ce287
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-07 16:53:17 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -107,14 +107,14 @@ class Bottleneck(nn.Module):
     def __init__(self, c1, c2, shortcut=True, g=1, k=(1, 3), e=0.5):  # ch_in, ch_out, shortcut, kernels, groups, expand
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, k[0], 1)
-        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
-        # self.cv2 = Conv(c_, c_, k[1], 1, g=2)
-        # self.cv3 = Conv(c_, c2, k[1], 1, g=2)
+        # self.cv1 = Conv(c1, c_, k[0], 1)
+        # self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.cv2 = Conv(c_, c_, k[1], 1, g=2)
+        self.cv3 = Conv(c_, c2, k[1], 1, g=2)
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
-        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+        return x + self.cv3(self.cv2(x)) if self.add else self.cv3(self.cv2(x))
 
 
 class BottleneckCSP(nn.Module):
