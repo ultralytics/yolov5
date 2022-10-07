482b237458a5b61972cf50808c5d0436a449765b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-07 16:42:04 +0200

    Update

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -215,8 +215,8 @@ def initialize_weights(model):
             pass  # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
         elif t is nn.BatchNorm2d:
             pass
-            # m.eps = 1e-3
-            # m.momentum = 0.03
+            m.eps = 1e-3
+            m.momentum = 0.03
         elif t in [nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU]:
             m.inplace = True
 
