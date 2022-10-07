4ed18530883c5f709a785913f305732695be9928
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-07 16:49:54 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -109,7 +109,7 @@ class Bottleneck(nn.Module):
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, k[0], 1)
         # self.cv2 = Conv(c_, c2, k[1], 1, g=g)
-        self.cv2 = Conv(c_, c2, k[1], 1, g=2)
+        self.cv2 = Conv(c_, c_, k[1], 1, g=2)
         self.cv3 = Conv(c_, c2, k[1], 1, g=2)
         self.add = shortcut and c1 == c2
 
