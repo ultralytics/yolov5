4fec5eba53f343bcd1ca95c466fce485591771af
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-08 13:34:39 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -45,7 +45,7 @@ class Conv(nn.Module):
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
-        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.act = Attention(c2) if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
