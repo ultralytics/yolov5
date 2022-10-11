f9a86cf90cde5c0066a9907b766daa27e5c6aaf3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-11 17:55:07 +0200

    Update

--- utils/loss.py
+++ utils/loss.py
@@ -5,6 +5,7 @@ Loss functions
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from utils.metrics import bbox_iou
 from utils.torch_utils import de_parallel
@@ -88,7 +89,7 @@ class QFocalLoss(nn.Module):
             return loss
 
 
-class ComputeLoss:
+class ComputeLoss_OLD:
     sort_obj_iou = False
 
     # Compute losses
@@ -162,10 +163,6 @@ class ComputeLoss:
                     t[range(n), tcls[i]] = self.cp
                     lcls += self.BCEcls(pcls, t)  # BCE
 
-                # Append targets to text file
-                # with open('targets.txt', 'a') as file:
-                #     [file.write('%11.5g ' * 4 % tuple(x) + '\n') for x in torch.cat((txy[i], twh[i]), 1)]
-
             obji = self.BCEobj(pi[:, 4], tobj)
             lobj += obji * self.balance[i]  # obj loss
             if self.autobalance:
@@ -232,6 +229,135 @@ class ComputeLoss:
             # Append
             indices.append((b, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1)))  # image, grid_y, grid_x indices
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
+            tcls.append(c)  # class
+
+        return tcls, tbox, indices
+
+
+class ComputeLoss:
+    sort_obj_iou = False
+
+    # Compute losses
+    def __init__(self, model, autobalance=False):
+        device = next(model.parameters()).device  # get model device
+        h = model.hyp  # hyperparameters
+
+        # Define criteria
+        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
+        BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
+
+        # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
+        self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
+
+        # Focal loss
+        g = h['fl_gamma']  # focal loss gamma
+        if g > 0:
+            BCEcls, BCEobj = FocalLoss(BCEcls, g), FocalLoss(BCEobj, g)
+
+        m = de_parallel(model).model[-1]  # Detect() module
+        self.balance = {3: [4.0, 1.0, 0.4]}.get(m.nl, [4.0, 1.0, 0.25, 0.06, 0.02])  # P3-P7
+        self.ssi = list(m.stride).index(16) if autobalance else 0  # stride 16 index
+        self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = BCEcls, BCEobj, 1.0, h, autobalance
+        self.nc = m.nc  # number of classes
+        self.nl = m.nl  # number of layers
+        self.anchors = m.anchors
+        self.device = device
+
+    def __call__(self, p, targets):  # predictions, targets
+        lcls = torch.zeros(1, device=self.device)  # class loss
+        lbox = torch.zeros(1, device=self.device)  # box loss
+        lobj = torch.zeros(1, device=self.device)  # object loss
+        tcls, tbox, indices = self.build_targets(p, targets)  # targets
+        criteria = nn.BCEWithLogitsLoss(reduction='none')
+
+        # Losses
+        assignment = []
+        for i, pi in enumerate(p):  # layer index, layer predictions
+            b, gj, gi = indices[i]  # image, anchor, gridy, gridx
+            tobj = torch.zeros((pi.shape[0], pi.shape[2], pi.shape[3]), dtype=pi.dtype,
+                               device=self.device)  # target obj
+
+            n = b.shape[0]  # number of targets
+            obji = criteria(pi[:, 4], tobj)
+            if n:
+                # pxy, pwh, _, pcls = pi[b, a, gj, gi].tensor_split((2, 4, 5), dim=1)  # faster, requires torch 1.8.0
+                pxy, pwh, _, pcls = pi[b, :, gj, gi].split((2, 2, 1, self.nc), 2)  # target-subset of predictions
+
+                # Regression
+                pxy = pxy.sigmoid() * 1.6 - 0.3
+                pwh = (0.2 + pwh.sigmoid() * 4.8) * self.anchors[i]
+                pbox = torch.cat((pxy, pwh), 2)  # predicted box
+                iou = bbox_iou(pbox, tbox[i], CIoU=True).squeeze()  # iou(prediction, target)
+
+                iou_obj_target = iou.detach().clamp(0).type(tobj.dtype)
+                obji[b, gj, gi] = iou_obj_target
+                assignment.append([(1.0 - iou) * self.hyp['box'],
+                                   criteria(pcls, F.one_hot(tcls[i], self.nc).float()).mean(2) * self.hyp['cls'])
+
+                lobj += obji.mean() * (self.hyp['obj'] * self.balance[i])  # obj loss
+
+                losses = [torch.cat(x, 1) for x in zip(*assignment)]
+                total_loss = losses[0] + losses[1]
+                i = torch.arange(n).view(-1, 1).repeat(1, 3)
+                j = torch.argsort(total_loss, dim=1)[:, :3]
+
+                lbox = losses[0][i, j].mean()
+                lcls = losses[1][i, j].mean()
+                bs = tobj.shape[0]  # batch size
+        return (lbox + lobj[0] + lcls) * bs, torch.stack([lbox, lobj[0], lcls]).detach()
+
+    def build_targets(self, p, targets):
+        # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
+        nt = targets.shape[0]  # number of anchors, targets
+        tcls, tbox, indices = [], [], []
+        gain = torch.ones(6, device=self.device)  # normalized to gridspace gain
+
+        g = 1.0  # bias
+        off = torch.tensor(
+            [
+                [0, 0],
+                [1, 0],
+                [0, 1],
+                [-1, 0],
+                [0, -1],  # j,k,l,m
+                # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
+            ],
+            device=self.device).float() * g  # offsets
+
+        for i in range(self.nl):
+            shape = p[i].shape
+            gain[2:6] = torch.tensor(shape)[[3, 2, 3, 2]]  # xyxy gain
+
+            # Match targets to anchors
+            t = targets * gain  # shape(3,n,7)
+            if nt:
+                # # Matches
+                # r = t[..., 4:6] / self.anchors[i]  # wh ratio
+                # j = torch.max(r, 1 / r).max(1)[0] < self.hyp['anchor_t']  # compare
+                # # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
+                # t = t[j]  # filter
+
+                # # Offsets
+                gxy = t[:, 2:4]  # grid xy
+                # gxi = gain[[2, 3]] - gxy  # inverse
+                # j, k = ((gxy % 1 < g) & (gxy > 1)).T
+                # l, m = ((gxi % 1 < g) & (gxi > 1)).T
+                # j = torch.stack((torch.ones_like(j), j, k, l, m))
+                t = t.repeat((5, 1, 1))
+                offsets = (torch.zeros_like(gxy)[None] + off[:, None])
+            else:
+                t = targets[0]
+                offsets = 0
+
+            # Define
+            bc, gxy, gwh = t.chunk(3, 2)  # (image, class), grid xy, grid wh
+            b, c = bc.long().T  # image, class
+            gij = (gxy - offsets).long()
+            gi, gj = gij.T  # grid indices
+
+            # Append
+            indices.append((b, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1)))  # image, grid_y, grid_x indices
+            tbox.append(torch.cat((gxy - gij, gwh), 2).permute(1, 0, 2).contiguous())  # box
             tcls.append(c)  # class
 
         return tcls, tbox, indices
--- utils/metrics.py
+++ utils/metrics.py
@@ -227,13 +227,13 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, eps=1e-7
 
     # Get the coordinates of bounding boxes
     if xywh:  # transform from xywh to xyxy
-        (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, 1), box2.chunk(4, 1)
+        (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, -1), box2.chunk(4, -1)
         w1_, h1_, w2_, h2_ = w1 / 2, h1 / 2, w2 / 2, h2 / 2
         b1_x1, b1_x2, b1_y1, b1_y2 = x1 - w1_, x1 + w1_, y1 - h1_, y1 + h1_
         b2_x1, b2_x2, b2_y1, b2_y2 = x2 - w2_, x2 + w2_, y2 - h2_, y2 + h2_
     else:  # x1, y1, x2, y2 = box1
-        b1_x1, b1_y1, b1_x2, b1_y2 = box1.chunk(4, 1)
-        b2_x1, b2_y1, b2_x2, b2_y2 = box2.chunk(4, 1)
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1.chunk(4, -1)
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2.chunk(4, -1)
         w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1
         w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1
 
