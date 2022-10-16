95ebc5fe75ad39d64008012b0fbd6c4ae6c39383
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-16 17:28:35 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -112,7 +112,7 @@ class Bottleneck(nn.Module):
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
-        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+        return x + self.cv2(x + self.cv1(x)) if self.add else self.cv2(self.cv1(x))
 
 
 class BottleneckCSP(nn.Module):
