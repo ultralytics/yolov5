17a268912deda131d5e1c392bf4c62a3e8f442c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-17 13:46:40 +0200

    fix CI

--- utils/general.py
+++ utils/general.py
@@ -57,8 +57,6 @@ cv2.setNumThreads(0)  # prevent OpenCV from multithreading (incompatible with Py
 os.environ['NUMEXPR_MAX_THREADS'] = str(NUM_THREADS)  # NumExpr max threads
 os.environ['OMP_NUM_THREADS'] = '1' if platform.system() == 'darwin' else str(NUM_THREADS)  # OpenMP (PyTorch and SciPy)
 
-ipython_type = str(type(IPython.get_ipython()))
-print(f'IPYTHON_TYPE: {ipython_type}')
 
 def is_ascii(s=''):
     # Is string composed of all ASCII (no UTF) characters? (note str().isascii() introduced in python 3.7)
