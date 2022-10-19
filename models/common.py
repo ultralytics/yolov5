c6e9ea5dd7f6d51cb8bf6792e0dc11e753feb1e4
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-19 15:53:24 +0200

    Cleanup

--- models/common.py
+++ models/common.py
@@ -155,7 +155,7 @@ class C3(nn.Module):
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=.2) for _ in range(n)))
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
 
     def forward(self, x):
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
