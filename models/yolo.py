ca15070be46b4f2c0aafd734e61d186ac5d55061
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-29 23:53:23 +0200

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -279,10 +279,10 @@ class DetectionModel(BaseModel):
 
         # Build strides, anchors
         m = self.model[-1]  # Detect()
-        if isinstance(m, (V6Detect, Detect, Segment)):
+        if isinstance(m, (Detect, Segment, V6Detect)):
             s = 256  # 2x min stride
             m.inplace = self.inplace
-            forward = lambda x: self.forward(x)[0] if isinstance(m, V6Detect, Segment) else self.forward(x)
+            forward = lambda x: self.forward(x)[0] if isinstance(m, (Segment, V6Detect)) else self.forward(x)
             m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, ch, s, s))])  # forward
             check_anchor_order(m)
             m.anchors /= m.stride.view(-1, 1, 1)
