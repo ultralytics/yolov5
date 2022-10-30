373839aea9fd90784ce61a3adf3e3de3235861c2
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-30 23:57:52 +0100

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -65,18 +65,16 @@ class V6Detect(nn.Module):
 
     def initialize_biases(self):
         for seq in self.cv3:
-            seq[-1].bias.data[:] = -math.log((1 - 1e-2) / 1e-2)
-            # conv = seq[-1]
-            # b = conv.bias.view(-1, )
-            # b.data.fill_(-math.log((1 - 1e-2) / 1e-2))
-            # conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
-
+            conv = seq[-1]
+            b = conv.bias.view(-1, )
+            b.data.fill_(-math.log((1 - 1e-2) / 1e-2))
+            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
         for seq in self.cv2:
-            seq[-1].bias.data[:] = 1.0
-            # conv = seq[-1]
-            # b = conv.bias.view(-1, )
-            # b.data.fill_(1.0)
-            # conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+            conv = seq[-1]
+            b = conv.bias.view(-1, )
+            b.data.fill_(1.0)
+            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
         self.proj = nn.Parameter(torch.linspace(0, self.reg_max, self.reg_max + 1), requires_grad=False)
         self.proj_conv.weight = nn.Parameter(self.proj.view([1, self.reg_max + 1, 1, 1]).clone().detach(),
                                              requires_grad=False)
