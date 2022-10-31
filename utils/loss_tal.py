7b66b3a33459333604c4dc7f9a1d91fef1659916
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-31 01:20:30 +0100

    Update

--- utils/loss_tal.py
+++ utils/loss_tal.py
@@ -249,14 +249,10 @@ class BboxLoss(nn.Module):
         target_right = target_left + 1
         weight_left = target_right.to(torch.float) - target
         weight_right = 1 - weight_left
-        loss_left = (
-                F.cross_entropy(pred_dist.view(-1, self.reg_max + 1), target_left.view(-1), reduction="none").view(
-                    target_left.shape) * weight_left
-        )
-        loss_right = (
-                F.cross_entropy(pred_dist.view(-1, self.reg_max + 1), target_right.view(-1), reduction="none").view(
-                    target_left.shape) * weight_right
-        )
+        loss_left = F.cross_entropy(pred_dist.view(-1, self.reg_max + 1), target_left.view(-1), reduction="none").view(
+            target_left.shape) * weight_left
+        loss_right = F.cross_entropy(pred_dist.view(-1, self.reg_max + 1), target_right.view(-1),
+                                     reduction="none").view(target_left.shape) * weight_right
         return (loss_left + loss_right).mean(-1, keepdim=True)
 
 
@@ -394,4 +390,3 @@ class ComputeLoss:
         bs = tobj.shape[0]  # batch size
 
         return (lbox + lobj + lcls + ldfl) * bs, torch.as_tensor([lbox, ldfl, lcls], device=lbox.device).detach()
-
