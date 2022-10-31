bd164809463d1afb55aef944ed3e4e5981f8bcb9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-31 22:31:06 +0100

    Update

--- utils/loss_tal.py
+++ utils/loss_tal.py
@@ -166,10 +166,9 @@ class ComputeLoss:
     def bbox_decode(self, anchor_points, pred_dist):
         if self.use_dfl:
             b, a, _ = pred_dist.shape
-            # pred_dist = pred_dist.view(b, a, 4, self.reg_max + 1).softmax(3).matmul(self.proj.type(pred_dist.dtype))
+            pred_dist = pred_dist.view(b, a, 4, self.reg_max + 1).softmax(3).matmul(self.proj.type(pred_dist.dtype))
             # pred_dist = (pred_dist.view(b, a, self.reg_max + 1, 4).softmax(2).mul(self.proj.type(pred_dist.dtype).view(1,1,17,1))).sum(2)
-            pred_dist = pred_dist.view(b, a, self.reg_max + 1, 4).permute(0, 1, 3, 2).contiguous().softmax(3).matmul(
-                self.proj.type(pred_dist.dtype))
+            # pred_dist = pred_dist.view(b, a, self.reg_max + 1, 4).permute(0, 1, 3, 2).contiguous().softmax(3).matmul(self.proj.type(pred_dist.dtype))
 
         return dist2bbox(pred_dist, anchor_points, box_format="xyxy")
 
