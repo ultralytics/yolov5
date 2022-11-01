37c1bfb198b3883fd5e6382d23a59c60aafe2762
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-01 14:18:41 +0100

    fix

--- utils/segment/dataloaders.py
+++ utils/segment/dataloaders.py
@@ -81,24 +81,25 @@ def create_dataloader(path,
 class LoadImagesAndLabelsAndMasks(LoadImagesAndLabels):  # for training/testing
 
     def __init__(
-        self,
-        path,
-        img_size=640,
-        batch_size=16,
-        augment=False,
-        hyp=None,
-        rect=False,
-        image_weights=False,
-        cache_images=False,
-        single_cls=False,
-        stride=32,
-        pad=0,
-        prefix="",
-        downsample_ratio=1,
-        overlap=False,
+            self,
+            path,
+            img_size=640,
+            batch_size=16,
+            augment=False,
+            hyp=None,
+            rect=False,
+            image_weights=False,
+            cache_images=False,
+            single_cls=False,
+            stride=32,
+            pad=0,
+            min_items=0,
+            prefix="",
+            downsample_ratio=1,
+            overlap=False,
     ):
         super().__init__(path, img_size, batch_size, augment, hyp, rect, image_weights, cache_images, single_cls,
-                         stride, pad, prefix)
+                         stride, pad, min_items, prefix)
         self.downsample_ratio = downsample_ratio
         self.overlap = overlap
 
