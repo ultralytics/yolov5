30c618bcc513eee64d0192bcca6e0b2cf6bb5061
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-03 16:36:21 +0100

    Optimize 1

--- utils/loss_tal.py
+++ utils/loss_tal.py
@@ -273,18 +273,30 @@ class ComputeLoss:
         self.use_dfl = use_dfl
         self.proj = nn.Parameter(torch.linspace(0, self.reg_max, self.reg_max + 1), requires_grad=False)
 
+    # def preprocess(self, targets, batch_size, scale_tensor):
+    #     targets_list = np.zeros((batch_size, 1, 5)).tolist()
+    #     for i, item in enumerate(targets.cpu().numpy().tolist()):
+    #         targets_list[int(item[0])].append(item[1:])
+    #     max_len = max((len(l) for l in targets_list))
+    #     targets = torch.from_numpy(
+    #         np.array(list(map(lambda l: l + [[-1, 0, 0, 0, 0]] * (max_len - len(l)), targets_list)))[:, 1:, :]).to(
+    #         targets.device
+    #     )
+    #     batch_target = targets[:, :, 1:5].mul_(scale_tensor)
+    #     targets[..., 1:] = xywh2xyxy(batch_target)
+    #     return targets
+
     def preprocess(self, targets, batch_size, scale_tensor):
-        targets_list = np.zeros((batch_size, 1, 5)).tolist()
-        for i, item in enumerate(targets.cpu().numpy().tolist()):
-            targets_list[int(item[0])].append(item[1:])
-        max_len = max((len(l) for l in targets_list))
-        targets = torch.from_numpy(
-            np.array(list(map(lambda l: l + [[-1, 0, 0, 0, 0]] * (max_len - len(l)), targets_list)))[:, 1:, :]).to(
-            targets.device
-        )
-        batch_target = targets[:, :, 1:5].mul_(scale_tensor)
-        targets[..., 1:] = xywh2xyxy(batch_target)
-        return targets
+        i = targets[:, 0]  # image index
+        _, counts = i.unique(return_counts=True)
+        out = torch.zeros(batch_size, counts.max(), 5, device=self.device)
+        for j in range(batch_size):
+            matches = i == j
+            n = matches.sum()
+            if n:
+                out[j, :n] = targets[matches, 1:]
+        out[..., 1:5] = xywh2xyxy(out[:, :, 1:5].mul_(scale_tensor))
+        return out
 
     def bbox_decode(self, anchor_points, pred_dist):
         if self.use_dfl:
