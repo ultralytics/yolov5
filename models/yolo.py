4e19aa8b8b3817a4ee364a77bf1e5706acac4510
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-06 14:09:49 +0100

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -55,7 +55,7 @@ class V6Detect(nn.Module):
         self.shape = (0, 0)  # initial grid shape
 
         c2, c3 = 32, max(ch[0], self.no - 4)  # channels
-        # c2, c3 = max(ch[0] / 4, 16), max(ch[0], self.no - 4)  # channels
+        # c2, c3 = max(ch[0] // 4, 16), max(ch[0], self.no - 4)  # channels
         self.cv2 = nn.ModuleList(
             nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
         self.cv3 = nn.ModuleList(
