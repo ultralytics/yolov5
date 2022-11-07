c9c753852d5af858da5bad9c7d90a1a63ff7fc38
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-07 11:52:18 +0100

    assigner.py updates

--- utils/loss_tal.py
+++ utils/loss_tal.py
@@ -248,7 +248,7 @@ class ComputeLoss:
 
         # lbox *= self.hyp["box"] * 3
         lbox *= 2.5 * 3
-        lobj *= 0.7 * 3 * 20
+        lobj *= 0.7 * 3 # * 20
         lcls *= 1.0 * 3 / 6
         ldfl *= 0.5 * 3
         bs = tobj.shape[0]  # batch size
