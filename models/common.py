ff4d27a906a641c2f891b152363591af3d5ef2ac
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-10 01:02:53 +0100

    Add `ultralytics` pip package

--- models/common.py
+++ models/common.py
@@ -836,14 +836,15 @@ class Proto(nn.Module):
     def __init__(self, c1, c_=256, c2=32):  # ch_in, number of protos, number of masks
         super().__init__()
         self.cv1 = Conv(c1, c_, k=3)
-        # self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.upsample_nearest = nn.Upsample(scale_factor=2, mode='nearest')
+        self.upsample_bilinear = nn.Upsample(scale_factor=2, mode='bilinear')
         # self.upsample = nn.ConvTranspose2d(c_, c_, 2, 2, 0)
         self.cv2 = Conv(c_, c_, k=3)
         self.cv3 = Conv(c_, c2)
 
     def forward(self, x):
-        return self.cv3(self.cv2(self.upsample(self.cv1(x))))
+        y = self.cv1(x)
+        return self.cv3(self.cv2(self.upsample_nearest(y) if self.training else self.upsample_bilinear(y)))
 
 
 class Classify(nn.Module):
