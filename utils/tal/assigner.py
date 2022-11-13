eac21c7b93cfcebe413ef4cc5e6667067ed80eed
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-13 21:34:11 +0100

    Update

--- utils/tal/assigner.py
+++ utils/tal/assigner.py
@@ -5,26 +5,6 @@ import torch.nn.functional as F
 from utils.metrics import bbox_iou
 
 
-def iou_calculator(box1, box2, eps=1e-9):
-    """Calculate iou for batch
-    Args:
-        box1 (Tensor): shape(bs, n_max_boxes, 4), xyxy
-        box2 (Tensor): shape(bs, num_total_anchors, 4), xyxy
-    Return:
-        (Tensor): shape(bs, n_max_boxes, num_total_anchors)
-    """
-    px1y1, px2y2 = box1.unsqueeze(2).chunk(2, 3)  # bs, n_max_boxes, 1, 4
-    gx1y1, gx2y2 = box2.unsqueeze(1).chunk(2, 3)  # bs, 1, n_max_boxes, 4
-    x1y1 = torch.maximum(px1y1, gx1y1)
-    x2y2 = torch.minimum(px2y2, gx2y2)
-    overlap = (x2y2 - x1y1).clip(0).prod(3)
-    area1 = (px2y2 - px1y1).clip(0).prod(3)
-    area2 = (gx2y2 - gx1y1).clip(0).prod(3)
-    union = area1 + area2 - overlap + eps
-
-    return overlap / union
-
-
 def select_candidates_in_gts(xy_centers, gt_bboxes, eps=1e-9):
     """select the positive anchor center in gt
 
@@ -147,14 +127,7 @@ class TaskAlignedAssigner(nn.Module):
         bbox_scores = pd_scores[ind[0], :, ind[1]]  # b, max_num_obj, h*w
 
         overlaps = bbox_iou(gt_bboxes.unsqueeze(2), pd_bboxes.unsqueeze(1), xywh=False, CIoU=False, eps=1e-9).squeeze(3)
-        overlaps_orig = iou_calculator(gt_bboxes, pd_bboxes)
-
-        diff = overlaps - overlaps_orig
-        if diff.max() != 0.0 or diff.min() != 0.0:
-            print(overlaps.shape, overlaps.dtype, overlaps_orig.shape, overlaps_orig.dtype, diff.max(), diff.min())
-
         align_metric = bbox_scores.pow(self.alpha) * overlaps.pow(self.beta)
-
         return align_metric, overlaps
 
     def select_topk_candidates(self, metrics, largest=True, topk_mask=None):
