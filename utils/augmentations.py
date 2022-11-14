1234baa0d4dcfca808e450e2c5c4617f0c9d8993
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-14 14:51:48 +0100

    Copy-Paste zero value fix
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -250,12 +250,10 @@ def copy_paste(im, labels, segments, p=0.5):
             if (ioa < 0.30).all():  # allow 30% obscuration of existing labels
                 labels = np.concatenate((labels, [[l[0], *box]]), 0)
                 segments.append(np.concatenate((w - s[:, 0:1], s[:, 1:2]), 1))
-                cv2.drawContours(im_new, [segments[j].astype(np.int32)], -1, (255, 255, 255), cv2.FILLED)
+                cv2.drawContours(im_new, [segments[j].astype(np.int32)], -1, (1, 1, 1), cv2.FILLED)
 
-        result = cv2.bitwise_and(src1=im, src2=im_new)
-        result = cv2.flip(result, 1)  # augment segments (flip left-right)
-        i = result > 0  # pixels to replace
-        # i[:, :] = result.max(2).reshape(h, w, 1)  # act over ch
+        result = np.flip(im, 1)  # augment segments (flip left-right)
+        i = np.flip(im_new.astype(bool), 1)
         im[i] = result[i]  # cv2.imwrite('debug.jpg', im)  # debug
 
     return im, labels, segments
