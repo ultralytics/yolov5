ce0c4faa8cfc74cff51d4f0fa815b6d6f7fcdb26
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-14 13:47:55 +0100

    regmax8

--- models/yolo.py
+++ models/yolo.py
@@ -44,7 +44,7 @@ class V6Detect(nn.Module):
         super().__init__()
         self.nc = nc  # number of classes
         self.nl = len(ch)  # number of detection layers
-        self.reg_max = 16 + 1
+        self.reg_max = 8
         self.no = nc + self.reg_max * 4  # number of outputs per anchor
         self.inplace = inplace  # use inplace ops (e.g. slice assignment)
 
--- utils/loss_tal.py
+++ utils/loss_tal.py
@@ -16,17 +16,6 @@ from utils.tal.assigner import TaskAlignedAssigner
 from utils.torch_utils import de_parallel
 
 
-# def xywh2xyxy(x):
-#     # from utils.general import xywh2xyxy
-#     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-#     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-#     y[..., 0] = x[..., 0] - x[..., 2] / 2  # top left x
-#     y[..., 1] = x[..., 1] - x[..., 3] / 2  # top left y
-#     y[..., 2] = x[..., 0] + x[..., 2] / 2  # bottom right x
-#     y[..., 3] = x[..., 1] + x[..., 3] / 2  # bottom right y
-#     return y
-
-
 def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
     # return positive, negative label smoothing BCE targets
     return 1.0 - 0.5 * eps, 0.5 * eps
