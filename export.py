f533dda3f5f6a80ee2f2f6a2c170d87b8a277358
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-15 12:14:06 +0100

    mosaic9

--- export.py
+++ export.py
@@ -68,7 +68,7 @@ if platform.system() != 'Windows':
     ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.experimental import attempt_load
-from models.yolo import ClassificationModel, Detect, DetectionModel, SegmentationModel
+from models.yolo import ClassificationModel, Detect, V6Detect, DetectionModel, SegmentationModel
 from utils.dataloaders import LoadImages
 from utils.general import (LOGGER, Profile, check_dataset, check_img_size, check_requirements, check_version,
                            check_yaml, colorstr, file_size, get_default_args, print_args, url2file, yaml_save)
@@ -540,7 +540,7 @@ def run(
     # Update model
     model.eval()
     for k, m in model.named_modules():
-        if isinstance(m, Detect):
+        if isinstance(m, (Detect, V6Detect)):
             m.inplace = inplace
             m.dynamic = dynamic
             m.export = True
