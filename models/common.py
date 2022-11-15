14e819e931693b6bdf5dd75ecea52488af2a07e0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-15 12:07:19 +0100

    mosaic9

--- models/common.py
+++ models/common.py
@@ -97,8 +97,8 @@ class DFL(nn.Module):
 
     def forward(self, x):
         b, c, a = x.shape  # batch, channels, anchors
-        # return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-        return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
+        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
 
 
 class TransformerLayer(nn.Module):
