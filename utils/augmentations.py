656bfc0480043c2976f9dbe4623e52ae1ee3133e
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-16 16:38:32 +0100

    remove old copy_paste

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -237,28 +237,6 @@ def random_perspective(im,
     return im, targets
 
 
-# def copy_paste_original(im, labels, segments, p=0.5):
-#     # Implement Copy-Paste augmentation https://arxiv.org/abs/2012.07177, labels as nx5 np.array(cls, xyxy)
-#     n = len(segments)
-#     if p and n:
-#         h, w, c = im.shape  # height, width, channels
-#         im_new = np.zeros(im.shape, np.uint8)
-#         for j in random.sample(range(n), k=round(p * n)):
-#             l, s = labels[j], segments[j]
-#             box = w - l[3], l[2], w - l[1], l[4]
-#             ioa = bbox_ioa(box, labels[:, 1:5])  # intersection over area
-#             if (ioa < 0.30).all():  # allow 30% obscuration of existing labels
-#                 labels = np.concatenate((labels, [[l[0], *box]]), 0)
-#                 segments.append(np.concatenate((w - s[:, 0:1], s[:, 1:2]), 1))
-#                 cv2.drawContours(im_new, [segments[j].astype(np.int32)], -1, (1, 1, 1), cv2.FILLED)
-#
-#         result = cv2.flip(im, 1)  # augment segments (flip left-right)
-#         i = cv2.flip(im_new, 1).astype(bool)
-#         im[i] = result[i]  # cv2.imwrite('debug.jpg', im)  # debug
-#
-#     return im, labels, segments
-
-
 def copy_paste(im, labels, segments, p=0.5):
     # Implement Copy-Paste augmentation https://arxiv.org/abs/2012.07177, labels as nx5 np.array(cls, xyxy)
     n = len(segments)
