a75b8b623c11fc325512a498a8ef89b0ece43a13
Ryan Echols <ryan@shadylakemedia.com>
2022-11-17 11:10:04 -0700

    chore: fill in comment
    no-op so a pre-commit hook can auto-format files
    
    Signed-off-by: Ryan Echols <ryan@shadylakemedia.com>

--- utils/general.py
+++ utils/general.py
@@ -116,7 +116,7 @@ LOGGING_NAME = "yolov5"
 
 
 def set_logging(name=LOGGING_NAME, verbose=True):
-    # sets up logging for
+    # sets up logging for the given name
     rank = int(os.getenv('RANK', -1))  # rank in world for Multi-GPU trainings
     level = logging.INFO if verbose and rank in {-1, 0} else logging.ERROR
     logging.config.dictConfig({
