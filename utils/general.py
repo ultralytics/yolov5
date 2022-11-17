1510111b46a24a0c0fa2d685a6f3c96693368654
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-17 19:22:18 +0100

    data.yaml `names.keys()` integer assert (#10190)
    * data.yaml `names.keys()` integer assert
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update general.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -482,9 +482,10 @@ def check_dataset(data, autodownload=True):
 
     # Checks
     for k in 'train', 'val', 'names':
-        assert k in data, f"data.yaml '{k}:' field missing ❌"
+        assert k in data, emojis(f"data.yaml '{k}:' field missing ❌")
     if isinstance(data['names'], (list, tuple)):  # old array format
         data['names'] = dict(enumerate(data['names']))  # convert to dict
+    assert all(isinstance(k, int) for k in data['names'].keys()), 'data.yaml names keys must be integers, i.e. 2: car'
     data['nc'] = len(data['names'])
 
     # Resolve paths
