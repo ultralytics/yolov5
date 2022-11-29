7f14c78c9acc4364793bce037a6ef9b688faaa9f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-29 10:28:58 -0800

    Update

--- models/common.py
+++ models/common.py
@@ -263,17 +263,16 @@ class C2f_orig(nn.Module):
 
 class C2f(nn.Module):
     # CSP Bottleneck with 2 convolutions
-    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.25):  # ch_in, ch_out, number, shortcut, groups, expansion
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv1 = Conv(c1, self.c, 1, 1)
         self.cv2 = Conv((1 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=4.0) for _ in range(n))
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
-        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y = [self.cv1(x)]
         y.extend(m(y[-1]) for m in self.m)
-        y.pop(1)
         return self.cv2(torch.cat(y, 1))
 
 
