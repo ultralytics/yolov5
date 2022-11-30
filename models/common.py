c4075453e5fe0382aae6d23162e98534a50edf91
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-11-29 17:36:00 -0800

    Update

--- models/common.py
+++ models/common.py
@@ -267,7 +267,7 @@ class C2f(nn.Module):
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.cv2 = Conv((1 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
