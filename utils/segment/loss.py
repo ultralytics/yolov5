44784dd54027354b76dd24c807a977a6d101be83
erliding <42523282+erliding@users.noreply.github.com>
2022-12-14 20:33:03 -0800

    bugfix: single_mask_loss
    Signed-off-by: erliding <42523282+erliding@users.noreply.github.com>

--- utils/segment/loss.py
+++ utils/segment/loss.py
@@ -113,7 +113,7 @@ class ComputeLoss:
         # Mask loss for one image
         pred_mask = (pred @ proto.view(self.nm, -1)).view(-1, *proto.shape[1:])  # (n,32) @ (32,80,80) -> (n,80,80)
         loss = F.binary_cross_entropy_with_logits(pred_mask, gt_mask, reduction="none")
-        return (crop_mask(loss, xyxy).mean(dim=(1, 2)) / area).mean()
+        return (crop_mask(loss, xyxy).sum(dim=(1, 2)) / area).mean()
 
     def build_targets(self, p, targets):
         # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
