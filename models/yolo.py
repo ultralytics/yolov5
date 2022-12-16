e5b618df3015ef8064c6052c5813d824b7e65001
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-12-17 00:24:26 +0100

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -47,12 +47,12 @@ class V6Detect(nn.Module):
         super().__init__()
         self.nc = nc  # number of classes
         self.nl = len(ch)  # number of detection layers
-        self.reg_max = 16
+        self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
         self.no = nc + self.reg_max * 4  # number of outputs per anchor
         self.inplace = inplace  # use inplace ops (e.g. slice assignment)
         self.stride = torch.zeros(self.nl)  # strides computed during build
 
-        c2, c3 = max(ch[0] // 4, 16), max(ch[0], self.no - 4)  # channels
+        c2, c3 = max(ch[0] // 4, self.reg_max * 4), max(ch[0], self.nc)  # channels
         self.cv2 = nn.ModuleList(
             nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
         self.cv3 = nn.ModuleList(
