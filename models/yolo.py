540fb1f0c0341ddd24cdf488fab9a83dcf1acadd
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-12-20 14:35:56 +0100

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -81,8 +81,8 @@ class V6Detect(nn.Module):
         # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
         # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
         for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
-            a[-1].bias.data[:] = 1.0  # box
-            b[-1].bias.data[:m.nc] = math.log(.01 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+            a[-1].bn.bias.data[:] = 1.0  # box
+            b[-1].bn.bias.data[:m.nc] = math.log(.01 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
 
 
 class Detect(nn.Module):
