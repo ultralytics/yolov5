6fde670b35d7769ad0081063e9bfa03b0843752d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-12-22 14:45:48 +0100

    Use math.ceil() for resize to enforce min floor of 1 pixel
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -737,11 +737,7 @@ class LoadImagesAndLabels(Dataset):
             r = self.img_size / max(h0, w0)  # ratio
             if r != 1:  # if sizes are not equal
                 interp = cv2.INTER_LINEAR if (self.augment or r > 1) else cv2.INTER_AREA
-
-                min_len = 32
-                w0_temp = min_len if int(w0 * r) < min_len else int(w0 * r)
-                h0_temp = min_len if int(h0 * r) < min_len else int(h0 * r)
-                im = cv2.resize(im, (w0_temp, h0_temp), interpolation=interp)
+                im = cv2.resize(im, (math.ceil(w0 * r), math.ceil(h0 * r)), interpolation=interp)
             return im, (h0, w0), im.shape[:2]  # im, hw_original, hw_resized
         return self.ims[i], self.im_hw0[i], self.im_hw[i]  # im, hw_original, hw_resized
 
