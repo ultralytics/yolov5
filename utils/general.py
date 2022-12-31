2eba818f2c03314d41f47dd4e51932d221259a82
Er <ercaronte@gmail.com>
2022-12-31 13:01:38 +0100

    Update clip_boxes function in general.py
    With torch >= 1.7, torch supports the clip API with the same signature as numpy. It is an alias for the clamp, see: https://pytorch.org/docs/1.7.0/generated/torch.clip.html?highlight=clip#torch.clip
    
    I am proposing this change since with mac os metal implementation the clamp_ functions does not work on the MPS device, while the standard clip does.
    
    Signed-off-by: Er <ercaronte@gmail.com>

--- utils/general.py
+++ utils/general.py
@@ -861,14 +861,8 @@ def scale_segments(img1_shape, segments, img0_shape, ratio_pad=None, normalize=F
 
 def clip_boxes(boxes, shape):
     # Clip boxes (xyxy) to image shape (height, width)
-    if isinstance(boxes, torch.Tensor):  # faster individually
-        boxes[..., 0].clamp_(0, shape[1])  # x1
-        boxes[..., 1].clamp_(0, shape[0])  # y1
-        boxes[..., 2].clamp_(0, shape[1])  # x2
-        boxes[..., 3].clamp_(0, shape[0])  # y2
-    else:  # np.array (faster grouped)
-        boxes[..., [0, 2]] = boxes[..., [0, 2]].clip(0, shape[1])  # x1, x2
-        boxes[..., [1, 3]] = boxes[..., [1, 3]].clip(0, shape[0])  # y1, y2
+    boxes[..., [0, 2]] = boxes[..., [0, 2]].clip(0, shape[1])  # x1, x2
+    boxes[..., [1, 3]] = boxes[..., [1, 3]].clip(0, shape[0])  # y1, y2
 
 
 def clip_segments(segments, shape):
