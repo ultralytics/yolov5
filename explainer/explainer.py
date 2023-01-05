e3368d4ae3c18f83ff44859587408a63288b5357
Amir Pourmand <pourmand1376@gmail.com>
2023-01-06 01:35:54 +0330

    add results

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -125,16 +125,17 @@ def run(
         method='GradCAM',  # the method for interpreting the results
         verbose=False,  # verbose output
 ):
-    model = torch.hub.load(
-        'ultralytics/yolov5',
-        'yolov5s',
-        #autoshape=False #because otherwise I have to resize the image, I just don't know for now
-    )
-    return attempt_load('yolov5s.pt')
+    # model = torch.hub.load(
+    #     'ultralytics/yolov5',
+    #     'yolov5s',
+    #     #autoshape=False #because otherwise I have to resize the image, I just don't know for now
+    # )
+    model = attempt_load('yolov5s.pt')
     # model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights)  # local model
     image_file = Image.open(source, 'r')
     raw_image = Image.Image.resize(image_file, (640, 384))
     results = model([raw_image])
+    return results
     # Results
     results.print()
     results.save()
