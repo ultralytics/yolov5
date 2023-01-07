dff642ba28c0a27dfee05b5a646ee20e7ed6de67
Amir Pourmand <pourmand1376@gmail.com>
2023-01-08 00:33:29 +0330

    add requires_grad

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -149,6 +149,9 @@ def run(
     #     #autoshape=False #because otherwise I have to resize the image, I just don't know for now
     # )
     model = attempt_load('yolov5s.pt')
+    model.requires_grad_(True)
+    model.eval() # not sure about this! 
+
     image_file = Image.open(source, 'r')
     raw_image = Image.Image.resize(image_file, (640, 384))
 
