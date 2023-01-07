07b6b9a99715443ee3b8735385bbe65b22d2de75
Amir Pourmand <pourmand1376@gmail.com>
2023-01-07 18:57:51 +0330

    remove target layer for eigenCAM

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -87,13 +87,13 @@ class YOLOBoxScoreTarget():
 
 def extract_eigenCAM(model, raw_image_fp):
     # eigenCAM doesn't acutally needs YOLOBoxScoreTarget. It doesn't call it.
-    target_layers = [model.model.model.model[-2]]
+    # eigenCAM doesn't also need a target layer. Anything will do fine.
+    target_layers = None
     cam = EigenCAM(model, target_layers, use_cuda=False)
     transform = transforms.ToTensor()
     tensor = transform(raw_image_fp).unsqueeze(0)
 
     grayscale_cam = cam(tensor)[0, :, :]
-    print(cam(tensor).shape)
     cam_image = show_cam_on_image(raw_image_fp, grayscale_cam, use_rgb=True)
     return cam_image
 
