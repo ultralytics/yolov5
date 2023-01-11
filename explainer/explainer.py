bd67c40435e995ea8ff25f40399e04510e2d0e3b
Amir Pourmand <pourmand1376@gmail.com>
2023-01-11 15:33:21 +0330

    .

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -86,7 +86,7 @@ def extract_eigenCAM(model, raw_image_fp, layer= -2):
     eigenCAM doesn't acutally needs YOLOBoxScoreTarget. It doesn't call it.
     to see eigenCAM layer changes, you have to restart COLAB completely
     """
-    target_layers = [model.model.model.model[layer]]
+    target_layers = [model.model.model[layer]]
     cam = EigenCAM(model, target_layers, use_cuda=False)
     transform = transforms.ToTensor()
     tensor = transform(raw_image_fp).unsqueeze(0)
