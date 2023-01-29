5626bbfc67e44ee0c5ba0065d06dc444d5431f11
Jiacong Fang <zldrobit@126.com>
2023-01-29 16:02:28 +0800

    Remove 'source' command line option

--- utils/gradio/demo.py
+++ utils/gradio/demo.py
@@ -1,12 +1,12 @@
 """
 Run Gradio demo.
-Add --host 0.0.0.0 to share with other machines without --share.
+Add --host 0.0.0.0 to share with other machines by IP address.
 
 Usage - local model:
-python utils/gradio/demo.py --source local --path MODEL_FILE
+python utils/gradio/demo.py --path MODEL_FILE
 
 Usage - github repo:
-python utils/gradio/demo.py --source github --model yolov5s
+python utils/gradio/demo.py --model yolov5s
 """
 
 import argparse
@@ -41,7 +41,6 @@ if __name__ == '__main__':
     parser.add_argument('--host', type=str, default='localhost', help='server ip/name (0.0.0.0 for network request)')
     parser.add_argument('--port', type=int, default=7860, help='server port')
     parser.add_argument('--example_dir', type=str, default=ROOT / 'data/images', help='example image dir')
-    parser.add_argument('--source', type=str, default='local', help='torch hub source: github/local')
     parser.add_argument('--model', type=str, default='custom', help='model name used by github source')
     parser.add_argument('--path', type=str, default=ROOT / 'yolov5l.pt', help='local model path')
     opt = parser.parse_args()
@@ -57,9 +56,10 @@ if __name__ == '__main__':
     for f in files:
         if f.suffix.lower()[1:] in IMG_FORMATS:
             examples.append([f])
-    kwargs = {'path': opt.path} if opt.source == 'local' else {}
-    repo = ROOT if opt.source == 'local' else 'ultralytics/yolov5'  # source == 'github'
-    model = torch.hub.load(repo, opt.model, source=opt.source, **kwargs)
+    source = 'local' if opt.model == 'custom' else 'github'
+    kwargs = {'path': opt.path} if source == 'local' else {}
+    repo = ROOT if source == 'local' else 'ultralytics/yolov5'
+    model = torch.hub.load(repo, opt.model, source=source, **kwargs)
     demo = gr.Interface(fn=predict,
                         inputs=[gr.Image(), gr.Slider(0, 1, 0.25),
                                 gr.Slider(0, 1, 0.45),
