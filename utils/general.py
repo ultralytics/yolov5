eb9651f908afb384ab790f8361befd8a2809edbb
Konstantin <konstantin@neuralmagic.com>
2023-02-10 11:55:41 +0000

    Minor data pathing fix

--- utils/general.py
+++ utils/general.py
@@ -47,7 +47,7 @@ RANK = int(os.getenv('RANK', -1))
 
 # Settings
 NUM_THREADS = min(8, max(1, os.cpu_count() - 1))  # number of YOLOv5 multiprocessing threads
-DATASETS_DIR = Path(os.getenv('YOLOv5_DATASETS_DIR', ROOT.parent / 'datasets'))  # global datasets directory
+DATASETS_DIR = Path(os.getenv('YOLOv5_DATASETS_DIR', SAVE_ROOT.parent / 'datasets'))  # global datasets directory
 AUTOINSTALL = str(os.getenv('YOLOv5_AUTOINSTALL', True)).lower() == 'true'  # global auto-install mode
 VERBOSE = str(os.getenv('YOLOv5_VERBOSE', True)).lower() == 'true'  # global verbose mode
 FONT = 'Arial.ttf'  # https://ultralytics.com/assets/Arial.ttf
