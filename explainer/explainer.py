1f73f2bf841f48cf4a3c38ceabab07ef83ff2484
Amir Pourmand <pourmand1376@gmail.com>
2023-02-14 15:15:05 +0330

    .

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -202,7 +202,7 @@ def run(
     model_classes =dict((v,k) for k,v in model.names.items())
     class_idx = [model_classes[item] for item in class_names]
 
-    for _, im, _ in dataset:
+    for _, im, _,_,_ in dataset:
         im = torch.from_numpy(im).to(model.device)
         im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
