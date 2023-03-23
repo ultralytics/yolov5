1344d143e2086df5efb9c4133a044be7875dfa64
Grzegorz Klimaszewski <2612193+grzegorzk@users.noreply.github.com>
2023-03-23 22:15:46 +0100

    Do not monkey-patch cv2 methods for unaware caller

--- utils/general.py
+++ utils/general.py
@@ -1135,6 +1135,7 @@ def imshow(path, im):
     imshow_(path.encode('unicode_escape').decode(), im)
 
 
-cv2.imread, cv2.imwrite, cv2.imshow = imread, imwrite, imshow  # redefine
+if Path(inspect.stack()[0].filename).parent.parent.as_posix() in inspect.stack()[-1].filename:
+    cv2.imread, cv2.imwrite, cv2.imshow = imread, imwrite, imshow  # redefine
 
 # Variables ------------------------------------------------------------------------------------------------------------
