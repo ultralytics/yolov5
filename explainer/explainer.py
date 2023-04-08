9dd6d6e02a226f7819fe901b7e801d08adbff589
Amir Pourmand <pourmand1376@gmail.com>
2023-04-08 11:38:54 +0330

    .

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -257,17 +257,16 @@ def run(
     class_idx = [model_classes[item] for item in class_names]
 
     for _, im, _,_,_ in dataset:
+        structured_output = autoshaped_model(im)
         im = torch.from_numpy(im).to(model.device)
         im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
         
-        structured_output = autoshaped_model(im)
         model = YoloOutputWrapper(model)
         _ = model(im) 
 
-        
 
         cam_image = explain(method=method,model= model, image=im, layer=layer, 
                     classes=class_idx, objectness_thres=objectness_thres,use_cuda=use_cuda)
