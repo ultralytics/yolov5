9ef4de5abcc9cbb84c8a4c230001a1b1bcef74cf
Amir Pourmand <pourmand1376@gmail.com>
2023-04-08 11:55:02 +0330

    .

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -263,11 +263,11 @@ def run(
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
-        
-        model = YoloOutputWrapper(model)
-        _ = model(im) 
 
         structured_output = autoshaped_model(im.copy())
+
+        model = YoloOutputWrapper(model)
+        _ = model(im) 
 
 
         cam_image = explain(method=method,model= model, image=im, layer=layer, 
