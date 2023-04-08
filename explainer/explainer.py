9b6d7fb2ffd947fcbaf480d6dc84b6854dd51c69
Amir Pourmand <pourmand1376@gmail.com>
2023-04-08 11:57:17 +0330

    .

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -244,7 +244,7 @@ def run(
     device = select_device(device)
     
     model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
-    autoshaped_model = AutoShape(model)
+    #autoshaped_model = AutoShape(model)
 
     stride, pt = model.stride, model.pt
     imgsz = check_img_size(imgsz, s=stride)  # check image size
