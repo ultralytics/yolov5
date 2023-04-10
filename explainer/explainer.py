89416fbd9cd3f263a50c9f3d58b496b32c79ecf9
Amir Pourmand <pourmand1376@gmail.com>
2023-04-10 17:13:09 +0330

    .

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -176,7 +176,7 @@ def extract_CAM(method, model: torch.nn.Module,predicted_bbox,image,layer:int, u
 
     bbox_torch = torch.tensor(predicted_bbox.drop('name',axis=1).values)
 
-    backprop_array = ['class','confidence']
+    backprop_array = ['x1','y1','x2','y2']
     final_cam = None
     for item in backprop_array:
         targets = [YOLOBoxScoreTarget2(predicted_bbox=bbox_torch,backprop=item)]
