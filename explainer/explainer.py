f99233a521130da0ce7218a1e40473f67117d5c3
Amir Pourmand <pourmand1376@gmail.com>
2023-05-01 17:20:55 +0330

    remove iou_thres

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -389,7 +389,6 @@ def parseopt():
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--method',
                         type=str,
