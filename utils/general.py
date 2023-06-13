3fea27069cfa275dd5d3ef0e7ab8a31537e394e3
MrAsimZahid <asimzahid02@gmail.com>
2023-06-13 13:45:31 +0500

    📦 NEW: Added MOT multi-object-tracking 2D detection annotation support

--- utils/general.py
+++ utils/general.py
@@ -729,6 +729,16 @@ def xyxy2xywh(x):
     return y
 
 
+def xyxy2xywh_mot(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[..., 0] = x[..., 0] - x[..., 2] / 2  # top left x
+    y[..., 1] = x[..., 1] - x[..., 3] / 2  # top left y
+    y[..., 2] = x[..., 2] - x[..., 0]  # width
+    y[..., 3] = x[..., 3] - x[..., 1]  # height
+    return y
+
+
 def xywh2xyxy(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
