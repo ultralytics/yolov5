361579787d553090eb27bf8b6dd57b186be6580f
Diana <epureanudiana13@gmail.com>
2023-06-22 13:44:51 +0200

    removed declaration

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -677,7 +677,6 @@ class LoadImagesAndLabels(Dataset):
     #     return self
 
     def __getitem__(self, index):
-        im0 = None
         index = self.indices[index]  # linear, shuffled, or image_weights
 
         hyp = self.hyp
