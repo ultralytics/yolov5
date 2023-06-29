cdf154e98fedb2f3d35704f2751fe4b3f148de7b
hlmhlr <halar.memon@hotmail.com>
2023-06-29 16:48:22 +0200

    output from run function

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -465,10 +465,9 @@ def run(
         cv2.imwrite(save_path.replace(path.suffix, '_concatenated_' + path.suffix), concatenated_image)
 
         LOGGER.info(f'saved image to {save_path}')
-        # last_image = cam_image
-        print('testing')
 
-    return renormalized_cam_image #(cam_image, , concatenated_image)
+
+    return renormalized_cam_image, cam_image, concatenated_image
 
 
 def parseopt():
