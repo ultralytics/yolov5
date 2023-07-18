d45d253a8946aacd7a2ff28b0e9d89c59b1ddc41
tomer-grin <tomer@get-grin.com>
2023-07-18 13:17:52 +0000

    latest changes to run in remote

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from ultralytics import YOLO
 import os
-from clearml import Dataset
+from clearml import Dataset, Task
+from utils.dataloaders import autosplit
 def main():
     # Create a new YOLO model from scratch
-    # model = YOLO('yolov5n.yaml')
+    model = YOLO('yolov5n.yaml')
 
     # Load a pretrained YOLO model (recommended for training)
-    model = YOLO('yolov5nu.pt')
-    ds = Dataset.get(dataset_id='0442ced245a348008d97adb338f020c6')
-    path_to_local = ds.get_local_copy()
+    # Task.init
+    # model = YOLO('yolov5nu.pt')
+    # ds = Dataset.get(dataset_id='0442ced245a348008d97adb338f020c6')
+    # path_to_local = ds.get_local_copy()
     # Train the model using the 'coco128.yaml' dataset for 3 epochs
-    results = model.train(data=os.path.join(path_to_local, 'fcc_new_none.yaml'), epochs=10,
-                          project='fcc-model', name='training_yolov5_full_ds1')
+    # copy data set to local
+    p = '../datasets/fcc_new_no_none'
+    if not os.path.exists(p):
+        os.makedirs(p)
+    ds = Dataset.get(dataset_id='e679ff62b4cd46afb041d31d65e88b90')
+    # path_to_local = ds.get_local_copy()
+    ds.get_mutable_local_copy(target_folder=p)
+    # p = autosplit(path='/home/tomernahshon/datasets/fcc_new_no_none/images/train', annotated_only=True)
+
+    # task.execute_remotely()
+    results = model.train(data='./data/fcc_new_none.yaml', epochs=1,
+                          project='fcc-model', name='training_yolov5_full_remote')
 
     # Evaluate the model's performance on the validation set
     results = model.val()
