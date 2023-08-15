98e5e58670e36aa3eb7d23789389c7b22ed0ac69
chrise96 <chrise96@hotmail.com>
2023-08-15 14:12:45 +0200

    Update val.py

--- val.py
+++ val.py
@@ -152,13 +152,15 @@ def process_batch(detections, labels, iouv):
 def parse_image_path(path):
     parts = path.split('/')  # Split the path using the forward slash as the separator
     image_filename = parts[-1]  # Last part is the filename
-    date_str = parts[-2]  # Second to last part is the date string
+    date_time_str = parts[-2]  # Second to last part is the date string
 
     try:
-        # Parse the date string as a datetime object
-        image_upload_date = datetime.strptime(date_str, "%Y-%m-%d")
+        # Parse the date and time string as a datetime object
+        image_upload_date = datetime.strptime(date_time_str, "%Y-%m-%d_%H:%M:%S")
+        # Extract the date portion of the datetime object
+        image_upload_date = image_upload_date.strftime("%Y-%m-%d")
     except ValueError as e:
-        LOGGER.info(f"Invalid folder structure, can not retrieve date: {date_str}")
+        LOGGER.info(f"Invalid folder structure, can not retrieve date: {image_upload_date}")
         raise e
 
     return image_filename, image_upload_date
