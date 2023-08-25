ebd1e71cbe02e849c639fe2b83b13423e6a24469
Haoyue Sheng <shenghaoyue9701@gmail.com>
2023-08-25 17:19:57 -0400

    remove import plotly at top

--- utils/metrics.py
+++ utils/metrics.py
@@ -10,7 +10,6 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 import torch
 from sklearn.metrics import roc_auc_score, roc_curve
 
