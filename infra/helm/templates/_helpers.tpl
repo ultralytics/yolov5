4091b624ad88707f0192948cbea3656ca9ca53b5
Pathik Ghugare <pathikghugare13@gmail.com>
2023-09-01 21:01:55 +0530

    azure integration

---
+++ infra/helm/templates/_helpers.tpl
@@ -0,0 +1,51 @@
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "pibit.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "pibit.fullname" -}}
+{{- if .Values.fullnameOverride }}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if contains $name .Release.Name }}
+{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+{{- end }}
+{{- end }}
+{{- end }}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "pibit.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Common labels
+*/}}
+{{- define "pibit.labels" -}}
+helm.sh/chart: {{ include "pibit.chart" . }}
+{{ include "pibit.selectorLabels" . }}
+{{- if .Chart.AppVersion }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+{{- end }}
+app.kubernetes.io/managed-by: {{ .Release.Service }}
+{{- end }}
+
+{{/*
+Selector labels
+*/}}
+{{- define "pibit.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "pibit.name" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+{{- end }}
---
+++ job.py
@@ -0,0 +1,62 @@
+# Handle to the workspace
+from azure.ai.ml import MLClient
+
+# Authentication package
+from azure.identity import DefaultAzureCredential
+
+# For Compute 
+from azure.ai.ml.entities import AmlCompute
+
+# For Job
+from azure.ai.ml import command
+from azure.ai.ml import Input
+
+import yaml
+
+# Load the config.yaml file
+with open('config.yaml') as file:
+    config = yaml.safe_load(file)
+
+# Assign the values to the respective variables
+subscription_id = config['subscription_id']
+resource_group_name = config['resource_group_name']
+workspace_name = config['workspace_name']
+gpu_compute_target = config['gpu_compute_target']
+custom_env_name = config['custom_env_name']
+azure_ds_path = config['azure_ds_path']
+experiment_name = config['project']
+display_name = config['name']
+
+credential = DefaultAzureCredential()
+
+# Get a handle to the workspace
+ml_client = MLClient(
+    credential=credential,
+    subscription_id=subscription_id,
+    resource_group_name=resource_group_name,
+    workspace_name=workspace_name,
+)
+
+job = command(
+    inputs = dict(
+        data=Input(
+            type = "uri_folder", 
+            path = azure_ds_path
+        ),
+    ),
+    compute=gpu_compute_target,
+    environment=custom_env_name,
+    code=".",  # location of source code
+    command="""
+    echo "The data asset path is ${{ inputs.data }}" && 
+    sed -i "s|path:.*$|path: ${{ inputs.data }}|" data/lossrun-ca.yaml &&
+    python train.py
+     """,
+    experiment_name=experiment_name,
+    display_name=display_name,
+)
+# for multi gpu, add the following in config.yaml
+# --device 0,1,2,3 \
+# --sync-bn \
+
+ml_client.create_or_update(job)
