ea4d780b62735bc27c3fc5f0ee630398c59fd7c7
Luis Filipe Araujo de Souza <58831491+Doquey@users.noreply.github.com>
2023-09-28 15:41:22 -0300

    Update export.py
    Signed-off-by: Luis Filipe Araujo de Souza <58831491+Doquey@users.noreply.github.com>

--- export.py
+++ export.py
@@ -155,8 +155,8 @@ def export_onnx(model, im, file, opset, dynamic, simplify, prefix=colorstr('ONNX
     import onnx
 
     LOGGER.info(f'\n{prefix} starting export with onnx {onnx.__version__}...')
-    f = file.with_suffix('.onnx')
-
+    f = str(file.with_suffix('.onnx'))
+    
     output_names = ['output0', 'output1'] if isinstance(model, SegmentationModel) else ['output0']
     if dynamic:
         dynamic = {'images': {0: 'batch', 2: 'height', 3: 'width'}}  # shape(1,3,640,640)
