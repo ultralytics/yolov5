8c440d9d8a5302e7400dccea043cc143c5645b75
hdiamond-foundationstack <110424039+hdiamond-foundationstack@users.noreply.github.com>
2023-10-19 15:05:07 -0700

    Update train.py
    Signed-off-by: hdiamond-foundationstack <110424039+hdiamond-foundationstack@users.noreply.github.com>

--- train.py
+++ train.py
@@ -522,7 +522,6 @@ def main(opt, callbacks=Callbacks()):
     device = select_device(opt.device, batch_size=opt.batch_size)
     if LOCAL_RANK != -1:
         msg = 'is not compatible with YOLOv5 Multi-GPU DDP training'
-        assert not opt.image_weights, f'--image-weights {msg}'
         assert not opt.evolve, f'--evolve {msg}'
         assert opt.batch_size != -1, f'AutoBatch with --batch-size -1 {msg}, please pass a valid --batch-size'
         assert opt.batch_size % WORLD_SIZE == 0, f'--batch-size {opt.batch_size} must be multiple of WORLD_SIZE'
