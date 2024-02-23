947f4b8f1d95e5e4713ae336342a4b1b4c72d951
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-23 23:30:42 +0100

    Add docstrings

--- utils/activations.py
+++ utils/activations.py
@@ -10,6 +10,9 @@ class SiLU(nn.Module):
     # SiLU activation https://arxiv.org/pdf/1606.08415.pdf
     @staticmethod
     def forward(x):
+        """Applies the Sigmoid-weighted Linear Unit (SiLU) activation function.
+
+        https://arxiv.org/pdf/1606.08415.pdf."""
         return x * torch.sigmoid(x)
 
 
@@ -17,14 +20,17 @@ class Hardswish(nn.Module):
     # Hard-SiLU activation
     @staticmethod
     def forward(x):
-        # return x * F.hardsigmoid(x)  # for TorchScript and CoreML
+        """Applies the Hardswish activation function, compatible with TorchScript, CoreML, and ONNX.
+
+        Equivalent to x * F.hardsigmoid(x)"""
         return x * F.hardtanh(x + 3, 0.0, 6.0) / 6.0  # for TorchScript, CoreML and ONNX
 
 
 class Mish(nn.Module):
-    # Mish activation https://github.com/digantamisra98/Mish
+    """Mish activation https://github.com/digantamisra98/Mish."""
     @staticmethod
     def forward(x):
+        """Applies the Mish activation function, a smooth alternative to ReLU."""
         return x * F.softplus(x).tanh()
 
 
@@ -33,17 +39,20 @@ class MemoryEfficientMish(nn.Module):
     class F(torch.autograd.Function):
         @staticmethod
         def forward(ctx, x):
+            """Applies the Mish activation function, a smooth ReLU alternative, to the input tensor `x`."""
             ctx.save_for_backward(x)
             return x.mul(torch.tanh(F.softplus(x)))  # x * tanh(ln(1 + exp(x)))
 
         @staticmethod
         def backward(ctx, grad_output):
+            """Computes the gradient of the Mish activation function with respect to input `x`."""
             x = ctx.saved_tensors[0]
             sx = torch.sigmoid(x)
             fx = F.softplus(x).tanh()
             return grad_output * (fx + x * sx * (1 - fx * fx))
 
     def forward(self, x):
+        """Applies the Mish activation function to the input tensor `x`."""
         return self.F.apply(x)
 
 
@@ -55,6 +64,9 @@ class FReLU(nn.Module):
         self.bn = nn.BatchNorm2d(c1)
 
     def forward(self, x):
+        """Applies FReLU activation with max operation between input and BN-convolved input.
+
+        https://arxiv.org/abs/2007.11824"""
         return torch.max(x, self.bn(self.conv(x)))
 
 
@@ -65,12 +77,14 @@ class AconC(nn.Module):
     """
 
     def __init__(self, c1):
+        """Initializes AconC with learnable parameters p1, p2, and beta for channel-wise activation control."""
         super().__init__()
         self.p1 = nn.Parameter(torch.randn(1, c1, 1, 1))
         self.p2 = nn.Parameter(torch.randn(1, c1, 1, 1))
         self.beta = nn.Parameter(torch.ones(1, c1, 1, 1))
 
     def forward(self, x):
+        """Applies AconC activation function with learnable parameters for channel-wise control on input tensor x."""
         dpx = (self.p1 - self.p2) * x
         return dpx * torch.sigmoid(self.beta * dpx) + self.p2 * x
 
@@ -92,6 +106,7 @@ class MetaAconC(nn.Module):
         # self.bn2 = nn.BatchNorm2d(c1)
 
     def forward(self, x):
+        """Applies a forward pass transforming input `x` using learnable parameters and sigmoid activation."""
         y = x.mean(dim=2, keepdims=True).mean(dim=3, keepdims=True)
         # batch-size 1 bug/instabilities https://github.com/ultralytics/yolov5/issues/2891
         # beta = torch.sigmoid(self.bn2(self.fc2(self.bn1(self.fc1(y)))))  # bug/unstable
