ca00c34c2ab8554ee994798e0ca78f5a48c23056
imyhxy <imyhxy@gmail.com>
2024-02-25 20:57:41 +0800

    Fix docs typos in callbacks.py (#12750)
    * Update callbacks.py
    
    Signed-off-by: imyhxy <imyhxy@gmail.com>
    
    * Update general.py
    
    Signed-off-by: imyhxy <imyhxy@gmail.com>
    
    ---------
    
    Signed-off-by: imyhxy <imyhxy@gmail.com>

--- utils/general.py
+++ utils/general.py
@@ -34,7 +34,7 @@ import torch
 import torchvision
 import yaml
 
-# Import 'ultralytics' package or install if if missing
+# Import 'ultralytics' package or install if missing
 try:
     import ultralytics
 
