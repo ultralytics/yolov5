5e3292fa0a1f0c963556c98efd1f6e49ddc369bd
David Morales <david.morales@connexta.com>
2024-03-06 14:43:03 -0800

    Add mlflow logging support

--- train.py
+++ train.py
@@ -30,6 +30,7 @@ try:
 except ImportError:
     comet_ml = None
 
+import mlflow
 import numpy as np
 import torch
 import torch.distributed as dist
@@ -128,6 +129,12 @@ def train(hyp, opt, device, callbacks):
     w = save_dir / "weights"  # weights dir
     (w.parent if evolve else w).mkdir(parents=True, exist_ok=True)  # make dir
     last, best = w / "last.pt", w / "best.pt"
+
+
+    for opt_key in opt.__dict__:
+        opt_value = opt.__dict__[opt_key]
+        if opt_value is not None and opt_value != '':
+            mlflow.log_param(opt_key, opt_value)
 
     # Hyperparameters
     if isinstance(hyp, str):
@@ -445,6 +452,15 @@ def train(hyp, opt, device, callbacks):
             log_vals = list(mloss) + list(results) + lr
             callbacks.run("on_fit_epoch_end", log_vals, epoch, best_fitness, fi)
 
+            mlflow.log_metric("P", results[0], step=epoch)
+            mlflow.log_metric("R", results[1], step=epoch)
+            mlflow.log_metric("mAP.5", results[2], step=epoch)
+            mlflow.log_metric("mAP.5-.95", results[3], step=epoch)
+            mloss_cpu = mloss.cpu().numpy()
+            mlflow.log_metric("box", mloss_cpu[0], step=epoch)
+            mlflow.log_metric("obj", mloss_cpu[1], step=epoch)
+            mlflow.log_metric("cls", mloss_cpu[2], step=epoch)
+
             # Save model
             if (not nosave) or (final_epoch and not evolve):  # if save
                 ckpt = {
@@ -467,6 +483,7 @@ def train(hyp, opt, device, callbacks):
                     torch.save(ckpt, w / f"epoch{epoch}.pt")
                 del ckpt
                 callbacks.run("on_model_save", last, epoch, final_epoch, best_fitness, fi)
+                mlflow.log_artifacts(w)
 
         # EarlyStopping
         if RANK != -1:  # if DDP training
@@ -620,7 +637,10 @@ def main(opt, callbacks=Callbacks()):
 
     # Train
     if not opt.evolve:
-        train(opt.hyp, opt, device, callbacks)
+        mlflow.set_tracking_uri('http://mlflow.mlflow:5000')
+        mlflow.set_experiment("retraining")
+        with mlflow.start_run(run_name=opt.name):
+            train(opt.hyp, opt, device, callbacks)
 
     # Evolve hyperparameters (optional)
     else:
