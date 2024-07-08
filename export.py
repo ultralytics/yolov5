9d5f41d4d4490647af8891ceffa2f5931fb2889a
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-07-08 21:32:43 +0200

    Update export.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -162,22 +162,6 @@ def export_formats():
         - Supports Training: Whether the format supports training.
         - Supports Detection: Whether the format supports detection.
     """
-        x = [
-            ["PyTorch", "-", ".pt", True, True],
-            ["TorchScript", "torchscript", ".torchscript", True, True],
-            ["ONNX", "onnx", ".onnx", True, True],
-            ["OpenVINO", "openvino", "_openvino_model", True, False],
-            ["TensorRT", "engine", ".engine", False, True],
-            ["CoreML", "coreml", ".mlmodel", True, False],
-            ["TensorFlow SavedModel", "saved_model", "_saved_model", True, True],
-            ["TensorFlow GraphDef", "pb", ".pb", True, True],
-            ["TensorFlow Lite", "tflite", ".tflite", True, False],
-            ["TensorFlow Edge TPU", "edgetpu", "_edgetpu.tflite", False, False],
-            ["TensorFlow.js", "tfjs", "_web_model", False, False],
-            ["PaddlePaddle", "paddle", "_paddle_model", True, True],
-        ]
-        return pd.DataFrame(x, columns=["Format", "Include Argument", "File Suffix", "Supports Training", "Supports Detection"])
-    """
     x = [
         ["PyTorch", "-", ".pt", True, True],
         ["TorchScript", "torchscript", ".torchscript", True, True],
