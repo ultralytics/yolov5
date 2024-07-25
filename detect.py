5bdc964da564616020370a4dc5bd0b84ba9a8ce6
Sanjith Satheesan <86307678+sxnjith@users.noreply.github.com>
2024-07-25 18:14:35 +0530

    detect.py
    Signed-off-by: Sanjith Satheesan <86307678+sxnjith@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -360,7 +360,7 @@ def parse_opt():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--weights", nargs="+", type=str, default=ROOT / "yolov5s.pt", help="model path or triton URL")
-    parser.add_argument("--source", type=str, default=ROOT / "data/images", help="file/dir/URL/glob/screen/0(webcam)")
+    parser.add_argument("--source", type=str, default=ROOT / "data/images", help="file/dir/URL/glob/screen/0 for webcam")
     parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="(optional) dataset.yaml path")
     parser.add_argument("--imgsz", "--img", "--img-size", nargs="+", type=int, default=[640], help="inference size h,w")
     parser.add_argument("--conf-thres", type=float, default=0.25, help="confidence threshold")
