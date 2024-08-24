878a98edc1ecbdb2e20adcdb1d6fa3c959560b21
UltralyticsAssistant <web@ultralytics.com>
2024-08-24 21:43:06 +0000

    Auto-format by https://ultralytics.com/actions

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -64,7 +64,6 @@ class Callbacks:
             thread: (boolean) Run callbacks in daemon thread
             kwargs: Keyword Arguments to receive from YOLOv5
         """
-
         assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
         for logger in self._callbacks[hook]:
             if thread:
