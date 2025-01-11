9d508db2b8edb47aa08f0f4730f58a3b885a0283
UltralyticsAssistant <web@ultralytics.com>
2025-01-11 11:52:22 +0000

    Auto-format by https://ultralytics.com

--- utils/loggers/clearml/clearml_utils.py
+++ utils/loggers/clearml/clearml_utils.py
@@ -41,9 +41,11 @@ def construct_dataset(clearml_info_string):
     with open(yaml_filenames[0]) as f:
         dataset_definition = yaml.safe_load(f)
 
-    assert set(dataset_definition.keys()).issuperset({"train", "test", "val", "nc", "names"}), (
-        "The right keys were not found in the yaml file, make sure it at least has the following keys: ('train', 'test', 'val', 'nc', 'names')"
-    )
+    assert set(
+        dataset_definition.keys()
+    ).issuperset(
+        {"train", "test", "val", "nc", "names"}
+    ), "The right keys were not found in the yaml file, make sure it at least has the following keys: ('train', 'test', 'val', 'nc', 'names')"
 
     data_dict = {
         "train": (
