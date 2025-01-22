c29e1d62e31ea8b49d050b0e162d2fde4fe06436
Shubham Phapale <94707673+ShubhamPhapale@users.noreply.github.com>
2025-01-22 22:06:30 +0530

    Update train.py
    updated deprecated call
    
    Signed-off-by: Shubham Phapale <94707673+ShubhamPhapale@users.noreply.github.com>

--- train.py
+++ train.py
@@ -409,7 +409,7 @@ def train(hyp, opt, device, callbacks):
                     imgs = nn.functional.interpolate(imgs, size=ns, mode="bilinear", align_corners=False)
 
             # Forward
-            with torch.cuda.amp.autocast(amp):
+            with torch.amp.autocast('cuda', amp):
                 pred = model(imgs)  # forward
                 loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
                 if RANK != -1:
