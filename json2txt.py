f57d9152f7c582cfa3694a4776af7f6c60f07fc2
UltralyticsAssistant <web@ultralytics.com>
2025-05-21 07:06:24 +0000

    Auto-format by https://ultralytics.com/actions

--- json2txt.py
+++ json2txt.py
@@ -7,10 +7,10 @@ Usage:
 """
 
 import argparse
-import os
-import sys
 import glob
 import json
+import os
+import sys
 from pathlib import Path
 
 FILE = Path(__file__).resolve()
@@ -19,12 +19,8 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from utils.general import (
-    LOGGER,
-    print_args,
-    check_file,
-    check_dataset
-)
+from utils.general import LOGGER, check_dataset, check_file, print_args
+
 
 def parse_opt(known=False):
     """
@@ -67,7 +63,7 @@ def main(opt):
         if not os.path.exists(label_file):
             continue
         label_file2 = sb.join(image_file.rsplit(sa, 1)).rsplit(".", 1)[0] + ".txt"
-        with open(label_file, "r", errors="ignore") as fin:
+        with open(label_file, errors="ignore") as fin:
             label_dict = json.load(fin)
             label_dict.pop("imageData")
             img_w, img_h = label_dict["imageWidth"], label_dict["imageHeight"]
@@ -83,9 +79,9 @@ def main(opt):
                     if yb < yt:
                         yt, yb = yb, yt
                     LOGGER.info(f"i: {i}, cls: {cls}, xl: {xl}, yt: {yt}, xr: {xr}, yb: {yb}")
-                    bbox_w, bbox_h = xr-xl, yb-yt
-                    xc, yc = xl+bbox_w/2, yt+bbox_h/2
-                    line = [idxs[cls], xc/img_w, yc/img_h, bbox_w/img_w, bbox_h/img_h]
+                    bbox_w, bbox_h = xr - xl, yb - yt
+                    xc, yc = xl + bbox_w / 2, yt + bbox_h / 2
+                    line = [idxs[cls], xc / img_w, yc / img_h, bbox_w / img_w, bbox_h / img_h]
                     bbox_infos.append(" ".join(list(map(str, line))))
             with open(label_file2, "w", errors="ignore") as fout:
                 fout.write("\n".join(bbox_infos))
