b062dc93d67c18cb702bafb25d8032daa9216108
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-12-21 17:53:04 +0100

    Fix numpy trapz usage

--- utils/metrics.py
+++ utils/metrics.py
@@ -118,7 +118,7 @@ def compute_ap(recall, precision):
     method = "interp"  # methods: 'continuous', 'interp'
     if method == "interp":
         x = np.linspace(0, 1, 101)  # 101-point interp (COCO)
-        ap = np.trapz(np.interp(x, mrec, mpre), x)  # integrate
+        ap = np.trapezoid(np.interp(x, mrec, mpre), x)  # integrate
     else:  # 'continuous'
         i = np.where(mrec[1:] != mrec[:-1])[0]  # points where x axis (recall) changes
         ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])  # area under curve
