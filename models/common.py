eed9bc192e37f77abc5e995c592c377ce97d5f4f
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-12-21 18:08:10 +0100

    Fix numpy trapz usage (#13692)
    * Fix numpy trapz usage
    
    * Update requirements.txt
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Fix numpy trapz usage
    
    ---------
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/common.py
+++ models/common.py
@@ -725,7 +725,7 @@ class DetectMultiBackend(nn.Module):
             y = self.model.predict({"image": im})  # coordinates are xywh normalized
             if "confidence" in y:
                 box = xywh2xyxy(y["coordinates"] * [[w, h, w, h]])  # xyxy pixels
-                conf, cls = y["confidence"].max(1), y["confidence"].argmax(1).astype(np.float)
+                conf, cls = y["confidence"].max(1), y["confidence"].argmax(1).astype(float)
                 y = np.concatenate((box, conf.reshape(-1, 1), cls.reshape(-1, 1)), 1)
             else:
                 y = list(reversed(y.values()))  # reversed for segmentation models (pred, proto)
