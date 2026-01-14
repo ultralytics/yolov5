e8245f0a1e47eb3bb4deaff257de7216cebe7079
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-01-14 19:11:23 +0000

    Pin paddle<3.3.0 to avoid mkldnn errors (#13697)
    * Pin paddle<3.3.0 to avoid mkldnn errors
    
    * Pin paddle<3.3.0 to avoid mkldnn errors

--- models/common.py
+++ models/common.py
@@ -661,6 +661,7 @@ class DetectMultiBackend(nn.Module):
             config = pdi.Config(str(model_file), str(params_file))
             if cuda:
                 config.enable_use_gpu(memory_pool_init_size_mb=2048, device_id=0)
+            config.disable_mkldnn()  # disable MKL-DNN for PIR compatibility
             predictor = pdi.create_predictor(config)
             input_handle = predictor.get_input_handle(predictor.get_input_names()[0])
             output_names = predictor.get_output_names()
