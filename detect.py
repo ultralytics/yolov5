29127158a2ec85e48d1c48751eafccc21afad899
oddzcv <audyfbrynti@gmail.com>
2026-02-23 16:47:21 +0800

    Adjust confidence and IOU thresholds in detect.py
    Signed-off-by: oddzcv <audyfbrynti@gmail.com>

--- detect.py
+++ detect.py
@@ -72,8 +72,8 @@ def run(
     source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
     data=ROOT / "data/coco128.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
-    conf_thres=0.25,  # confidence threshold
-    iou_thres=0.45,  # NMS IOU threshold
+    conf_thres=0.3,  # confidence threshold
+    iou_thres=0.1,  # NMS IOU threshold
     max_det=1000,  # maximum detections per image
     device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
     view_img=False,  # show results
